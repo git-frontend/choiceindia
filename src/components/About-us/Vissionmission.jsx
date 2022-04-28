68c3cb55e68ffa182171dea51610055314122404
madhu <madhumati@choicetechlab.com>
2022-04-28 15:09:12 +0530

    vissionmission section

--- src/components/About-us/Vissionmission.jsx
+++ src/components/About-us/Vissionmission.jsx
@@ -1,6 +1,7 @@
 
 import React from "react";
 import "../../assets/css/aboutus.scss";
+import Bannerimage from '../../assets/images/about-us/banner.png';
 
 function Vissionmission() {
 
@@ -10,12 +11,16 @@ function Vissionmission() {
      
     <section className="Vissionmissionsection">
         <div className="container">
-            <div className="row">
+        <div className="row">
                 <div className="col-md-6">
-                    <h2>fvxcxvfsefdcffc</h2>
+                    <h1 className="title">About Us</h1>
+
+                    <p className="para">Choice Group, incorporated in 1992, is a financial institution which finds its legacy in solving financial problems and bridging gaps for individuals, institutions and governments.</p>
+
+                    <p className="para">For past 25 years we are committed to help our clients achieve their financial aspirations and promote value creation for society.</p>
                 </div>
                 <div className="col-md-6">
-                   
+                    <img src={Bannerimage} alt="Banner Images" className="img-fluid "></img>
                 </div>
             </div>
         </div>
