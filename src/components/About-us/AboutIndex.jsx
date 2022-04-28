65036a09a24108115f1c3564fc3f5fbb04495beb
jagruti.palkar@choicetechlab.com <jagruti.palkar@choicetechlab.com>
2022-04-28 13:57:13 +0530

    changes

---
+++ src/components/About-us/AboutIndex.jsx
@@ -0,0 +1,21 @@
+
+import React from "react";
+import Banner from './Banner';
+
+function AboutIndex() {
+
+  return (
+    <div>
+      
+     
+       <div className="mainwrapper">
+       <Banner />
+
+       </div>
+      
+    
+    </div>
+  );
+}
+
+export default AboutIndex;
---
+++ src/components/About-us/Banner.jsx
@@ -0,0 +1,33 @@
+
+import React from "react";
+import "../../assets/css/aboutus.scss";
+import Bannerimage from '../../assets/images/about-us/banner.png';
+function Banner() {
+
+  return (
+    <div>
+      
+     
+    <section className="bannersection">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6">
+                    <h1 className="title">About Us</h1>
+
+                    <p className="para">Choice Group, incorporated in 1992, is a financial institution which finds its legacy in solving financial problems and bridging gaps for individuals, institutions and governments.</p>
+
+                    <p className="para">For past 25 years we are committed to help our clients achieve their financial aspirations and promote value creation for society.</p>
+                </div>
+                <div className="col-md-6">
+                    <img src={Bannerimage} alt="Banner Images" className="img-fluid "></img>
+                </div>
+            </div>
+        </div>
+    </section>
+      
+    
+    </div>
+  );
+}
+
+export default Banner;
