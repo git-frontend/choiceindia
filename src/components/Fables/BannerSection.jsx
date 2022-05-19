d40051d011183d5ac58721e6b6814a9fd9bb1553
Swapnil <swapnil.nevarekar@choicetechlab.com>
2022-05-19 19:10:06 +0530

    changes done

--- src/components/Fables/BannerSection.jsx
+++ src/components/Fables/BannerSection.jsx
@@ -10,7 +10,7 @@ function BannerSection() {
             <section className="fables-banner">
 				<div className="container">
 					<div className="row">
-						<div className="col-md-12 d-flex justify-content-between">
+						<div className="col-md-12 d-flex justify-content-between flex-wrap">
 							<div className="fables-caption-left">
 								<h2 className="big-ttl">
 									Fables
@@ -22,33 +22,33 @@ function BannerSection() {
 									<h3 className="title-secnd">Trending </h3>
 								</div>
 								<div className="fable-trending-list">
-									<a href="#" className="trending-itm">
+									<a href="/" className="trending-itm">
 										<div className="trending-itm-img">
-											<img src={Blog1} alt="" className="img-blog"/>
+											<img src={Blog1} alt="loading" className="img-blog"/>
 										</div>
 										<div className="trending-itm-des">
 											<p>Russia &amp; Ukraine conflict sparking Inflation in India!!</p>
 										</div>
 									</a>
-									<a href="#" className="trending-itm">
+									<a href="/" className="trending-itm">
 										<div className="trending-itm-img">
-											<img src={Blog1} alt="" className="img-blog"/>
+											<img src={Blog1} alt="loading" className="img-blog"/>
 										</div>
 										<div className="trending-itm-des">
 											<p>RBL Bank Management Upheaval &amp; The Chaos Around It</p>
 										</div>
 									</a>
-									<a href="#" className="trending-itm">
+									<a href="/" className="trending-itm">
 										<div className="trending-itm-img">
-											<img src={Blog1} alt="" className="img-blog"/>
+											<img src={Blog1} alt="loading" className="img-blog"/>
 										</div>
 										<div className="trending-itm-des">
 											<p>Amazon-Reliance Territorial Fight In The Indian Retail Jungle</p>
 										</div>
 									</a>
-									<a href="#" className="trending-itm">
+									<a href="/" className="trending-itm">
 										<div className="trending-itm-img">
-											<img src={Blog1} alt="" className="img-blog"/>
+											<img src={Blog1} alt="loading" className="img-blog"/>
 										</div>
 										<div className="trending-itm-des">
 											<p>What will be the future of Cryptocurrencies in India?</p>
--- src/components/Fables/FableBlogList.jsx
+++ src/components/Fables/FableBlogList.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import  Blog1  from '../../assets/images/fable/blog-list.png';
-import  Blog2  from '../../assets/images/fable/blog-big.png';
 
 function FableBlogList() {
 
@@ -12,43 +11,43 @@ function FableBlogList() {
 					<div className="fable-list-menu">
 						<ul>
 							<li>
-								<a href="#" className="link-txt link-active">All</a>
+								<a href="/" className="link-txt link-active">All</a>
 							</li>
 							<li>
-								<a href="#" className="link-txt">National</a>
+								<a href="/" className="link-txt">National</a>
 							</li>
 							<li>
-								<a href="#" className="link-txt">International</a>
+								<a href="/" className="link-txt">International</a>
 							</li>
 							<li>
-								<a href="#" className="link-txt">Economy</a>
+								<a href="/" className="link-txt">Economy</a>
 							</li>
 							<li>
-								<a href="#" className="link-txt">Industry</a>
+								<a href="/" className="link-txt">Industry</a>
 							</li>
 							<li>
-								<a href="#" className="link-txt">Commodity</a>
+								<a href="/" className="link-txt">Commodity</a>
 							</li>
 							<li>
-								<a href="#" className="link-txt">IPO</a>
+								<a href="/" className="link-txt">IPO</a>
 							</li>
 							<li>
-								<a href="#" className="link-txt">Currency</a>
+								<a href="/" className="link-txt">Currency</a>
 							</li>
 							<li>
-								<a href="#" className="link-txt">Policies</a>
+								<a href="/" className="link-txt">Policies</a>
 							</li>
 							<li>
-								<a href="#" className="link-txt">M&amp;A</a>
+								<a href="/" className="link-txt">M&amp;A</a>
 							</li>
 							<li>
-								<a href="#" className="link-txt">Trades</a>
+								<a href="/" className="link-txt">Trades</a>
 							</li>
 							<li>
-								<a href="#" className="link-txt">Banking</a>
+								<a href="/" className="link-txt">Banking</a>
 							</li>
 							<li>
-								<a href="#" className="link-txt">Technology</a>
+								<a href="/" className="link-txt">Technology</a>
 							</li>
 						</ul>
 					</div>
@@ -58,9 +57,9 @@ function FableBlogList() {
 					<div className="row">
 						<div className="col-md-12">
 							<div className="tab-blog-list">
-								<a href="#" className="tab-blog-item">
+								<a href="/" className="tab-blog-item">
 									<div className="blog-item-img">
-										<img src={Blog1} className="" />
+										<img src={Blog1} className="" alt="loading" />
 									</div>
 									<div className="blog-item-des">
 										<h4>Shein setting a trend again for fashionistas – It’s a comeback</h4>
@@ -71,9 +70,9 @@ function FableBlogList() {
 										</div>
 									</div>
 								</a>
-								<a href="#" className="tab-blog-item">
+								<a href="/" className="tab-blog-item">
 									<div className="blog-item-img">
-										<img src={Blog1} className="" />
+										<img src={Blog1} className="" alt="loading" />
 									</div>
 									<div className="blog-item-des">
 										<h4>How Indian Telcos are gearing up for 5G?</h4>
@@ -84,9 +83,9 @@ function FableBlogList() {
 										</div>
 									</div>
 								</a>
-								<a href="#" className="tab-blog-item">
+								<a href="/" className="tab-blog-item">
 									<div className="blog-item-img">
-										<img src={Blog1} className="" />
+										<img src={Blog1} className="" alt="loading" />
 									</div>
 									<div className="blog-item-des">
 										<h4>The journey of Idea cellular from idea to VI</h4>
