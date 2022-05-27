19f059fa5b243a6bdbf00addeac7d38a8ae22c9f
Swapnil <swapnil.nevarekar@choicetechlab.com>
2022-05-27 14:29:26 +0530

    changes done

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "../src/assets/css/common.scss"
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
