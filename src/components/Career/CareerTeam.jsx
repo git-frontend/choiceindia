ce39248a2ded82f480168797f3af03454984314e
krushna.mohanty <krushna.mohanty@choicetechlab.com>
2022-06-13 12:31:02 +0530

    merge error resolved

--- src/components/Career/CareerTeam.jsx
+++ src/components/Career/CareerTeam.jsx
@@ -1,5 +1,5 @@
 
-import CareerImage13 from '../../assets/images/career/bg1.jpg';
+import CareerImage13 from '../../assets/images/career/old/team-bg.jpg';
 import React from 'react';
 import { Dropdown } from 'react-bootstrap';
 // import { MDBDropdown, MDBDropdownMenu, MDBDropdownToggle, MDBDropdownItem, MDBDropdownLink } from 'mdb-react-ui-kit';
