e645900fb6fa5ed8f530455ad3c1bbb75d0a4b76
trilochan.badal <trilochan.badal@choicetechlab.com>
2022-06-16 16:10:16 +0530

    onhover change on partner page.

---
+++ src/Data/Partner.js
@@ -0,0 +1,35 @@
+
+const partnerConnect = [
+    {
+        "title": "Lineage",
+        "content": "Backed by the Choice Group -a conglomerate with 25+ years of financial experience."
+    },
+
+    {
+        "title": "Multifaceted",
+        "content": "Diversified financial services like Loans, Insurance, Mutual Funds, Stock Broking available to all."
+    },
+
+    {
+        "title": "Network",
+        "content": "Leverage our reach, our identity and our connections into the deeper underserved markets."
+    },
+
+    {
+        "title": "Security",
+        "content": "Aim to be financially independent with sustainable full-time/part-time job and guaranteed home."
+    },
+
+    {
+        "title": "Infrastructure",
+        "content": "A flexible, organic and holistic platform backed with a reputed brand and committed team."
+    },
+
+    {
+        "title": "Flexibility",
+        "content": "Sample Data for flexibility."   
+    },
+
+];
+
+export default partnerConnect;
