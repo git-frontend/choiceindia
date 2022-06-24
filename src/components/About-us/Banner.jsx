95f31747a542421aba4d9c1ac0df8e9d6f415237
madhumati <madhumati@choicetechlab.com>
2022-06-24 12:29:58 +0530

    width and height added

--- src/components/About-us/Banner.jsx
+++ src/components/About-us/Banner.jsx
@@ -22,7 +22,7 @@ function Banner() {
                 <div className="col-md-6">
                     <div className="rightsec">
 
-                    <LazyLoader src={Bannerimage} alt="Banner Images" width="826" height="646" className="img-fluid " />
+                              <LazyLoader src={Bannerimage} alt={"Banner Images"} className={"img-fluid"} width={"826"} height={"646"} />
                             {/*  <img src={Bannerimage} alt="Banner Images" width="826" height="646" className="img-fluid "></img> */}
                     </div>
                 </div>
