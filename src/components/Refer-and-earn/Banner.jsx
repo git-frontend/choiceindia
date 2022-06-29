8c4dcafc107bb4fb96209581d4e21cf146d8685b
SWAPNIL NEVAREKAR <swapnil.nevarekar@choicetechlab.com>
2022-06-29 18:04:30 +0530

    changes done

--- src/components/Refer-and-earn/Banner.jsx
+++ src/components/Refer-and-earn/Banner.jsx
@@ -2,6 +2,8 @@
 import React from "react";
 import Bannerimage from '../../assets/images/refer-earn/banner.webp';
 import LazyLoader from '../Common-features/LazyLoader';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 function Banner() {
 
@@ -11,39 +13,31 @@ function Banner() {
 
             <section className="ref-sec-banner">
                 <div className="container">
-                    <div className="row">
-                    <div className="col-md-7">
-                        <div className="sec-banner-left">
-                            <h1>Join the Best Demat Account <span>Refer and Earn</span>  Program</h1>
-                            <div className="banner-box">
-                                <p>Invite your friends and get up to Rs.1000 incentive per referral and your friend will get up to Rs. 500.</p>
-                               
-                                    <div className="get-link-form refer-link-generated-wrap mt-20" >
-                                        <div className="refer-link-generated mb-20">
-                                           
-                                                
-                                                <div className="refer-social-share">
-                                                    <a href="#" className="fb" >
-                                                        <i className="fa fa-facebook"></i></a>
+                    <div className="row align-items-center">
+                        <div className="col-xl-6 col-md-7">
+                            <div className="sec-banner-left">
+                                <h1>Join the Best Demat Account Refer and Earn Program</h1>
+                                <div className="banner-box">
+                                    <p>Invite your friends and get up to Rs.1000 incentive per referral and your friend will get up to Rs. 500.</p>
 
-                                                    <a href="#" data-action="share/whatsapp/share" className="whatsapp">
-                                                        <i className="fa fa-whatsapp" aria-hidden="true"></i>
-                                                    </a>
-
-                                                </div>
-                                </div>
-
-                                        </div>
+                                    <div class="get-link-form generate-refer-link">
+                                        <Form className="refer-form"> 
+                                            <Form.Group className="formgrp sub-formgrp" controlId="formBasicEmail">
+                                                <Form.Control type="text" name="firstName" placeholder="Enter Client ID" className="btn-bg formcontrol" />
+                                                <Button variant="primary"
+                                                    type="submit" className="btn-bg sendbtn">
+                                                    Get Link
+                                                </Button>
+                                            </Form.Group>
+                                        </Form>
                                     </div>
-                                    <h4>Don’t have an Account? <a href="https://choiceindia.com/open-free-demat-account">Open Now</a></h4>
                                 </div>
-                    </div>
-                    <div className="col-md-5">
-                        <div className="sec-banner-right">
-                            <LazyLoader src={Bannerimage} alt={"Banner Images"} className={"img-fluid"} width={"496"} height={"536"} />
-
+                                <h4>Donâ€™t have an Account? <a href="https://choiceindia.com/open-free-demat-account">Open Now</a></h4>
+                            </div>
                         </div>
-                    </div>
+                        <div className="col-xl-6 col-md-5 d-flex justify-content-end">
+                            <LazyLoader src={Bannerimage} alt={"Banner Images"} className={"img-fluid refer-banner"} width={"400"} height={"432"} />
+                        </div>
                     </div>
                 </div>
             </section>
--- src/components/Refer-and-earn/EarnUp.jsx
+++ src/components/Refer-and-earn/EarnUp.jsx
@@ -7,17 +7,17 @@ function EarnUp() {
 
   return (
       <div>
-          <section class="refer-footer-top">
-              <div class="container">
-                  <div class="row">
-                      <div class="col-md-12">
-                          <div class="top-foot-sec">
-                              <div class="foot-sec-left">
-                                  <LazyLoader src={Speaker} alt={"Banner Images"} className={"img-fluid"} width={"201"} height={"174"} />
+          <section className="refer-footer-top">
+              <div className="container">
+                  <div className="row">
+                      <div className="col-md-12">
+                          <div className="top-foot-sec">
+                              <div className="foot-sec-left">
+                                  <LazyLoader src={Speaker} alt={"Banner Images"} className={"img-fluid"} width={"80"} height={"77"} />
                                       <h3>Earn up to Rs.1000 incentive <span>per referral now!</span> </h3>
                             </div>
-                                  <div class="foot-sec-right">
-                                      <a href="#home-sec" class="join-btn">Join Now</a>
+                                  <div className="foot-sec-right">
+                                      <a href="#home-sec" className="join-btn">Join Now</a>
                                   </div>
                               </div>
                           </div>
