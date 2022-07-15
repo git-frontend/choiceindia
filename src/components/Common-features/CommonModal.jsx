6572010903f94191a0155f44e61916b2b73610b6
trilochan.badal <trilochan.badal@choicetechlab.com>
2022-07-15 18:41:13 +0530

    footer

---
+++ src/components/Common-features/CommonModal.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import './common-modal.scss'
+function CommonModal(props) {
+console.log("PROPS",props)
+    const [display, setDisplay] = useState(true);
+   // console.log('MODAL', props);
+
+    useEffect(() => {
+    }, [])
+
+    function closeModal(){
+        setDisplay(false);
+    }
+
+    return (
+
+        <>
+            <Modal className='common-modal-css'
+                show={display}
+                size="sm"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <Modal.Header>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        {props.title}
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>
+                        {props.content}
+                    </p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={()=>{closeModal()}}>{props.closebtn}</Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    )
+}
+
+export default CommonModal;
