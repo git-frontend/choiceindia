b28e1242cde670e2f3c5a20c069ebbd47633a845
madhumati <madhumati@choicetechlab.com>
2022-07-21 18:19:46 +0530

    pop up added

--- src/components/Contact/CampaignHeader.jsx
+++ src/components/Contact/CampaignHeader.jsx
@@ -13,14 +13,14 @@ function CampaignHeader() {
             <Link className="navbar-brand logo-main" to="/">
               <img src={ImageSub0} width="150" height="37" className="img-fluid logo-dark" alt="Choice India" />
             </Link>
-            <div className="collapse navbar-collapse">
+            {/* <div className="collapse navbar-collapse">
               <ul className="navbar-nav ms-auto nav-header">
 
                 <li className="nav-item get-btn">
                   <button className="nav-link">Join Now</button>
                 </li>
               </ul>
-            </div>
+            </div> */}
           </div>
         </nav>
       </header>
