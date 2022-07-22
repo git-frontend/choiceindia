d0d83edda607e7499e714c8108ff598327f05c56
SWAPNIL NEVAREKAR <swapnil.nevarekar@choicetechlab.com>
2022-07-22 14:55:47 +0530

    changes done

---
+++ src/components/SubBroker/WhyBecomeSubBrokerHindi.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+function WhyBecomeSubBrokerHindi () {
+    return (
+        <div>
+            <section className="franchise-abt">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-xl-10 col-md-12">
+                            <h2 className="ttl-wht">चॉइस सब ब्रोकर क्यों बनें?</h2>
+                            <p className="sml-para-dv">सेबी सब ब्रोकर के रूप में, आप जाने-माने और सम्मानित चॉइस ग्रुप का हिस्सा बन सकते हैं। किसी भी स्टॉक एक्सचेंज (NSE/BSE) के साथ रजिस्टर करें और एक प्रतिष्ठित ब्रांड के साथ स्टॉक मार्केट में सब ब्रोकर बनें।</p>
+                        </div>
+                    </div>
+                    <div className="row justify-content-center">
+                        <div className="col-xl-10 col-md-12">
+                            <div className="counter-list">
+                                <div className="count-item">
+                                    <h3><span className="counter">4</span>L+</h3>
+
+                                    <h6>ग्राहक</h6>
+                                </div>
+                                <div className="count-item">
+                                    <h3><span className="counter">50</span></h3>
+                                    <h6>स्थानीय कार्यालय</h6>
+                                </div>
+                                <div className="count-item">
+                                    <h3><span className="counter">3000</span>+</h3>
+                                    <h6>फ्रेंचाइजी</h6>
+                                </div>
+                                <div className="count-item">
+                                    <h3><span className="counter">1500</span>+</h3>
+                                    <h6>कर्मचारी</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+
+    );
+}
+
+export default WhyBecomeSubBrokerHindi;
