860082f4c94bfaff338811c3db300d765a59b9db
krushna.mohanty <krushna.mohanty@choicetechlab.com>
2022-08-01 12:07:30 +0530

    change slider content to hindi

---
+++ src/Data/Onlinetrading/HindiRealTrade.js
@@ -0,0 +1,43 @@
+
+import  ImageSub0  from '../../assets/images/online-trading-app/jiffy_signal.webp';
+import  ImageSub1  from '../../assets/images/online-trading-app/c-quant.webp';
+import  ImageSub2  from '../../assets/images/online-trading-app/sumeet_bagadia.webp';
+import  ImageSub3  from '../../assets/images/online-trading-app/long_term_research_calls.webp';
+
+const imageData =
+[
+    {
+        "image":`${ImageSub0}`,
+        "title":"जिफ्फी सिग्नल",
+        "id": 1,
+        "description":"AI पर आधारित रिसर्च इंजन जो रीयल टाइम ट्रेडिंग सिग्नल प्रदान करता है|"
+        
+        
+    },
+    {
+        "image":`${ImageSub1}`,
+        "title":"सी-क्वांट ",
+        "id": 2,
+        "description":"टॉप 200 शेयरों की एक क्यूरेटेड सूची, रिसर्च विभाग द्वारा पूरी तरह से बैक-टेस्ट और सांख्यिकीय रूप से मान्य।"
+        
+        
+    },
+    {
+        "image":`${ImageSub2}`,
+        "title":"सुमीत बगड़िया डेस्क",
+        "id": 3,
+        "description":" सेक्टर स्पेसिफिक निवेशों के साथ-साथ सभी प्रकार के ट्रेडों के लिए एक्सपर्ट स्टॉक रेकमेन्डेशन प्राप्त करें।"
+    
+        
+    },
+    {
+        "image":`${ImageSub3}`,
+        "title":"लंबी अवधि के रिसर्च कॉल ",
+        "id": 4,
+        "description":"लंबी अवधि के रिसर्च कॉल में निवेश करें जो अच्छी तरह से शोध किए गए हैं और विशेषज्ञों की हमारी टीम द्वारा चुने गए हैं।"
+    
+        
+    }
+]
+
+export default imageData
