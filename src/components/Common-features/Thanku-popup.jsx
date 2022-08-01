52a14639140c10acec42c6b3e191c1dcccdea1b5
trilochan.badal <trilochan.badal@choicetechlab.com>
2022-08-01 16:36:41 +0530

    success, failure, and info popup.

--- src/components/Common-features/Thanku-popup.jsx
+++ src/components/Common-features/Thanku-popup.jsx
@@ -1,35 +1,69 @@
 import './Thankyoupopup.scss';
 import { Link } from "react-router-dom";
-function Thankyoupopup() {
+import { useEffect, useState, useRef } from 'react';
+import successimg from '../../assets/images/success.svg';
+import failureimg from '../../assets/images/failure.svg';
+
+function Thankyoupopup({isShow}) {
+
+  console.log('TRTR',isShow);
+  // const [show, setShow] = useState(isShow);
+  // const show = useRef(null);
+
+  // useEffect(() => {
+  //   // setShow(() => isShow)
+  //   show.current = isShow.showModal;
+  //   console.log('effect',show);
+  // },[props]);
+
+  // function closeModal(){
+  //   // setShow(() => false);
+  //   show.current = false;
+  //   console.log('CLosemodal',show)
+  // }
+
   return (
     <>
-<div>
-      <div className="thanku-overlay thanku-popup-active">
-        <div className="thanku-popup">
-          <div className="sub-row">
-             <div className="close">
-                <Link to="" className="closebtn" >&times;</Link>
-              </div> 
-            <div className="thanku">
-              <h1>Thank You !</h1>
-            </div>
-            <div className="body-content">
-              <p className="heading">Thank You For Your Registration!</p>
-              <p className="subheading">You are being redirected to onboarding page!</p>
-              <button className="btn-yellow">OK</button>
-              <div>
+      {
+        <div>
+          <div className="thanku-overlay thanku-popup-active">
+            <div className="thanku-popup">
+              <div className="sub-row">
+                {/* <div className="close">
+                  <Link to="" className="closebtn" onClick={closeModal} >&times;</Link>
+                </div> */}
+                {/* <div className="thanku">
+                  <h1>Thank You !</h1>
+                </div> */}
+                <div className="body-content">
+                <div className="close">
+                  <Link to="" className="closebtn" onClick={()=>isShow.closeMd()}>&times;</Link>
+                </div>
+                  {/* <p className="heading">Thank You For Your Registration!</p> */}
+                  <div className='body-img'>
+                    {
+                      isShow.isFailure? <img src = {failureimg} height="100" width="100" alt='Failure'/>:<img src = {successimg} height="100" width="100" alt='Success'/>
+                    }
+                  </div>
+                  <p className="heading">{isShow.titleText}</p>
+                  {/* <p className="subheading">You are being redirected to onboarding page!</p> */}
+                  <p className="subheading">{isShow.msgText}</p>
+                  <button className="btn-yellow" onClick={()=>isShow.closeMd()}>OK</button>
+                  <div>
+
+                  </div>
+
+
+
+                </div>
+
 
               </div>
-
-
-
             </div>
-
-
           </div>
-        </div>
-      </div>
-      </div>
+        </div> 
+      }
+
 
     </>
   )
