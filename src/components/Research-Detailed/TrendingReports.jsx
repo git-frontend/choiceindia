6d00d6410df733bf6ced6460438d528238e68daa
krushna.mohanty <krushna.mohanty@choicetechlab.com>
2022-08-04 16:44:00 +0530

    research detail

--- src/components/Research-Detailed/TrendingReports.jsx
+++ src/components/Research-Detailed/TrendingReports.jsx
@@ -1,27 +1,29 @@
 
 import React from "react";
-import {useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import thumb1 from '../../assets/images/research/thumbnail-1.webp';
 import ResearchService from "../../Services/ResearchService";
-import {Routes, Route, useNavigate, useLocation} from 'react-router-dom';
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 
 function TrendingReports(props) {
 
   const [list, setList] = useState(null);
+  const [showData, setShowData] = useState([]);
+  const [check, setCheck] = useState(false);
   const navigate = useNavigate();
   const search = useLocation().search;
   const name = new URLSearchParams(search).get('id');
   // console.log('BBBBBBBBBBB',props);
   let sliceCount = Math.floor((Math.random() * 5) + 1);
 
-  function getSingleDetail(id){
-    console.log('IIIII',id);
-    
-      // navigate(`/research-new/${id}/${props.data}`);
-      navigate({
-        pathname: `/research-new/${id}/${props.data}`,
-        search: `?id=${name? name: '41041eaf-c9f1-41b3-a2fc-b6c20d29c4ad'}`
-      })
+  function getSingleDetail(id) {
+    console.log('IIIII', id);
+
+    // navigate(`/research-new/${id}/${props.data}`);
+    navigate({
+      pathname: `/research-new/${id}/${props.data}`,
+      search: `?id=${name ? name : '41041eaf-c9f1-41b3-a2fc-b6c20d29c4ad'}`
+    })
   }
 
   // function getSingleResearchDetail(id){
@@ -44,13 +46,14 @@ function TrendingReports(props) {
       res => {
         // console.log('YYYYY',res.response.totalCount);
         setList(res.response.data);
+        setShowData(res.response.data);
       }
     )
   };
 
   useEffect(() => {
-    loadResearch(name? name: '41041eaf-c9f1-41b3-a2fc-b6c20d29c4ad')
-  },[])
+    loadResearch(name ? name : '41041eaf-c9f1-41b3-a2fc-b6c20d29c4ad')
+  }, [])
 
   return (
     <div>
@@ -60,7 +63,7 @@ function TrendingReports(props) {
         <div className="container">
           <div className="row">
             <div className="col-md-12">
-              
+
               <div className="heading-sec">
                 <h3 className="title-first">Trending Reports</h3>
               </div>
@@ -69,94 +72,136 @@ function TrendingReports(props) {
           <div className="row">
             <div className="col-md-12">
               <div className="res-detailtab-cont">
-                <div className="res-detailtab-list">
 
-                  {
-                    list?.slice(0,4)?.map((res,i)=> {
-                      
-                      console.log('RESPONSE',res)
-                      return(
-                        
-                        <div className="res-tab-itm">
-                        <div className="tab-itm-img">
-                        <img src={res?.feature_image? res?.feature_image : ''  } alt="Banner Images" className="img-fluid thumb-img" width="237" height="257"></img>
-                          {/* <img src={thumb1} alt="Banner Images" className="img-fluid thumb-img" width="237" height="257"></img> */}
-                        </div>
-                        <div className="tab-itm-des">
-                        <h3 className="ttl-des">{res?.title? res?.title : ''} </h3>
-                          {/* <h3 className="ttl-des">Equity Research Report <span className="info-txt">WINDLAS ( BSE )</span> </h3> */}
-                          <p className="tag-pref">{res?.report_name? res?.report_name : ''}</p>
-                          <div className="itm-des-sub">
-                            <span className="date-post">03 Mar 2022</span>
-                            <a onClick={() => {getSingleDetail(res.uuid)}} className="post-read">Read More</a>
-                            {/* <a href="#" className="post-read">Read More</a> */}
-                          </div>
-                        </div>
+                {
+                  check ?
+                    <div className="res-detailtab-list">
+                      {
+                        list?.map((res, i) => {
+
+                          console.log('RESPONSE', res)
+                          return (
+
+                            <div className="res-tab-itm">
+                              <div className="tab-itm-img">
+                                <img src={res?.feature_image ? res?.feature_image : ''} alt="Banner Images" className="img-fluid thumb-img" width="237" height="257"></img>
+                                {/* <img src={thumb1} alt="Banner Images" className="img-fluid thumb-img" width="237" height="257"></img> */}
+                              </div>
+                              <div className="tab-itm-des">
+                                <h3 className="ttl-des">{res?.title ? res?.title : ''} </h3>
+                                {/* <h3 className="ttl-des">Equity Research Report <span className="info-txt">WINDLAS ( BSE )</span> </h3> */}
+                                <p className="tag-pref">{res?.report_name ? res?.report_name : ''}</p>
+                                <div className="itm-des-sub">
+                                  <span className="date-post">03 Mar 2022</span>
+                                  <a onClick={() => { getSingleDetail(res.uuid) }} className="post-read">Read More</a>
+                                  {/* <a href="#" className="post-read">Read More</a> */}
+                                </div>
+                              </div>
+                            </div>
+                          )
+                        })
+                      }
+                    </div> :
+                    <div className="res-detailtab-list">
+
+                      {
+                        list?.slice(0, 4)?.map((res, i) => {
+
+                          console.log('RESPONSE', res)
+                          return (
+
+                            <div className="res-tab-itm">
+                              <div className="tab-itm-img">
+                                <img src={res?.feature_image ? res?.feature_image : ''} alt="Banner Images" className="img-fluid thumb-img" width="237" height="257"></img>
+                                {/* <img src={thumb1} alt="Banner Images" className="img-fluid thumb-img" width="237" height="257"></img> */}
+                              </div>
+                              <div className="tab-itm-des">
+                                <h3 className="ttl-des">{res?.title ? res?.title : ''} </h3>
+                                {/* <h3 className="ttl-des">Equity Research Report <span className="info-txt">WINDLAS ( BSE )</span> </h3> */}
+                                <p className="tag-pref">{res?.report_name ? res?.report_name : ''}</p>
+                                <div className="itm-des-sub">
+                                  <span className="date-post">03 Mar 2022</span>
+                                  <a onClick={() => { getSingleDetail(res.uuid) }} className="post-read">Read More</a>
+                                  {/* <a href="#" className="post-read">Read More</a> */}
+                                </div>
+                              </div>
+                            </div>
+                          )
+                        })
+                      }
+                    </div>
+
+                }
+
+
+
+
+                {/* <div className="res-tab-itm">
+                    <div className="tab-itm-img">
+                      <img src={thumb1} alt="Banner Images" className="img-fluid thumb-img" width="237" height="257"></img>
+                    </div>
+                    <div className="tab-itm-des">
+                      <h3 className="ttl-des">Equity Research Report <span className="info-txt">WINDLAS ( BSE )</span> </h3>
+                      <p className="tag-pref">FUNDAMENTAL</p>
+                      <div className="itm-des-sub">
+                        <span className="date-post">03 Mar 2022</span>
+                        <a onClick={() => {getSingleDetail(res.uuid)}} className="post-read">Read More</a>
+                        <a href="#" className="post-read">Read More</a>
                       </div>
-                      )
-                    })
+                    </div>
+                  </div> */}
+                {/* <div className="res-tab-itm">
+                    <div className="tab-itm-img">
+                      <img src={thumb1} alt="Banner Images" className="img-fluid thumb-img" width="237" height="257"></img>
+                    </div>
+                    <div className="tab-itm-des">
+                      <h3 className="ttl-des">Equity Research Report <span className="info-txt">WINDLAS ( BSE )</span> </h3>
+                      <p className="tag-pref">FUNDAMENTAL</p>
+                      <div className="itm-des-sub">
+                        <span className="date-post">03 Mar 2022</span>
+                        <a onClick={() => {getSingleDetail(res.uuid)}} className="post-read">Read More</a>
+                        <a href="#" className="post-read">Read More</a>
+                      </div>
+                    </div>
+                  </div> */}
+                {/* <div className="res-tab-itm">
+                    <div className="tab-itm-img">
+                      <img src={thumb1} alt="Banner Images" className="img-fluid thumb-img" width="237" height="257"></img>
+                    </div>
+                    <div className="tab-itm-des">
+                      <h3 className="ttl-des">Equity Research Report <span className="info-txt">WINDLAS ( BSE )</span> </h3>
+                      <p className="tag-pref">FUNDAMENTAL</p>
+                      <div className="itm-des-sub">
+                        <span className="date-post">03 Mar 2022</span>
+                        <a onClick={() => {getSingleDetail(res.uuid)}} className="post-read">Read More</a>
+                        <a href="#" className="post-read">Read More</a>
+                      </div>
+                    </div>
+                  </div> */}
+                {/* <div className="res-tab-itm">
+                    <div className="tab-itm-img">
+                      <img src={thumb1} alt="Banner Images" className="img-fluid thumb-img" width="237" height="257"></img>
+                    </div>
+                    <div className="tab-itm-des">
+                      <h3 className="ttl-des">Equity Research Report <span className="info-txt">WINDLAS ( BSE )</span> </h3>
+                      <p className="tag-pref">FUNDAMENTAL</p>
+                      <div className="itm-des-sub">
+                        <span className="date-post">03 Mar 2022</span>
+                        <a onClick={() => {getSingleDetail(res.uuid)}} className="post-read">Read More</a>
+                        <a href="#" className="post-read">Read More</a>
+                      </div>
+                    </div>
+                  </div> */}
+                  {
+                    check ?
+                    "":<div className="mt-5 d-flex justify-content-center">
+                    <button className="btn-bg"onClick={() => {setCheck(true)}}>Explore All</button>
+                  </div>
                   }
 
-                  {/* <div className="res-tab-itm">
-                    <div className="tab-itm-img">
-                      <img src={thumb1} alt="Banner Images" className="img-fluid thumb-img" width="237" height="257"></img>
-                    </div>
-                    <div className="tab-itm-des">
-                      <h3 className="ttl-des">Equity Research Report <span className="info-txt">WINDLAS ( BSE )</span> </h3>
-                      <p className="tag-pref">FUNDAMENTAL</p>
-                      <div className="itm-des-sub">
-                        <span className="date-post">03 Mar 2022</span>
-                        <a onClick={() => {getSingleDetail(res.uuid)}} className="post-read">Read More</a>
-                        <a href="#" className="post-read">Read More</a>
-                      </div>
-                    </div>
-                  </div> */}
-                  {/* <div className="res-tab-itm">
-                    <div className="tab-itm-img">
-                      <img src={thumb1} alt="Banner Images" className="img-fluid thumb-img" width="237" height="257"></img>
-                    </div>
-                    <div className="tab-itm-des">
-                      <h3 className="ttl-des">Equity Research Report <span className="info-txt">WINDLAS ( BSE )</span> </h3>
-                      <p className="tag-pref">FUNDAMENTAL</p>
-                      <div className="itm-des-sub">
-                        <span className="date-post">03 Mar 2022</span>
-                        <a onClick={() => {getSingleDetail(res.uuid)}} className="post-read">Read More</a>
-                        <a href="#" className="post-read">Read More</a>
-                      </div>
-                    </div>
-                  </div> */}
-                  {/* <div className="res-tab-itm">
-                    <div className="tab-itm-img">
-                      <img src={thumb1} alt="Banner Images" className="img-fluid thumb-img" width="237" height="257"></img>
-                    </div>
-                    <div className="tab-itm-des">
-                      <h3 className="ttl-des">Equity Research Report <span className="info-txt">WINDLAS ( BSE )</span> </h3>
-                      <p className="tag-pref">FUNDAMENTAL</p>
-                      <div className="itm-des-sub">
-                        <span className="date-post">03 Mar 2022</span>
-                        <a onClick={() => {getSingleDetail(res.uuid)}} className="post-read">Read More</a>
-                        <a href="#" className="post-read">Read More</a>
-                      </div>
-                    </div>
-                  </div> */}
-                  {/* <div className="res-tab-itm">
-                    <div className="tab-itm-img">
-                      <img src={thumb1} alt="Banner Images" className="img-fluid thumb-img" width="237" height="257"></img>
-                    </div>
-                    <div className="tab-itm-des">
-                      <h3 className="ttl-des">Equity Research Report <span className="info-txt">WINDLAS ( BSE )</span> </h3>
-                      <p className="tag-pref">FUNDAMENTAL</p>
-                      <div className="itm-des-sub">
-                        <span className="date-post">03 Mar 2022</span>
-                        <a onClick={() => {getSingleDetail(res.uuid)}} className="post-read">Read More</a>
-                        <a href="#" className="post-read">Read More</a>
-                      </div>
-                    </div>
-                  </div> */}
-                </div>
-                <div className="mt-5 d-flex justify-content-center">
-                  <a className="btn-bg" href="/">Explore All</a>
-                </div>
+                {/* <div className="mt-5 d-flex justify-content-center">
+                  <button className="btn-bg"onClick={() => {setCheck(true)}}>Explore All</button>
+                </div> */}
               </div>
             </div>
           </div>
