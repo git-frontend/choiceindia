bd0c879dcca4740a57fe509126ad2409661ccc46
madhumati <madhumati@choicetechlab.com>
2022-08-05 14:38:56 +0530

    div format

--- src/components/Fact-Sheet/FactSheetMenu.jsx
+++ src/components/Fact-Sheet/FactSheetMenu.jsx
@@ -13,34 +13,36 @@ function FactSheetMenu() {
                             <Navbar />
                         </div>
                     </div>
-                    <div className="row code-mainwrapper cgmainwrap">
-                        <div className="col-md-12">
 
-                        <div className="d-flex justify-content-between">
+
+
+                    {/* <div className="col-md-5 ">
                                 <h3 className="head">Name </h3>
                                 <h3 className="head">Choice International Limitied</h3>
                                 
-                                </div>
-                                <div className="subtext">
-                                   
+                                </div> */}
+                    <div className=" pl-3 pr-3 cgmainwrap code-mainwrapper subtext">
+                        <div className="row border-bottom">
+                            <div className=" col-md-5 col "><h3 className="head">Name </h3></div>
+                            <div className="col-md-7 col "> <h3 className="head">Choice International Limitied</h3></div>
+                        </div>
+                        <div className="row border-bottom ">
+                            <div className=" col-md-5 col pb-3 pt-3">CIN </div>
+                            <div className="col-md-7 col pb-3 pt-3">L67190MH1993PLC071117</div>
+                        </div>
+                    </div>
 
-                                <div className="border-bottom d-flex justify-content-between pb-3 pt-3">
-                                   <div>CIN </div>
-                                   <div>L67190MH1993PLC071117</div>
-                                   
-                                   </div>
-
-                                   <div className="border-bottom d-flex justify-content-between pb-3 pt-3">
+                    {/* <div className="border-bottom d-flex justify-content-between pb-3 pt-3">
                                    <div>Business Description </div>
                                    <div>Financial Services</div>
                                    
-                                   </div>
-                                   <div className="border-bottom d-flex justify-content-between pb-3 pt-3">
+                                   </div> */}
+                    {/* <div className="border-bottom d-flex justify-content-between pb-3 pt-3">
                                    <div>Established</div>
                                    <div>March 12, 1993</div>
                                    
-                                   </div>
-                                   <div className="border-bottom d-flex justify-content-between pb-3 pt-3">
+                                   </div> */}
+                    {/* <div className="border-bottom d-flex justify-content-between pb-3 pt-3">
                                    <div>Registered Office</div>
                                    <div>Choice International Limited, Sunil Patodia Tower,J B Nagar, Andheri (East),<br/>
 Mumbai 400099, Maharashtra<br/>
@@ -48,56 +50,56 @@ Tel.No. +91-022-67079999<br/>
 Fax:+91-022-67079959<br/>
 Email Id: <a href="mailto:info@choiceindia.com">info@choiceindia.com</a></div>
                                    
-                                   </div>
+                                   </div> */}
 
-                                   <div className="border-bottom d-flex justify-content-between pb-3 pt-3">
+                    {/* <div className="border-bottom d-flex justify-content-between pb-3 pt-3">
                                    <div>Managing Director</div>
                                    <div>Mr. Kamal Poddar</div>
                                    
-                                   </div>
+                                   </div> */}
 
-                                   <div className="border-bottom d-flex justify-content-between pb-3 pt-3">
+                    {/* <div className="border-bottom d-flex justify-content-between pb-3 pt-3">
                                    <div>Compliance Officer</div>
                                    <div>Ms Karishma Shah</div>
                                    
-                                   </div>
+                                   </div> */}
 
-                                   <div className="border-bottom d-flex justify-content-between pb-3 pt-3">
+                    {/* <div className="border-bottom d-flex justify-content-between pb-3 pt-3">
                                    <div>Listings</div>
                                    <div>BSE & NSE</div>
                                    
-                                   </div>
-                                   <div className="border-bottom d-flex justify-content-between pb-3 pt-3">
+                                   </div> */}
+                    {/* <div className="border-bottom d-flex justify-content-between pb-3 pt-3">
                                    <div>BSE Code</div>
                                    <div>531358</div>
                                    
-                                   </div>
+                                   </div> */}
 
-                                   <div className="border-bottom d-flex justify-content-between pb-3 pt-3">
+                    {/* <div className="border-bottom d-flex justify-content-between pb-3 pt-3">
                                    <div>Script ID</div>
                                    <div>CHOICEIN</div>
                                    
-                                   </div>
+                                   </div> */}
 
-                                   <div className="border-bottom d-flex justify-content-between pb-3 pt-3">
+                    {/* <div className="border-bottom d-flex justify-content-between pb-3 pt-3">
                                    <div>Demat</div>
                                    <div>NSDL, CSDL</div>
                                    
-                                   </div>
+                                   </div> */}
 
-                                   <div className="border-bottom d-flex justify-content-between pb-3 pt-3">
+                    {/* <div className="border-bottom d-flex justify-content-between pb-3 pt-3">
                                    <div>Demat</div>
                                    <div>NSDL, CSDL</div>
                                    
-                                   </div>
+                                   </div> */}
 
-                                   <div className="border-bottom d-flex justify-content-between pb-3 pt-3">
+                    {/* <div className="border-bottom d-flex justify-content-between pb-3 pt-3">
                                    <div>ISIN</div>
                                    <div>INE102B01014</div>
                                    
-                                   </div>
+                                   </div> */}
 
-                                   <div className="border-bottom d-flex justify-content-between pb-3 pt-3">
+                    {/* <div className="border-bottom d-flex justify-content-between pb-3 pt-3">
                                    <div>Registrar & Share Transfer Agent ( RTA )</div>
                                    <div>Name : Link Intime India Pvt Ltd<br/>
 Add : C-101, 247 Park, L.B.S. Marg, Vikhroli (West), Mumbai -400 083<br/>
@@ -105,22 +107,22 @@ TEL NO : 022 49186270<br/>
 EMAIL ID : <a href="mailto:rnt.helpdesk@linkintime.co.in">rnt.helpdesk@linkintime.co.in</a><br/>
 WEBSITE : <a href="https://www.linkintime.co.in/">www.linkintime.co.in</a></div>
                                    
-                                   </div>
-                                 
-                                  
-                                </div>
-                           
-                           
+                                   </div> */}
 
-                        </div>
-                       
-                       
-                       
-                      
-                    </div>
-                  
 
                 </div>
+
+
+
+
+
+
+
+
+
+
+
+
             </section>
         </div>
     );
