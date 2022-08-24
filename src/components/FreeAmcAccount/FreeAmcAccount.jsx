aacde168139d0b45069526f95b33098699a23262
krushna.mohanty <krushna.mohanty@choicetechlab.com>
2022-08-24 19:48:17 +0530

    meta changes

--- src/components/FreeAmcAccount/FreeAmcAccount.jsx
+++ src/components/FreeAmcAccount/FreeAmcAccount.jsx
@@ -7,7 +7,24 @@ import AmcAccountOpeningProcess from "./AmcAccountOpeningProcess";
 import LowBrokerageAMCAccount from "./LowBrokerageAMCAccount";
 import WhyChoiceamc from "./WhyChoiceamc";
 import AmcFaq from "./AmcFaq";
+import meta_tags from "../../Data/MetaTags";
+import { useState,useEffect } from "react";
 function FreeAmcAccount() {
+    const [rendercount, setRenderCount] = useState(() => false);
+
+  useEffect(() => {
+    setRenderCount(true)
+    if (rendercount === true) {
+      // let parser = new DOMParser();
+      // let doc = parser.parseFromString(meta_tags['sub-broker'].faqscript, 'text/html');
+      // document.body.appendChild(doc.getElementsByTagName('script')[0]? doc.getElementsByTagName('script')[0]: '' );
+      document.title = meta_tags[location.pathname.replace('/', "")] ? meta_tags[location.pathname.replace('/', "")].title : '';
+      // document.getElementById('meta-tags').name= meta_tags[location.pathname.replace('/',"")]? meta_tags[location.pathname.replace('/',"")].title : ''  ;
+      document.getElementById('meta-tags').content = meta_tags[location.pathname.replace('/', "")] ? meta_tags[location.pathname.replace('/', "")].content : '';
+      document.getElementById('canonical-link').href = meta_tags[location.pathname.replace('/', "")] ? meta_tags[location.pathname.replace('/', "")].link : '';
+    }
+  }, [rendercount])
+
     return (
         <>
             {/* Open Trading Account
--- src/components/Terms-Conditions/TermsConditions.jsx
+++ src/components/Terms-Conditions/TermsConditions.jsx
@@ -1,7 +1,26 @@
 import '../Privacy-policy/privacypolicy.scss';
 import { Link } from 'react-router-dom';
+import meta_tags from '../../Data/MetaTags';
+import { useState,useEffect } from 'react';
 
 function TermsConditions() {
+
+    const [rendercount, setRenderCount] = useState(() => false);
+
+  useEffect(() => {
+    setRenderCount(true)
+    if (rendercount === true) {
+      // let parser = new DOMParser();
+      // let doc = parser.parseFromString(meta_tags['sub-broker'].faqscript, 'text/html');
+      // document.body.appendChild(doc.getElementsByTagName('script')[0]? doc.getElementsByTagName('script')[0]: '' );
+      document.title = meta_tags[location.pathname.replace('/', "")] ? meta_tags[location.pathname.replace('/', "")].title : '';
+      // document.getElementById('meta-tags').name= meta_tags[location.pathname.replace('/',"")]? meta_tags[location.pathname.replace('/',"")].title : ''  ;
+      document.getElementById('meta-tags').content = meta_tags[location.pathname.replace('/', "")] ? meta_tags[location.pathname.replace('/', "")].content : '';
+      document.getElementById('canonical-link').href = meta_tags[location.pathname.replace('/', "")] ? meta_tags[location.pathname.replace('/', "")].link : '';
+    }
+  }, [rendercount])
+
+
   return (
       <>
       <div>
