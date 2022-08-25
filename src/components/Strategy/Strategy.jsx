70ed5b1b0d623f1425eca39ccf7d609675f1e6c8
krushna.mohanty <krushna.mohanty@choicetechlab.com>
2022-08-25 18:13:40 +0530

    changes

--- src/components/Strategy/Strategy.jsx
+++ src/components/Strategy/Strategy.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./css/stratezy-landing-page.scss";
 import FaqAccordion from "./StartegyFaq";
 import WhyStrategy from "./Whystrategy";
