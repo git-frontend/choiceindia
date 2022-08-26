fe82c1b9c3c6e140fd89ff397e79358aefb64ddb
krushna.mohanty <krushna.mohanty@choicetechlab.com>
2022-08-26 11:35:56 +0530

    changes

--- src/components/home-page/HomePageBanner.jsx
+++ src/components/home-page/HomePageBanner.jsx
@@ -9,16 +9,16 @@ function HomePageBanner() {
 
     /** scroll id view */
 
-  function chapterScroll(id) {
-    var element = document.getElementById(id);
-    var headerOffset = 140;
-    var elementPosition = element.getBoundingClientRect().top;
-    var offsetPosition = elementPosition + window.pageYOffset - headerOffset;
-    window.scrollTo({
-      top: offsetPosition,
-      behavior: "smooth"
-    });
-  }
+    function chapterScroll(id) {
+        var element = document.getElementById(id);
+        var headerOffset = 140;
+        var elementPosition = element.getBoundingClientRect().top;
+        var offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: "smooth"
+        });
+    }
 
     // const [primary, setPrimary] = useState([]);
     // const [trigger, setTrigger] = useState(false);
@@ -56,31 +56,32 @@ function HomePageBanner() {
 
                     return ( */}
 
-                        <section className="home-banner" >
-                            <div className="banner-desc">
-                                <div className="container">
-                                    <div className="row">
-                                        <div className="col-md-6">
-                                            <div className="banner-caption" >
+            <section className="home-banner" >
+                <div className="banner-desc">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-6">
+                                <div className="banner-caption" >
 
-                                                {/* <h1>{response.title} <br/>{response.description}</h1> */}
-                                                <h1>Experience <br />The Joy of Earning</h1>
-                                                <p className="pt-5 pb-5">Choice makes it easier for you to handle your own finances. Let's take pleasure in each and every moment of your financial journey with us.</p>
-                                                <Link to="/" className="text-md-start text-center cursor-pointer" onClick={()=>{chapterScroll('everydayfin')}} >
-                        <span className="btn-bg">Explore Now</span>
-                      </Link>
-                                            </div>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <LazyLoader src={ImageT} className={"banner-img"} width={"654"} height={"904"} alt={"The Joy of Earning"} />
-                                            {/**<img src={`https://cmsapi.choiceindia.com/assets/${response.image}`} className="banner-img" alt="Loading" />*/}
-                                        </div>
-                                    </div>
+                                    {/* <h1>{response.title} <br/>{response.description}</h1> */}
+                                    <h1>Experience <br />The Joy of Earning</h1>
+                                    <p className="pt-5 pb-5">Choice makes it easier for you to handle your own finances. Let's take pleasure in each and every moment of your financial journey with us.</p>
+                                    <Link to="/" className="text-md-start text-center cursor-pointer" onClick={() => { chapterScroll('everydayfin') }} >
+                                        <span className="btn-bg">Explore Now</span>
+                                    </Link>
                                 </div>
                             </div>
-                        </section>
 
-                    {/* )
+                            <div className="col-md-6">
+                                <LazyLoader src={ImageT} className={"banner-img"} width={"654"} height={"904"} alt={"The Joy of Earning"} />
+                                {/**<img src={`https://cmsapi.choiceindia.com/assets/${response.image}`} className="banner-img" alt="Loading" />*/}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            {/* )
 
                 })
 
