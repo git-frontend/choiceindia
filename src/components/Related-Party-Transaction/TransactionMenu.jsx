1d1be1e579723a58b2dd7551c0b0f8eda875f93e
SWAPNIL NEVAREKAR <swapnil.nevarekar@choicetechlab.com>
2022-08-30 15:52:53 +0530

    changes done

--- src/components/Related-Party-Transaction/TransactionMenu.jsx
+++ src/components/Related-Party-Transaction/TransactionMenu.jsx
@@ -53,10 +53,10 @@ function TransactionMenu() {
                             <Navbar />
                         </div>
                     </div>
-                    <div className="row code-mainwrapper cgmainwrap">
+                    <div className="row code-mainwrapper cgmainwrap voting-result">
                         <div className="col-md-12">
 
-                            <div className="d-flex justify-content-between">
+                            <div className="d-flex justify-content-between result-ttl">
                                 <h3 className="head">Date of the General Meeting</h3>
                                 <h3 className="head">Voting Result of the General Meeting</h3>
                                 <h3 className="head"></h3>
@@ -66,10 +66,10 @@ function TransactionMenu() {
                                     (data || []).map((res) => {
                                         return (
 
-                                            <div className="border-bottom d-flex justify-content-between pb-3 pt-3">
-                                                <div>{res.month} </div>
-                                                <div>{res.titel}</div>
-                                                <div><FontAwesomeIcon icon={faEye} onClick={() => { window.open("https://cmsapi.choiceindia.com/assets/" + res.view) }} className="cursor-pointer" /></div>
+                                            <div className="border-bottom d-flex justify-content-between pb-3 pt-3 result-item">
+                                                <div className="itm-nm">{res.month} </div>
+                                                <div className="itm-nm">{res.titel}</div>
+                                                <div className="itm-nm"><FontAwesomeIcon icon={faEye} onClick={() => { window.open("https://cmsapi.choiceindia.com/assets/" + res.view) }} className="cursor-pointer" /></div>
                                             </div>
                                         )
                                     })
