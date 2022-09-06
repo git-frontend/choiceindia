cc0e64ea5bb48df94e742c467f379652a8334ccc
krushna.mohanty <krushna.mohanty@choicetechlab.com>
2022-09-06 18:27:43 +0530

    remove extra meta tag

--- src/components/Fables-details/FablesDetails.jsx
+++ src/components/Fables-details/FablesDetails.jsx
@@ -29,22 +29,6 @@ var formName = useRef('');
     setSkeleton(() => false);
   }, 200)
 
-  const [rendercount, setRenderCount] = useState(() => false);
-
-  useEffect(() => {
-    setRenderCount(true)
-    if (rendercount === true) {
-      // let parser = new DOMParser();
-      // let doc = parser.parseFromString(meta_tags['sub-broker'].faqscript, 'text/html');
-      // document.body.appendChild(doc.getElementsByTagName('script')[0]? doc.getElementsByTagName('script')[0]: '' );
-      document.title = meta_tags[location.pathname.replace('/', "")] ? meta_tags[location.pathname.replace('/', "")].title : '';
-      // document.getElementById('meta-tags').name= meta_tags[location.pathname.replace('/',"")]? meta_tags[location.pathname.replace('/',"")].title : ''  ;
-      document.getElementById('meta-tags').content = meta_tags[location.pathname.replace('/', "")] ? meta_tags[location.pathname.replace('/', "")].content : '';
-      document.getElementById('canonical-link').href = meta_tags[location.pathname.replace('/', "")] ? meta_tags[location.pathname.replace('/', "")].link : '';
-    }
-  }, [rendercount])
-
-
     /** to call single fabal detail */
     function getSingleFablesDetail() {
       homeServices.fablesBlog(id).then(
