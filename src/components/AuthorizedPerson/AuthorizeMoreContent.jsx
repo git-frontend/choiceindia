4bf42585adaa8911fb5af9cd6c1bbf272c0bb1c7
trilochan.badal <trilochan.badal@choicetechlab.com>
2022-09-13 18:45:22 +0530

    removed links.

--- src/components/AuthorizedPerson/AuthorizeMoreContent.jsx
+++ src/components/AuthorizedPerson/AuthorizeMoreContent.jsx
@@ -25,8 +25,8 @@ const RemisierMoreContent = () => {
         
         <ul>
             <li>Stock Market Investment</li>
-            <li><a href="/mutual-funds-investment">Mutual Funds</a></li>
-            <li><a href='/insurance'>Insurance</a></li>
+            <li>Mutual Funds</li>
+            <li>Insurance</li>
             <li>Retail Loans</li>
             <li>Portfolio Management Services</li>
         </ul>
