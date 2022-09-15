f7f2b4533dde3328f030808aaafa2ea097923f5e
madhumati <madhumati@choicetechlab.com>
2022-09-15 17:11:58 +0530

    emitra

--- src/components/Partner-Assests/PartnerAssests.jsx
+++ src/components/Partner-Assests/PartnerAssests.jsx
@@ -1,13 +1,19 @@
-import NewsImage0 from '../../assets/images/news-media/what-choice-represents.webp';
-import NewsImage1 from '../../assets/images/news-media/video-play.webp';
-import CareerImage2 from '../../assets/images/news-media/assets.webp';
-import CareerImage3 from '../../assets/images/news-media/save-water.png';
-import CareerImage4 from '../../assets/images/news-media/bit-coin.webp';
-import CareerImage5 from '../../assets/images/news-media/newspaper.webp';
-import CareerImage6 from '../../assets/images/news-media/laptop.webp';
-import bannerimg from '../../assets/images/news-media/banner-img.webp';
+import NewsImage0 from '../../assets/images/choice-emitra/Bike-Kharidne-ka-SIPplan-MFChoice.jpg';
+import CareerImage2 from '../../assets/images/choice-emitra/crore-kaise-kamayein-MFChoice.jpg';
+import CareerImage3 from '../../assets/images/choice-emitra/FD-vs-Stock-ChoiceStock.jpg';
+import CareerImage4 from '../../assets/images/choice-emitra/Mehengai-se-kaise-bachein-StockChoice.jpg';
+import CareerImage5 from '../../assets/images/choice-emitra/power-of-Investing-early-MFChoice.jpg';
+import CareerImage6 from '../../assets/images/choice-emitra/Shares-jo-guna-ho-Sakte-hai-StockChoice.jpg';
+import CareerImage7 from '../../assets/images/choice-emitra/Pdf-Icon-01.png';
+
+
+import whatsapp from '../../assets/images/choice-emitra/whatsapp.svg';
+import instagram from '../../assets/images/choice-emitra/instagram.svg';
+import download from '../../assets/images/choice-emitra/download.svg';
+import fb from '../../assets/images/choice-emitra/fb.svg';
 import { useState,useEffect } from 'react';
 import meta_tags from '../../Data/MetaTags';
+
 import '././partner-assests.scss';
 import LazyLoader from '../Common-features/LazyLoader';
 
@@ -49,7 +55,7 @@ function PartnerAssests() {
 			<div className="container">
 			<div className="row">
 						<div className="col-md-12">
-            <h1 className='text-center mt-5 mb-5 title-first'>Choice Broking Media</h1>
+            <h1 className='text-center mt-5 mb-5 title-first'>ई-मित्र की प्रचार सामग्री</h1>
 						</div>
 					</div>
 			</div>
@@ -58,80 +64,178 @@ function PartnerAssests() {
         <div className='media-banner'>
           <div className="container">
             <div className="row mt-5">
-              <div className="col-md-4 mb3">
+            <div className="col-md-4 mb3">
                 <div className="card">
-                  <div className="youtube-play">
-                    <LazyLoader src={NewsImage0} className={'img-fluid '} width={"400"} height={"400"} alt={""} />
-
-                  </div>
-                  <div className="card-footer">
-                    do you know what choice represents ?
-
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-4 mb3">
-                <div className="card">
-                  <div className="youtube-play">
-                    <LazyLoader src={CareerImage2} className={'img-fluid '} width={"400"} height={"400"} alt={""} />
-
-                  </div>
-                  <div className="card-footer">
-                  Wealth Creation Ideas to Increase Your Assets
-
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-4 mb3">
-                <div className="card">
-                  <div className="youtube-play">
-                    <LazyLoader src={CareerImage3} className={'img-fluid '} width={"400"} height={"400"} alt={""} />
-
-                  </div>
-                  <div className="card-footer">
-                  save water save life
-
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="row mt-7">
-              <div className="col-md-4 mb3">
-                <div className="card">
-                  <div className="youtube-play">
-                    <LazyLoader src={CareerImage4} className={'img-fluid '} width={"400"} height={"400"} alt={""} />
-
-                  </div>
-                  <div className="card-footer">
-                    do you know what choice represents ?
-
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-4 mb3">
-                <div className="card">
-                  <div className="youtube-play">
-                    <LazyLoader src={CareerImage5} className={'img-fluid '} width={"400"} height={"400"} alt={""} />
-
-                  </div>
-                  <div className="card-footer">
-                  Wealth Creation Ideas to Increase Your Assets
-
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-4">
-                <div className="card">
-                  <div className="youtube-play">
+                  <div >
                     <LazyLoader src={CareerImage6} className={'img-fluid '} width={"400"} height={"400"} alt={""} />
 
                   </div>
                   <div className="card-footer">
-                  save water save life
+                  शेयर जो 10 गुना हो सकते है!
 
+                  </div>
+                  <div className='socail-media'>
+                    <div className='d-flex justify-content-between'>
+                  <div><img src={download} className="" alt={"Download"} width="22" height="22" /></div>
+                  <div><img src={whatsapp} className="" alt={"Whatsapp"} width="22" height="22" /></div>
+                  <div><img src={fb} className="" alt={"Facebook"} width="22" height="22" /></div>
+                  <div><img src={instagram} className="" alt={"Instagram"} width="22" height="22" /></div>
+                  </div>
                   </div>
                 </div>
               </div>
+              <div className="col-md-4 mb3">
+                <div className="card">
+                  <div >
+                    <LazyLoader src={CareerImage5} className={'img-fluid '} width={"400"} height={"400"} alt={""} />
+
+                  </div>
+                  <div className="card-footer">
+                  जल्दी निवेश करने के फायदे
+
+                  </div>
+                  <div className='socail-media'>
+                    <div className='d-flex justify-content-between'>
+                  <div><img src={download} className="" alt={"Download"} width="22" height="22" /></div>
+                  <div><img src={whatsapp} className="" alt={"Whatsapp"} width="22" height="22" /></div>
+                  <div><img src={fb} className="" alt={"Facebook"} width="22" height="22" /></div>
+                  <div><img src={instagram} className="" alt={"Instagram"} width="22" height="22" /></div>
+                  </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 mb3">
+                <div className="card">
+                  <div >
+                    <LazyLoader src={CareerImage4} className={'img-fluid '} width={"400"} height={"400"} alt={""} />
+
+                  </div>
+                  <div className="card-footer">
+                  मेहंगाई से कैसे बचे?
+
+                  </div>
+                  <div className='socail-media'>
+                    <div className='d-flex justify-content-between'>
+                  <div><img src={download} className="" alt={"Download"} width="22" height="22" /></div>
+                  <div><img src={whatsapp} className="" alt={"Whatsapp"} width="22" height="22" /></div>
+                  <div><img src={fb} className="" alt={"Facebook"} width="22" height="22" /></div>
+                  <div><img src={instagram} className="" alt={"Instagram"} width="22" height="22" /></div>
+                  </div>
+                  </div>
+                </div>
+              </div>
+           
+            </div>
+            <div className="row mt-7">
+            <div className="col-md-4 mb3">
+                <div className="card">
+                  <div >
+                    <LazyLoader src={CareerImage3} className={'img-fluid '} width={"400"} height={"400"} alt={""} />
+
+                  </div>
+                  <div className="card-footer">
+                  फिक्स्ड डिपॉजिट Vs स्टॉक इन्व्हेस्टमेंट
+
+                  </div>
+                  <div className='socail-media'>
+                    <div className='d-flex justify-content-between'>
+                  <div><img src={download} className="" alt={"Download"} width="22" height="22" /></div>
+                  <div><img src={whatsapp} className="" alt={"Whatsapp"} width="22" height="22" /></div>
+                  <div><img src={fb} className="" alt={"Facebook"} width="22" height="22" /></div>
+                  <div><img src={instagram} className="" alt={"Instagram"} width="22" height="22" /></div>
+                  </div>
+                  </div>
+                </div>
+              </div>
+            <div className="col-md-4 mb3">
+                <div className="card">
+                  <div>
+                    <LazyLoader src={CareerImage7} className={'img-fluid '} width={"400"} height={"400"} alt={""} />
+
+                  </div>
+                  <div className="card-footer">
+                  चॉइस के साथ म्यूचुअल फंड खाता कैसे खोले?
+
+                  </div>
+                  <div className='socail-media'>
+                    <div className='d-flex justify-content-between'>
+                  <div><img src={download} className="" alt={"Download"} width="22" height="22" /></div>
+                  <div><img src={whatsapp} className="" alt={"Whatsapp"} width="22" height="22" /></div>
+                  <div><img src={fb} className="" alt={"Facebook"} width="22" height="22" /></div>
+                  <div><img src={instagram} className="" alt={"Instagram"} width="22" height="22" /></div>
+                  </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 mb3">
+                <div className="card">
+                  <div >
+                  <LazyLoader src={CareerImage7} className={'img-fluid '} width={"400"} height={"400"} alt={""} />
+
+                  </div>
+                  <div className="card-footer">
+                  चॉइस के साथ डीमैट खाता कैसे खोले?
+
+                  </div>
+                  <div className='socail-media'>
+                    <div className='d-flex justify-content-between'>
+                  <div><img src={download} className="" alt={"Download"} width="22" height="22" /></div>
+                  <div><img src={whatsapp} className="" alt={"Whatsapp"} width="22" height="22" /></div>
+                  <div><img src={fb} className="" alt={"Facebook"} width="22" height="22" /></div>
+                  <div><img src={instagram} className="" alt={"Instagram"} width="22" height="22" /></div>
+                  </div>
+                  </div>
+                </div>
+              </div>
+             
+           
+              
+              
+            </div>
+            <div className="row mt-7">
+            <div className="col-md-4 mb3">
+                <div className="card">
+                  <div >
+                    <LazyLoader src={NewsImage0} className={'img-fluid '} width={"400"} height={"400"} alt={""} />
+
+                  </div>
+                  <div className="card-footer">
+                  बाइक ख़रीदने का एसआईपी प्लान
+
+                  </div>
+                  <div className='socail-media'>
+                    <div className='d-flex justify-content-between'>
+                  <div><img src={download} className="" alt={"Download"} width="22" height="22" /></div>
+                  <div><img src={whatsapp} className="" alt={"Whatsapp"} width="22" height="22" /></div>
+                  <div><img src={fb} className="" alt={"Facebook"} width="22" height="22" /></div>
+                  <div><img src={instagram} className="" alt={"Instagram"} width="22" height="22" /></div>
+                  </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 mb3">
+                <div className="card">
+                  <div >
+                    <LazyLoader src={CareerImage2} className={'img-fluid '} width={"400"} height={"400"} alt={""} />
+
+                  </div>
+                  <div className="card-footer">
+                  1 करोड़ कैसे कमाए?
+
+                  </div>
+                  <div className='socail-media'>
+                    <div className='d-flex justify-content-between'>
+                  <div><img src={download} className="" alt={"Download"} width="22" height="22" /></div>
+                  <div><img src={whatsapp} className="" alt={"Whatsapp"} width="22" height="22" /></div>
+                  <div><img src={fb} className="" alt={"Facebook"} width="22" height="22" /></div>
+                  <div><img src={instagram} className="" alt={"Instagram"} width="22" height="22" /></div>
+                  </div>
+                  </div>
+                </div>
+              </div>
+             
+             
+            
             </div>
           </div>
         </div>
