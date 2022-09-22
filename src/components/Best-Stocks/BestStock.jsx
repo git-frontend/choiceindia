28e1288313565fcb1c70d2b99b5d8865d81d2cfc
jayashri <jayashri.yadav@choicetechlab.com>
2022-09-22 18:37:08 +0530

    changes done

--- src/components/Best-Stocks/BestStock.jsx
+++ src/components/Best-Stocks/BestStock.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import "./BestStock";
+import "./BestStock.scss";
 import Template5 from '../Common-features/Template5';
 
 
@@ -60,13 +60,68 @@ function BestStock() {
           <div className="sub-broker-skeleton-parent">
             <section className="mainhead">
               <div className="container">
-                <div className="row ">
-<h1>hello</h1>
+                <div className="row d-flex justify-content-center ">
+                  <div className="col-md-10 ">
+                    <h2 className="title-secnd1">Best Stocks to Buy Today</h2>
+                    <p className="title_para">Choose the best stocks to buy today according to the holding period.</p>
+                  </div>
+                  <div className="col-md-6">
+                    <ul className="list-group list-group-horizontal-sm justify-content-center list_group1">
+                      <li className="list-group-item list">All Stocks</li>
+                      <li className="list-group-item list listsec">Intraday Stocks</li>
+                      <li className="list-group-item list">Short Term Stocks</li>
+                      <li className="list-group-item list"> Long Term Stocks</li>
+                    </ul>
+                    <p className="title_para pt-4">Get the list of best stocks to buy today for intraday trading!</p>
+                  </div>
                 </div>
-
+                <div className="row d-flex justify-content-center">
+                  <div className="col-md-10">
+                    <div className="row">
+                      <div className="col-md-6">
+                        <div className="main-left">
+                          <div className="top-section">
+                            <div className="top-left">
+                              <h6 className="top-text">Reco Date</h6>
+                              <h6 className="top-date">12th Apr '22</h6>
+                            </div>
+                            <div className="top-right"><button className="btn-buy">buy</button></div>
+                          </div>
+                          <div className="middle-section">
+                            <div className="middle-left">
+                              <h4 className="big-text">Bandhan Bank Ltd</h4>
+                              <span className="small-text">BANDHANBANK</span>
+                            </div>
+                            <div className="middle-right">
+                              <span className="right-big-text">333.45</span>
+                              <h6 className="right-small-text">+10.6 (3.28%)</h6>
+                            </div>
+                          </div>
+                          <hr></hr>
+                          <div className="bottom-section">
+                            <div className="row d-flex justify-content-between">
+                              <div className="col-4">
+                              <h6>Entry Price</h6>
+                              <h4 >327.30</h4>
+                              </div>
+                              <div className="col-4 text-center">
+                              <h6>Potential Price</h6>
+                              <h4 >350.00</h4>
+                              </div>
+                              <div className="col-4 text-end">
+                              <h6>Entry Price</h6>
+                              <h4 >4.96%</h4>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="col-md-6">hello</div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </section>
-
           </div>
       }
     </div>
