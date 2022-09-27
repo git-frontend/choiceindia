a12e83a91ac53c8cd6939bc57ba146cae985f1a4
jayashri <jayashri.yadav@choicetechlab.com>
2022-09-27 16:18:34 +0530

    changes done

--- src/components/Best-Stocks/BestStock.jsx
+++ src/components/Best-Stocks/BestStock.jsx
@@ -82,7 +82,7 @@ function BestStock() {
                       <li className="list-group-item list">Short Term Stocks</li>
                       <li className="list-group-item list"> Long Term Stocks</li>
                     </ul> */}
-                    <div className="justify-content-center list_group1 bloc-tabs">
+                    <div className=" list_group1 bloc-tabs">
                       <button
                         className={toggleState === 1 ? "list listsec" : "list"}
                         onClick={() => toggleTab(1)}
@@ -1144,16 +1144,16 @@ function BestStock() {
             <section className="sendopt">
               <div className="container">
                 <div className="form_main">
-                  <div className="demat_text"><span className="form-ttl">Open a Free <span className="reshide"> Demat</span> Account <span className="reshide"><br />+ Free 1st Year AMC</span></span></div>
-                  <div className="Mobile_text">
+                  <div className=" demat_text"><span className="form-ttl">Open a Free <span className="reshide"> Demat</span> Account <span className="reshide"><br />+ Free 1st Year AMC</span></span></div>
+                  <div className="  Mobile_text">
                   <input type="text" id="form-email" className="form-textbox" required/>
                   <label for="form-email" className="form-label">Mobile Number</label>
                   </div>
-                  <div className="cust_check">
+                  <div className="  cust_check">
                     <input type="checkbox" className="form_check" />
                     <label className="form_check_text">I agree that I have read and  accept<br /> the <a href="/"><span className="link_tc">Terms and Conditions</span></a></label>
                   </div>
-                  <div className="sub_group"><a href="/" ><span className="send_OPT_btn" >Send OTP</span></a></div>
+                  <div className=" OPt_item sub_group"><a href="/" ><span className="send_OPT_btn" >Send OTP</span></a></div>
                 </div>
               </div>
             </section>
