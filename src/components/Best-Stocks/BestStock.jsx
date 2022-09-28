9645765428e98e368d408ec1029d20c6c7b00f95
jayashri <jayashri.yadav@choicetechlab.com>
2022-09-28 12:07:10 +0530

    changes done

--- src/components/Best-Stocks/BestStock.jsx
+++ src/components/Best-Stocks/BestStock.jsx
@@ -1147,8 +1147,8 @@ function BestStock() {
                 <div className="form_main">
                   <div className=" demat_text"><span className="form-ttl">Open a Free <span className="reshide"> Demat</span> Account <span className="reshide"><br />+ Free 1st Year AMC</span></span></div>
                   <div className="  Mobile_text">
-                  <input type="text" id="form-email" className="form-textbox" required/>
-                  <label for="form-email" className="form-label">Mobile Number</label>
+                    <input type="text" id="form-email" className="form-textbox" required />
+                    <label for="form-email" className="form-label">Mobile Number</label>
                   </div>
                   <div className="  cust_check">
                     <input type="checkbox" className="form_check" />
@@ -1158,117 +1158,32 @@ function BestStock() {
                 </div>
               </div>
             </section>
-            {/* <section className="Stockes">
+
+            <section className="readmoresection">
+
               <div className="container">
                 <div className="row">
                   <div className="col-md-12">
-                    <h1 className="stock_head">Best Stocks to Buy Today in India</h1>
+                    <h2 className="secttitle text-center">Best Stocks to Buy Today in India</h2>
                   </div>
                   <div className="col-md-12">
-                    <div className="content">
-                      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-                      dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-                      clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
-                      consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-                      sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-                      no se....<label htmlFor="post-1" className="read-more-trigger moreless-button">Read More</label>
+                    <div className="para">
+                      <input type="checkbox" className="read-more-state" id="post-1" />
+                      <span className="read-more-wrap content"> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy 
+                      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et 
+                      justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit 
+                      amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+                       labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea 
+                       rebum. Stet clita kasd gubergren, no se....
+
+                        <span className="read-more-target t">
+                          <p className="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, perspiciatis quia nihil asperiores deserunt suscipit et doloremque aperiam iusto quam in similique, culpa fuga soluta maiores placeat architecto nisi.</p>
+                          <p className="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, perspiciatis quia nihil asperiores deserunt suscipit et doloremque aperiam iusto quam in similique, culpa fuga soluta maiores placeat architecto nisi.</p>
+                        </span></span> <label htmlFor="post-1" className="read-more-trigger moreless-button"></label>
                     </div>
                   </div>
                 </div>
               </div>
-            </section> */}
-
-<section className="readmoresection">
-
-<div className="container">
-  <div className="row">
-    <div className="col-md-12">
-      <h2 className="secttitle text-center">Best Stocks to Buy Today in India</h2>
-    </div>
-    <div className="col-md-12">
-      <div className="para">
-        <input type="checkbox" className="read-more-state" id="post-1" />
-        <span className="read-more-wrap "> The stock market investors can opt to invest via an Authorised person, who is a professional who can guide investors through the process.
-
-          <span className="read-more-target">
-        <p>Authorised persons are those who function as a point of contact between investors and stockbrokers to make the entire investment process easier. They match the investing needs of the investors with the greatest bargains available, allowing them to maximize their earnings.</p>
-        <p>Choice has a diverse product portfolio that is supported by more than 25 years of experience. Authorised Persons have the ability to provide the full range of financial products such as -</p>
-        
-        
-        <ul>
-            <li>Stock Market Investment</li>
-            <li>Mutual Funds</li>
-            <li>Insurance</li>
-            <li>Retail Loans</li>
-            <li>Portfolio Management Services</li>
-        </ul>
-        <h3 className='ttl-para'>Work of An Authorised Person</h3>
-        <p>The most important task of an Authorised person is to become a bridge between the investors and the brokerage firms. An Authorised person's main work responsibilities include -</p>
-        
-        <ul>
-            <li>Buying, selling, and dealing with securities</li>
-            <li>Bringing in new investors</li>
-            <li>Assisting stockbrokers in increasing their business volume</li>
-            <li>Assisting existing clients in reinvesting</li>
-        </ul>
-        <h3 className='ttl-para'>Who can become an Authorised Person</h3>
-        <p>Unlike <Link to="/sub-broker-franchise">sub-brokers</Link>, who are registered entities, the authorised person enjoys the freedom to serve with an approval of SEBI. The <Link to="/blog/difference-between-sub-broker-and-authorized-person/">difference between the sub-broker and authorised person</Link> needs to be specified.</p>
-        <p>However, anyone who wants to earn money in a passive way can become an Authorised Person. With Choice, the benchmarks to become an Authorised person are as follows -</p>
-        <ul>
-            <li>There are no educational requirements.</li>
-            <li>Individuals willing to become Authorised Person must be at least 18 years old.</li>
-            <li>An uncanny ability to communicate and sell in equity markets.</li>
-            <li>A desire to be your own boss is a strong motivator.</li>
-        </ul>
-        <p>Generally speaking, there are no strict criteria for being an Authorised Person in the Stock Market. Following entities can be designated as an Authorised person:</p>
-        <ul>
-                                            <li>Any person – individual, partnership firm, LLP, or body corporate – who is appointed by <Link to="/">stock broker</Link> / trading member and who provides access to the Exchange's trading platform as the stock broker's agent</li>
-          <li>The eligibility and qualifying requirements are minimal; all we want is willingness and passion.</li>
-          <li>Excellent Communication Skills and Sales Aptitudes are an added benefit.</li>
-        </ul>
-        <h3 className='ttl-para'>How To Become An Authorised Person</h3>
-        <ul>
-          <li>In general, becoming an Authorised person in the stock market is a straightforward procedure. The only difficult aspect of the application is the verification process.</li>
-          <li>Go to our official website to start registration to become a Choice Authorised Person.</li>
-          <li>Feel in your details and share required documents with us to complete your registration.</li>
-          <li>After a quick verification by our Choice India expert executives, your registration gets approved.</li>
-          <li>The entire registration process is overseen by the authorities. The applicant must have all of the necessary documentation and qualifications for this position.</li>
-        </ul>
-        <h3 className='ttl-para'>Benefits Of Becoming An Authorised Person</h3>
-        <p>You can join the well-known and respected Choice group as a SEBI Authorised Person. Join our reputable brand and enjoy the following advantages –</p>
-        <ul>
-            <li>
-            High Incentives
-                <ol>
-                    <li>Highest Revenue Sharing</li>
-                    <li>Up to ₹ 1000 Per A/C Opening Incentive</li>
-                    <li>No Registration Charges</li>
-                    <li>Earn For A Lifetime</li>
-                </ol>
-            </li>
-            <li>Transparent and Growth-Oriented Business Model</li>
-            <li>
-            Advanced Technical Assistance
-                <ol>
-                    <li>Cutting-edge Trading Platforms (<Link to="/online-trading-app">Jiffy Trading App</Link> &amp; Advanced Website)</li>
-                    <li>Hassle-Free Software for Tracking and Monthly Reports</li>
-                    <li>Dedicated Whatsapp and Telegram Channels</li>
-                    <li>Training through Webinars and Conferences</li>
-                </ol>
-            </li>
-            <li>
-            Support
-                <ol>
-                    <li>Marketing and Sales Support</li>
-                    <li>Local Relationship Manager Assistance</li>
-                </ol>
-            </li>
-        </ul>
-          </span></span> <label htmlFor="post-1" className="read-more-trigger moreless-button"></label>
-      </div>
-    </div>
-  </div>
-</div>
 
             </section>
 
