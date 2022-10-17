ee2d75b87c2a9a28c8a67f230fedf278503d4db1
Jagruti <jagruti.palkar@choicetechlab.com>
2022-10-17 14:49:52 +0530

    changes

--- src/components/Goverment-advisory/Service.jsx
+++ src/components/Goverment-advisory/Service.jsx
@@ -8,6 +8,10 @@ import partnertwo from '../../assets/images/goverment-advisory/partnertwo.svg';
 import partnerthree from '../../assets/images/goverment-advisory/partnerthree.svg';
 import partnerfour from '../../assets/images/goverment-advisory/partnerfour.svg';
 import partnerfive from '../../assets/images/goverment-advisory/partnerfive.svg';
+import socialpartnerone from '../../assets/images/goverment-advisory/socialpartnerone.svg';
+import socialpartnertwo from '../../assets/images/goverment-advisory/socialpartnertwo.svg';
+import socialpartnerthree from '../../assets/images/goverment-advisory/socialpartnerthree.svg';
+import socialpartnerfour from '../../assets/images/goverment-advisory/socialpartnerfour.svg';
 import socialsectorone from '../../assets/images/goverment-advisory/tribal-welfare.webp';
 import socialsectortwo from '../../assets/images/goverment-advisory/health.webp';
 import socialsectorthree from '../../assets/images/goverment-advisory/agriculture-allied.webp';
@@ -94,7 +98,35 @@ function Service() {
     ],
   };
   
-
+  const settings2 = {
+    infinite: true,
+    speed: 1500,
+    arrows: false,
+    slidesToShow: 4,
+    autoplay: false,
+    margin: 15,
+    dots: false,
+    autoplaySpeed: 1000,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          adaptiveHeight: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  
 
   return (
     <div>
@@ -249,27 +281,27 @@ function Service() {
 
                     <div className="image_wrapper">
                       <div className="img-item">
-                        <LazyLoader src={partnerone} className={'img-fluid'} width={"96"} height={"95"} alt={"Niti Aayog"} />
+                        <LazyLoader src={partnerone} className={'img-fluid'} width={"96"} height={"120"} alt={"Niti Aayog"} />
                       </div>
                     </div>
                     <div className="image_wrapper">
                       <div className="img-item">
-                        <LazyLoader src={partnerfour} className={'img-fluid'} width={'96'} height={'95'} alt={"Navkar Corporation"} />
+                        <LazyLoader src={partnerfour} className={'img-fluid'} width={'96'} height={'120'} alt={"Navkar Corporation"} />
                       </div>
                     </div>
                     <div className="image_wrapper">
                       <div className="img-item">
-                        <LazyLoader src={partnerfive} className={'img-fluid'} width={'172'} height={'95'} alt={"ICICI"} />
+                        <LazyLoader src={partnerfive} className={'img-fluid'} width={'172'} height={'120'} alt={"ICICI"} />
                       </div>
                     </div>
                     <div className="image_wrapper">
                       <div className="img-item">
-                        <LazyLoader src={partnertwo} className={'img-fluid'} width={'96'} height={'95'} alt={"Liberty Insurance"} />
+                        <LazyLoader src={partnertwo} className={'img-fluid'} width={'239'} height={'120'} alt={"Liberty Insurance"} />
                       </div>
                     </div>
                     <div className="image_wrapper">
                       <div className="img-item">
-                        <LazyLoader src={partnerthree} className={'img-fluid'} width={'172'} height={'92'} alt={"Aadhaar"} />
+                        <LazyLoader src={partnerthree} className={'img-fluid'} width={'278'} height={'120'} alt={"Aadhaar"} />
                       </div>
                     </div>
 
@@ -299,8 +331,8 @@ function Service() {
           
               <div className="wrapper">
                   {/* <div className={"hexagonwrap"+ count===0 ? "active":"" } onClick={() => { chapterScroll('tribalwelfare'); setcount(0)}}> */}
-                <div className="hexagonwrap"> 
-                    <LazyLoader src={socialsectorone} className={'img-fluid'} width={'212'} height={'240'} alt={"Tribal Welfare"} />
+                <div className="hexagonwrap" onClick={() => { chapterScroll('tribalwelfare') }}> 
+                    <LazyLoader src={socialsectorone} className={'img-fluid'}  width={'212'} height={'240'} alt={"Tribal Welfare"} />
                     <p className="text">Tribal Welfare</p>
                   </div>
                   {/* <div className={"hexagonwrap"+ count===1 ? "active":"" } onClick={() => { chapterScroll('health'); setcount(1)}}> */}
@@ -410,33 +442,33 @@ function Service() {
                 </div>
                 <div className="right-img">
 
-                  <Slider {...settings1} id="clientslider">
+                  <Slider {...settings2} id="clientslider">
 
                     <div className="image_wrapper">
                       <div className="img-item">
-                        <LazyLoader src={partnerone} className={'img-fluid'} width={"96"} height={"95"} alt={"Niti Aayog"} />
+                        <LazyLoader src={socialpartnerone} className={'img-fluid'} width={"254"} height={"120"} alt={"Niti Aayog"} />
                       </div>
                     </div>
                     <div className="image_wrapper">
                       <div className="img-item">
-                        <LazyLoader src={partnerfour} className={'img-fluid'} width={'96'} height={'95'} alt={"Navkar Corporation"} />
+                        <LazyLoader src={socialpartnertwo} className={'img-fluid'} width={'188'} height={'120'} alt={"Navkar Corporation"} />
                       </div>
                     </div>
                     <div className="image_wrapper">
                       <div className="img-item">
-                        <LazyLoader src={partnerfive} className={'img-fluid'} width={'172'} height={'95'} alt={"ICICI"} />
+                        <LazyLoader src={socialpartnerthree} className={'img-fluid'} width={'263'} height={'120'} alt={"ICICI"} />
                       </div>
                     </div>
                     <div className="image_wrapper">
                       <div className="img-item">
-                        <LazyLoader src={partnertwo} className={'img-fluid'} width={'96'} height={'95'} alt={"Liberty Insurance"} />
+                        <LazyLoader src={socialpartnerfour} className={'img-fluid'} width={'249'} height={'120'} alt={"Liberty Insurance"} />
                       </div>
                     </div>
-                    <div className="image_wrapper">
+                    {/* <div className="image_wrapper">
                       <div className="img-item">
                         <LazyLoader src={partnerthree} className={'img-fluid'} width={'172'} height={'92'} alt={"Aadhaar"} />
                       </div>
-                    </div>
+                    </div> */}
 
 
                   </Slider>
@@ -451,7 +483,7 @@ function Service() {
         </div>
 
       </div>
-      <div id="health" className="sectordetail">
+      {/* <div id="health" className="sectordetail">
         <div className="container">
 
           <div className="row">
@@ -575,29 +607,9 @@ function Service() {
 
         </div>
 
-      </div>
+      </div> */}
 
-      {/* <div id="wealth">
-			<WealthManagement1 />
-			</div>
-			<div id="insurance">
-			<Insurance1/>
-			</div>
-			<div id="loan">
-			<Loans1 />
-			</div>
-			<div id="capital">
-			<CapitalAdvisory1 />
-			</div>
-			<div id="management">
-			<ManagementConsultancy1 />
-			</div>
-			<div id="goverment">
-			<GovernmentAdvisory1 />
-			</div>
-			<div id="tax">
-			<TaxAdvisory1 />
-			</div> */}
+     
 
 
 
