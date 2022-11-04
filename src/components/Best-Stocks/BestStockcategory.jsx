a155629eeef4305848205e715e011e5054f6c55f
jayashri <jayashri.yadav@choicetechlab.com>
2022-11-04 18:31:43 +0530

    session id

--- src/components/Best-Stocks/BestStockcategory.jsx
+++ src/components/Best-Stocks/BestStockcategory.jsx
@@ -20,53 +20,54 @@ import meta_tags from "../../Data/MetaTags";
 import { useEffect } from "react";
 function BestStockcategory() {
   const [toggleState, setToggleState] = useState(1);
-  const [list,setlist]= useState();
+  const [list, setlist] = useState();
   const toggleTab = (index) => {
     setToggleState(index);
   };
 
   const [skeleton, setSkeleton] = useState(() => true);
   const [trigger, setTrigger] = useState(false)
-    /**Show loader */
-    const [showLoader, setShowLoader] = useState(false)
+  const [data, setData] = useState(false);
+  /**Show loader */
+  const [showLoader, setShowLoader] = useState(false)
 
   useEffect(() => {
     setTrigger(true)
     if (trigger === true) {
 
-        generateSessionId()
+      generateSessionId()
     }
 
-}, [trigger])
+  }, [trigger])
 
 
-/**
- * Generate Session Id
- */
-function generateSessionId() {
+  /**
+   * Generate Session Id
+   */
+  function generateSessionId() {
     setShowLoader(true)
     let api = new API_URLS()
     fetch(api.getSessionUrl())
-        .then(response => {
-            return response.json();
-        })
-        .then(res => {
-            if (res.Status == 'Success') {
-              IntraStocks(res.Response)
-            } else {
-              IntraStocks()
-            }
+      .then(response => {
+        return response.json();
+      })
+      .then(res => {
+        if (res.Status == 'Success') {
+          // IntraStocks(res.Response)
+        } else {
+          // IntraStocks([])
+        }
 
-        }, err => {
-          IntraStocks()
-        })
+      }, err => {
+        // IntraStocks([])
+      })
 
-}
+  }
 
 
-  function LongTermStocks(){
-   
-    let request ={
+  function LongTermStocks() {
+
+    let request = {
       "end_date": "",
       "is_expert": 0,
       "research_type": "Medium To Long Term",
@@ -83,99 +84,102 @@ function generateSessionId() {
       "category_id": 1
 
     }
-    rest.expertReportData(request).then(      
-    res => {
-      if (res) {
-       
-        setlist(res.response);
-        console.log(res);
+    rest.expertReportData(request).then(
+      res => {
+        if (res) {
 
-      } else {
-       
-        setlist([]);
+          setlist(res.response);
+          console.log(res);
+
+        } else {
+
+          setlist([]);
+        }
+
+
       }
+    ).catch((error) => {
 
+      setlist([]);
+    });
+  };
 
-    }
-  ).catch((error) => {
-  
+  function ShortTermStocks() {
+    setData(true)
     setlist([]);
-  });
-};
+    let request = {
+      "end_date": "2022-11-01",
+      "is_expert": 0,
+      "research_type": "",
+      "limit": 10,
+      "offset": 0,
+      "segment": "EQ",
+      "start_date": "2021-11-01",
+      "status": "target_achieved",
+      "subcategory_id": "",
+      "search": "",
+      "id": "",
+      "user_id": "",
+      "timeline_enabled": 1,
+      "category_id": 2
+    }
+    rest.expertReportData(request).then(
+     
+      res => {
+        if (res) {
+          setData(false)
+          setlist(res.response.response);
+          console.log(res.response.response);
 
-function ShortTermStocks(){
 
-  let request ={
-    "end_date": "2022-11-01",
-    "is_expert": 0,
-    "research_type": "",
-    "limit": 10,
-    "offset": 0,
-    "segment": "EQ",
-    "start_date": "2021-11-01",
-    "status": "target_achieved",
-    "subcategory_id": "",
-    "search": "",
-    "id": "",
-    "user_id": "",
-    "timeline_enabled": 1,
-    "category_id": 2
-  }
-  rest.expertReportData(request).then(      
-    res => {
-      if (res) {
-       
-        setlist(res.response);
-        console.log(res);
+        } else {
+          setData(false)
+          setlist([]);
+        }
 
-      } else {
-       
-        setlist([]);
+
       }
-
-
-    }
-  ).catch((error) => {
-  
-    setlist([]);
-  });
-}
-
-
-let IntraStocks = (session) => {
-  setShowLoader(true)
-  let request ={
-    "Count": 10,
-    "endDate": utils.formatDate(new Date(), "dd-MM-yyyy"),
-    "SessionId": session,
-    "Start": 0,
-    "startDate": utils.formatDate(new Date(new Date().setFullYear(new Date().getFullYear() - 1)), "dd-MM-yyyy"),
-    "status": "T1",
-    "type": "EQ",
-    "UserId": "guest",
-    "search": ""
+    ).catch((error) => {
+      setData(false)
+      setlist([]);
+    });
   }
-  rest.signalReportData(request).then(      
-    res => {
+
+
+  let IntraStocks = (session) => {
+    setShowLoader(true)
+    let request = {
+      "Count": 10,
+      "endDate": utils.formatDate(new Date(), "dd-MM-yyyy"),
+      "SessionId": session,
+      "Start": 0,
+      "startDate": utils.formatDate(new Date(new Date().setFullYear(new Date().getFullYear() - 1)), "dd-MM-yyyy"),
+      "status": "T1",
+      "type": "EQ",
+      "UserId": "guest",
+      "search": ""
+    }
+    rest.signalReportData(request).then(
+      res => {
+        setShowLoader(false)
+        if (res) {
+
+          setlist(res.response);
+          console.log(res);
+
+        } else {
+
+          setlist([]);
+        }
+
+
+      }
+    ).catch((error) => {
       setShowLoader(false)
-      if (res) {
-       
-        setlist(res.response);
-        console.log(res);
+      setlist([]);
+    });
+  }
 
-      } else {
-       
-        setlist([]);
-      }
-
-
-    }
-  ).catch((error) => {
-    setShowLoader(false)
-    setlist([]);
-  });
- }
-  
 
 
 
@@ -201,9 +205,8 @@ let IntraStocks = (session) => {
       // document.getElementById('meta-tags').name= meta_tags[location.pathname.replace('/',"")]? meta_tags[location.pathname.replace('/',"")].title : ''  ;
       document.getElementById('meta-tags').content = meta_tags[location.pathname.replace('/', "")] ? meta_tags[location.pathname.replace('/', "")].content : '';
       document.getElementById('canonical-link').href = meta_tags[location.pathname.replace('/', "")] ? meta_tags[location.pathname.replace('/', "")].link : '';
-      LongTermStocks();
-      ShortTermStocks();
-      IntraStocks();
+
+      ShortTermStocks()
       generateSessionId();
     }
   }, [rendercount])
@@ -231,12 +234,12 @@ let IntraStocks = (session) => {
                     <p className="title_para res_para ">Get the list of best stocks to buy today for intraday trading!</p>
                   </div>
                   <div className="col-xl-8 col-md-12">
-                      <ul className="list-group list-group-horizontal list_group1">
-                        <li className="list-group-item list">All Stocks</li>
-                        <li className={toggleState === 1 ?"list-group-item list listsec ":"list-group-item list"} onClick={() =>{IntraStocks();generateSessionId();toggleTab(1)}}>Intraday Stocks</li>
-                        <li className={toggleState === 2 ?"list-group-item list listsec ":"list-group-item list"} onClick={() =>{ShortTermStocks();toggleTab(2)}}>Short Term Stocks</li>
-                        <li className={toggleState === 3 ?"list-group-item list listsec ":"list-group-item list"} onClick={() =>{LongTermStocks();toggleTab(3)}}>Long Term Stocks</li>
-                      </ul>
+                    <ul className="list-group list-group-horizontal list_group1">
+                      <li className="list-group-item list">All Stocks</li>
+                      <li className={toggleState === 1 ? "list-group-item list listsec curs " : "list-group-item list"} onClick={() => { IntraStocks(); toggleTab(1) }}>Intraday Stocks</li>
+                      <li className={toggleState === 2 ? "list-group-item list listsec " : "list-group-item list"} onClick={() => { ShortTermStocks(); toggleTab(2) }}>Short Term Stocks</li>
+                      <li className={toggleState === 3 ? "list-group-item list listsec " : "list-group-item list"} onClick={() => { LongTermStocks(); toggleTab(3) }}>Long Term Stocks</li>
+                    </ul>
                   </div>
                 </div>
               </div>
@@ -244,49 +247,101 @@ let IntraStocks = (session) => {
 
             <section className="main">
               <div className="container">
-                <div className="content-tabs best-stock-tabs-cont">
+                <div className="content-tabs best-stock-tabs-cont active-content">
 
-                    <div className="row d-flex justify-content-center">
+                  {
+                    (list || []).slice(0, 4).map((res, i) => {
+
+                      return (
+                        // <div className="row d-flex justify-content-center">
+                        //   <div className="col-md-12">
+                          //   <div className="row gx-5">
+                          //     <div className="col-md-6">
+                          //       <div className="main-left">
+                          //         <div className="top-section">
+                          //           <div className="top-left">
+                          //             <h6 className="top-text">Reco Date</h6>
+                          //             <h6 className="top-date">12th Apr '22</h6>
+                          //           </div>
+                          //           <div className="top-right"><button className="btn-buy">buy</button></div>
+                          //         </div>
+                          //         <div className="middle-section">
+                          //           <div className="middle-left">
+                          //             <h4 className="big-text">Bandhan Bank Ltd</h4>
+                          //             <span className="small-text">BANDHANBANK</span>
+                          //           </div>
+                          //           <div className="middle-right">
+                          //             <span className="right-big-text">333.45</span>
+                          //             <h6 className="right-small-text">+10.6 (3.28%)</h6>
+                          //           </div>
+                          //         </div>
+
+                          //         <div className="bottom-section">
+                          //           <div className="d-flex justify-content-between pt-3">
+                          //             <div className="bottom">
+                          //               <h6 className="bottom_small_text">Entry Price</h6>
+                          //               <h4 className="bottom_big_text">327.30</h4>
+                          //             </div>
+                          //             <div className="bottom">
+                          //               <h6 className="bottom_small_text">Potential Price</h6>
+                          //               <h4 className="bottom_big_text" >350.00</h4>
+                          //             </div>
+                          //             <div className="bottom">
+                          //               <h6 className="bottom_small_text">Exp. Returns</h6>
+                          //               <h4 className="bottom_big_text">4.96%</h4>
+                          //             </div>
+                          //           </div>
+                          //         </div>
+                          //       </div>
+                          //     </div>
+                          //     {/* <div className="col-md-6">
+                          //   <div className="main-left">
+                          //     <div className="top-section">
+                          //       <div className="top-left">
+                          //         <h6 className="top-text">Reco Date</h6>
+                          //         <h6 className="top-date">12th Apr '22</h6>
+                          //       </div>
+                          //       <div className="top-right"><button className="btn-buy">buy</button></div>
+                          //     </div>
+                          //     <div className="middle-section">
+                          //       <div className="middle-left">
+                          //         <h4 className="big-text">JSW Steel Ltd.</h4>
+                          //         <span className="small-text">BANDHANBANK</span>
+                          //       </div>
+                          //       <div className="middle-right">
+                          //         <span className="right-big-text">755.90</span>
+                          //         <h6 className="right-small-text text_color">-11.5 (1.50%)</h6>
+                          //       </div>
+                          //     </div>
+
+                          //     <div className="bottom-section">
+                          //       <div className="d-flex justify-content-between pt-3">
+                          //         <div className="bottom">
+                          //           <h6 className="bottom_small_text">Entry Price</h6>
+                          //           <h4 className="bottom_big_text">757.00</h4>
+                          //         </div>
+                          //         <div className="bottom">
+                          //           <h6 className="bottom_small_text">Potential Price</h6>
+                          //           <h4 className="bottom_big_text" >810.00</h4>
+                          //         </div>
+                          //         <div className="bottom">
+                          //           <h6 className="bottom_small_text">Exp. Returns</h6>
+                          //           <h4 className="bottom_big_text">7.16%</h4>
+                          //         </div>
+                          //       </div>
+                          //     </div>
+                          //   </div>
+                          // </div> */}
+                          //   </div>
+                        //   </div>
+                        // </div>
+                        <div></div>
+                      
+                    )}
+                    )}
+                  {/* <div className="row d-flex justify-content-center">
                       <div className="col-md-12">
                         <div className="row gx-5">
-                          <div className="col-md-6">
-                            <div className="main-left">
-                              <div className="top-section">
-                                <div className="top-left">
-                                  <h6 className="top-text">Reco Date</h6>
-                                  <h6 className="top-date">12th Apr '22</h6>
-                                </div>
-                                <div className="top-right"><button className="btn-buy">buy</button></div>
-                              </div>
-                              <div className="middle-section">
-                                <div className="middle-left">
-                                  <h4 className="big-text">Bandhan Bank Ltd</h4>
-                                  <span className="small-text">BANDHANBANK</span>
-                                </div>
-                                <div className="middle-right">
-                                  <span className="right-big-text">333.45</span>
-                                  <h6 className="right-small-text">+10.6 (3.28%)</h6>
-                                </div>
-                              </div>
-
-                              <div className="bottom-section">
-                                <div className="d-flex justify-content-between pt-3">
-                                  <div className="bottom">
-                                    <h6 className="bottom_small_text">Entry Price</h6>
-                                    <h4 className="bottom_big_text">327.30</h4>
-                                  </div>
-                                  <div className="bottom">
-                                    <h6 className="bottom_small_text">Potential Price</h6>
-                                    <h4 className="bottom_big_text" >350.00</h4>
-                                  </div>
-                                  <div className="bottom">
-                                    <h6 className="bottom_small_text">Exp. Returns</h6>
-                                    <h4 className="bottom_big_text">4.96%</h4>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
                           <div className="col-md-6">
                             <div className="main-left">
                               <div className="top-section">
@@ -325,50 +380,6 @@ let IntraStocks = (session) => {
                               </div>
                             </div>
                           </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="row d-flex justify-content-center">
-                      <div className="col-md-12">
-                        <div className="row gx-5">
-                          <div className="col-md-6">
-                            <div className="main-left">
-                              <div className="top-section">
-                                <div className="top-left">
-                                  <h6 className="top-text">Reco Date</h6>
-                                  <h6 className="top-date">12th Apr '22</h6>
-                                </div>
-                                <div className="top-right"><button className="btn-buy">buy</button></div>
-                              </div>
-                              <div className="middle-section">
-                                <div className="middle-left">
-                                  <h4 className="big-text">JSW Steel Ltd.</h4>
-                                  <span className="small-text">BANDHANBANK</span>
-                                </div>
-                                <div className="middle-right">
-                                  <span className="right-big-text">755.90</span>
-                                  <h6 className="right-small-text text_color">-11.5 (1.50%)</h6>
-                                </div>
-                              </div>
-
-                              <div className="bottom-section">
-                                <div className="d-flex justify-content-between pt-3">
-                                  <div className="bottom">
-                                    <h6 className="bottom_small_text">Entry Price</h6>
-                                    <h4 className="bottom_big_text">757.00</h4>
-                                  </div>
-                                  <div className="bottom">
-                                    <h6 className="bottom_small_text">Potential Price</h6>
-                                    <h4 className="bottom_big_text" >810.00</h4>
-                                  </div>
-                                  <div className="bottom">
-                                    <h6 className="bottom_small_text">Exp. Returns</h6>
-                                    <h4 className="bottom_big_text">7.16%</h4>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
                           <div className="col-md-6">
                             <div className="main-left">
                               <div className="top-section">
@@ -409,7 +420,7 @@ let IntraStocks = (session) => {
                           </div>
                         </div>
                       </div>
-                    </div>
+                    </div> */}
                 </div>
               </div>
             </section>
