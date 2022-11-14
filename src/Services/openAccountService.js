94dab8868cf9caf489fc67468f2681fcceb3addb
krushna.mohanty <krushna.mohanty@choicetechlab.com>
2022-11-14 11:59:47 +0530

    changes done

--- src/Services/openAccountService.js
+++ src/Services/openAccountService.js
@@ -22,22 +22,22 @@ const openAccountService = {
 
   sendOTP: function (request,type) {
     let url =((type=='MF')? apiURL.getSendInvesOTPURL() : apiURL.getSendOTPURL());
-    return axios.post(url, request, headers);
+    return ((type=='MF')? axios.post(url, request,{ headers: newheaders } ):axios.post(url, request, headers))
   },
 
   resendOTPAgain: function (request,type) {
     let url = ((type=='MF')? apiURL.getResendInvestOTPURL() : apiURL.getResendOTPURL()); 
-    return axios.post(url, request, headers);
+    return ((type=='MF')? axios.post(url, request,{ headers: newheaders } ):axios.post(url, request, headers))
   },
 
   OTPOnCall: function (request,type) {
     let url = ((type=='MF')? apiURL.getOTPInvesOnCallURL() : apiURL.getOTPOnCallURL()); 
-    return axios.post(url, request, headers);
+    return ((type=='MF')? axios.post(url, request,{ headers: newheaders } ):axios.post(url, request, headers));
   },
 
   verifyOTP: function (request,type) {
     let url = ((type=='MF')? apiURL.getVerifyInvestOTPURL() : apiURL.getVerifyOTPURL()); 
-    return axios.post(url, request,{ headers: newheaders } );
+     return  axios.post(url, request,{ headers: newheaders } );
   },
 
   addNewLead: function (request) {
