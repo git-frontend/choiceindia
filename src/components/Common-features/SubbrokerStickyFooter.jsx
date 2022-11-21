7734de65a110ba5e5dc55780c9e3699e4b2726c8
krushna.mohanty <krushna.mohanty@choicetechlab.com>
2022-11-21 18:52:54 +0530

    change

--- src/components/Common-features/SubbrokerStickyFooter.jsx
+++ src/components/Common-features/SubbrokerStickyFooter.jsx
@@ -4,7 +4,7 @@ import Modal from 'react-bootstrap/Modal';
 import './Subbrokerpopupform.jsx';
 import utils from '../../Services/utils';
 
-function SubbrokerStickyFooter({Subbrokerpopupform}) {
+function SubbrokerStickyFooter({Subbrokerpopupform,openInfoPopup}) {
   
     function scrollToId(id) {
         console.log("check123")
@@ -22,7 +22,7 @@ function SubbrokerStickyFooter({Subbrokerpopupform}) {
     return (
         <>
             <div className="d-flex justify-content-center btn-view-more-sticky  mt-5 btn-fixed">
-                <button className=" primary-orange-btn scroll-top-account openbtn"  >Become a Sub Broker</button>
+                <button className=" primary-orange-btn scroll-top-account openbtn"onClick={openDemateAccountPopup}  >Become a Sub Broker</button>
             </div>
             <section className="stickybottom">
                 <div className="container mx-auto">
