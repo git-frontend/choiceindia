513414ff34345b37a8f5f109a338611e61829bae
SWAPNIL NEVAREKAR <swapnil.nevarekar@choicetechlab.com>
2022-11-23 18:34:57 +0530

    nbfc page

--- src/components/Invoice-Financing/InvoiceFinancing.jsx
+++ src/components/Invoice-Financing/InvoiceFinancing.jsx
@@ -2,11 +2,15 @@
 import React from "react";
 import "./inv-financing.scss";
 import InvoiceFinancingSec from './InvoiceFinancingSec.jsx'
+import InvoiceFinancingBanner from './InvoiceFinancingBanner.jsx'
+import StepsSlider from './StepsSlider.jsx'
 
 function InvoiceFinancing() {
   return (
     <div>
+        <InvoiceFinancingBanner/>
         <InvoiceFinancingSec/>
+        <StepsSlider/>
     </div>
   );
 }
