01966679e8ead3eec7bbd2e3ab27fe01793e8037
SWAPNIL NEVAREKAR <swapnil.nevarekar@choicetechlab.com>
2022-11-28 12:42:38 +0530

    chages done in nbfc

--- src/components/NBFCvehicleLoan/FlexitabVehLoan.jsx
+++ src/components/NBFCvehicleLoan/FlexitabVehLoan.jsx
@@ -5,9 +5,9 @@ import Slider from 'react-slick';
 import LazyLoader from "../Common-features/LazyLoader";
 import time from '../../assets/images/nbfc-indivial-loan/time.svg';
 import disburse from '../../assets/images/nbfc-indivial-loan/get-loan-to-disburse.svg';
-import paperwork from '../../assets/images/nbfc-indivial-loan/paperwork.webp';
+import paperwork from '../../assets/images/nbfc-indivial-loan/paperwork.svg';
 import fee from '../../assets/images/nbfc-indivial-loan/processing-fee.svg';
-function flexitab() {
+function FlexitabVehLoan() {
 
     const settings1 = {
         infinite: true,
@@ -114,4 +114,4 @@ function flexitab() {
     );
 }
 
-export default flexitab;
+export default FlexitabVehLoan;
