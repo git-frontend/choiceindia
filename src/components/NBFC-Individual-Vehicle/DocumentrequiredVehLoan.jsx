df3dbfa61ca938856f779a901a38720a3cf943c2
Jagruti <jagruti.palkar@choicetechlab.com>
2022-12-13 10:37:36 +0530

    chnge

--- src/components/NBFC-Individual-Vehicle/DocumentrequiredVehLoan.jsx
+++ src/components/NBFC-Individual-Vehicle/DocumentrequiredVehLoan.jsx
@@ -36,9 +36,9 @@ function Documentrequired() {
                   <h3 className="title-doc text-center ">KYC</h3>
                 
                 <ul className="list-item">
-                  <li >ID Proof</li>
-                  <li>Address Proof</li>
-                  <li>Pan Card/Form 60</li>
+                  <li >Aadhar Card</li>
+                  <li>Pan Card</li>
+                  <li>Driving Licence</li>
                   <li>Income Proof</li>
                 </ul>
               </div>
