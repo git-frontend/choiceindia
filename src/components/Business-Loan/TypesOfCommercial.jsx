754282011d6f4dd6259b7bafa24c52414c3ff935
SWAPNIL NEVAREKAR <swapnil.nevarekar@choicetechlab.com>
2022-12-13 11:24:39 +0530

    changes done in nbfc pages

--- src/components/Business-Loan/TypesOfCommercial.jsx
+++ src/components/Business-Loan/TypesOfCommercial.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import LazyLoader from "../Common-features/LazyLoader";
 import commercialvehicle from '../../assets/images/business-loan/commercial-vehicle.webp';
-
+import { Link } from "react-router-dom";
 function TypesOfCommercial() {
   const [show, setshow]=useState(false);
   return (
@@ -16,7 +16,7 @@ function TypesOfCommercial() {
                 <h2 className="title-secnd">Commercial Vehicle Loan</h2>
                 <p>Are you struggling for days and still unable to find a way to get your desired vehicle financed even after multiple attempts? Do{!show ? <span onClick={()=>{setshow(true);console.log("hhh")}}>... <em className="read-ext">Read more</em></span>:""}{show ?<span>&nbsp;not worry! <br/>
 Your search can stop right here with us. We are a customer-oriented organization where we treat our customers like family. We at Choice Finserv offer affordable and budget-friendly vehicle loans making sure that the wheels of your growth keep moving.<span onClick={()=>{setshow(false);console.log("hhh")}}>&nbsp;<em className="read-ext">Read less</em></span></span>:""}</p>
-                <a href="#"><span class="btn-bg">Know more</span></a>
+                <Link to="/commercial-vehicle-loan"><span class="btn-bg">Know more</span></Link>
               </div>
             </div>
             <div className="col-md-6 d-flex justify-content-end">
