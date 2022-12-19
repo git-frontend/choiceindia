399895f1ec3f441e033a6b669b807a3cd6d735b2
satyavendra singh <satyavendra.singh@choicetechlab.com>
2022-12-19 18:23:13 +0530

    Added Popup on About Us page

--- src/components/About-us/Team.jsx
+++ src/components/About-us/Team.jsx
@@ -1,6 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import Modal from 'react-bootstrap/Modal';
+import { Accordion, Button } from "react-bootstrap";
 
 
+import { faClock, faLocationDot, faPhone, faEnvelope, faHeart, faClose, faHeadphones } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import image1 from '../../assets/images/about-us/vinita-patodia.webp';
 import image2 from '../../assets/images/about-us/kamal-poddar.webp';
 import image3 from '../../assets/images/about-us/suyash-patodia.webp';
@@ -11,18 +15,23 @@ import image7 from '../../assets/images/about-us/akthakur.webp';
 import image8 from '../../assets/images/about-us/sudha-bhushan.webp';
 import image9 from '../../assets/images/about-us/sandeep-singh.webp';
 import image10 from '../../assets/images/about-us/kanhaiyalal-beriwal.webp';
+import image11 from '../../assets/images/about-us/kamal-poddar-2.webp';
 
 import Slider from "react-slick";
 import LazyLoader from '../Common-features/LazyLoader';
+import BoardOfDirector from '../../Data/Strategies';
 
 function Team() {
-  const [check, setcheck] = useState(false);
+  const [value, setValue] = useState(0);
+  const[IsShown2,setIsShown2]= useState(false)
+  const[IsShown3,setIsShown3]= useState()
+
   const settings = {
     infinite: true,
     speed: 2000,
     arrows: false,
     slidesToShow: 4,
-    autoplay: true,
+    autoplay: false,
     margin: 15,
     dots: true,
     autoplaySpeed: 800,
@@ -46,6 +55,12 @@ function Team() {
       },
     ],
   };
+
+function closesection(){
+  setIsShown2(false)
+  console.log("check",IsShown2);
+}
+
   return (
     <div>
 
@@ -60,70 +75,49 @@ function Team() {
             <div className="col-md-12">
               <div className="team-list">
                 <Slider {...settings} className="team-list-slider">
-                  <div className="">
-                    <div onClick={() => { setcheck(true) }} className="team-item">
+                {
+                      BoardOfDirector?.map((res,i)=>{
+                        return(
+                  <div className="" onClick={() => {setValue(i),setIsShown2(true)}}>
+                   <div className="team-item">
                       <span className="img-itm">
-                        <LazyLoader src={image1} className={"img-fluid"} width={"224"} height={"349"} alt={"Vinita Patodia"} />
+                        <LazyLoader src={res.image} className={"img-fluid"} width={"224"} height={"349"} alt={"Vinita Patodia"} />
                         {/* <img src={imageP} width="224" height="349" className="img-fluid" alt="loading" /> */}
                       </span>
                       <div className="namedesg">
-                        <h5>Mrs. Vinita Patodia</h5>
-                        <p className="designation">Non-Executive Chairperson</p>
+                        <h5>{res.title}</h5>
+                        <p className="designation">{res.description}</p>
                       </div>
 
 
                     </div>
-                    {
-                    check ?
-                    <div className="content-extra">
-                      <span className="icon-table cursor-pointer" onClick={() => { setcheck(false) }}>close</span>
-                        <p>loream</p>
-                    </div>:
-                    ""
+
+
+                      
+                    
+
+                    <Modal show={IsShown2}  size="md"  aria-labelledby="contained-modal-title-vcenter" className="contact-modal" centered>
+                    <div className="content-extra" >
+                      <Button onClick={() => {setIsShown2(false),console.log("kk",IsShown2) }}>close</Button>
+                    <FontAwesomeIcon icon={faClose} className="icon-table cursor-pointer" onClick={() => { setIsShown3("kkk"),console.log("kk",IsShown3) }} />
+                      <div className="team-img-pos">
+                          <div className="team-img">
+                          <LazyLoader src={BoardOfDirector[value].image} className={"img-fluid"} width={"224"} height={"349"} alt={"Vinita Patodia"} />
+                          </div>
+                          <div className="team-position">
+                              <h4>{BoardOfDirector[value].title}</h4>
+                          </div>
+                        </div>
+                    </div>
+                    </Modal>
+
+                   
+                  </div>
+                   )
+                      })
+
                     }
-                  </div>
-                  <div className="">
-                    <div className="team-item">
-                      <span className="img-itm">
-                        <LazyLoader src={image1} className={"img-fluid"} width={"224"} height={"349"} alt={"Vinita Patodia"} />
-                        {/* <img src={imageP} width="224" height="349" className="img-fluid" alt="loading" /> */}
-                      </span>
-                      <div className="namedesg">
-                        <h5>Mrs. Vinita Patodia</h5>
-                        <p className="designation">Non-Executive Chairperson</p>
-                      </div>
-
-
-                    </div>
-                  </div>
-                  <div className="">
-                    <div className="team-item">
-                      <span className="img-itm">
-                        <LazyLoader src={image1} className={"img-fluid"} width={"224"} height={"349"} alt={"Vinita Patodia"} />
-                        {/* <img src={imageP} width="224" height="349" className="img-fluid" alt="loading" /> */}
-                      </span>
-                      <div className="namedesg">
-                        <h5>Mrs. Vinita Patodia</h5>
-                        <p className="designation">Non-Executive Chairperson</p>
-                      </div>
-
-
-                    </div>
-                  </div>
-                  <div className="">
-                    <div className="team-item">
-                      <span className="img-itm">
-                        <LazyLoader src={image1} className={"img-fluid"} width={"224"} height={"349"} alt={"Vinita Patodia"} />
-                        {/* <img src={imageP} width="224" height="349" className="img-fluid" alt="loading" /> */}
-                      </span>
-                      <div className="namedesg">
-                        <h5>Mrs. Vinita Patodia</h5>
-                        <p className="designation">Non-Executive Chairperson</p>
-                      </div>
-
-
-                    </div>
-                  </div>
+                 
                 </Slider>
               </div>
             </div>
