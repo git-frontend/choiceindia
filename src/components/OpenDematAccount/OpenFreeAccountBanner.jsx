904147199b2c53edbda63b76bcbd5319d956abf1
trilochan.badal <trilochan.badal@choicetechlab.com>
2022-12-20 19:08:01 +0530

    recaptcha added to subbroker pages.

--- src/components/OpenDematAccount/OpenFreeAccountBanner.jsx
+++ src/components/OpenDematAccount/OpenFreeAccountBanner.jsx
@@ -80,7 +80,9 @@ const OpenFreeAccountBanner = () => {
         
     }, [toggle]);
 
-    
+    // useEffect(() => {
+    //     console.log('')
+    // },[])    
     return (
         <div>
              <section className="banner-sect" >
--- src/components/SubBroker/BannerSBrokerCampaign.jsx
+++ src/components/SubBroker/BannerSBrokerCampaign.jsx
@@ -38,7 +38,9 @@ function BannerSBroker() {
                         </div>
                         <div className="col-md-6 col-lg-4 ">
                             <div className="franchise-form justify-content-end d-flex" id="form-banner">
-                                <SubBrokerForm />
+                                <GoogleReCaptchaProvider reCaptchaKey="6Lc9qf4hAAAAABMa3-oFLk9BAkvihcEhVHnnS7Uz">
+                                    <SubBrokerForm />
+                                </GoogleReCaptchaProvider>
                             </div>
                         </div>
                     </div>
