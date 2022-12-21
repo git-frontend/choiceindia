6c2a0fc9b3f62c2d8863db1d7dedb48ce4d2374d
Jagruti <jagruti.palkar@choicetechlab.com>
2022-12-21 11:31:51 +0530

    changes

---
+++ src/components/CurrencyTrading/MoreContent.jsx
@@ -0,0 +1,98 @@
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+function MoreContent() {
+
+  return (
+      <div>
+          <section className="ref-sec-terms">
+              <div className="container">
+                  <div className="row">
+                      <div className="col-md-12 text-center">
+                          <h2 className="title-secnd">How to Open Forex Currency Trading Account</h2>
+                     </div>
+                          <div className="col-md-12">
+                              <div className="terms-cont" id="style-1">
+                                    <p className="sml-para-dv">The foreign exchange market, commonly abbreviated as forex, is a consolidated space where users can exchange different currencies. Hence, it is also known as the currency market and is most frequently used for business transactions where organisations convert their currency into the domestic currency of the country from where they make a purchase.</p>
+                                    <p className="sml-para-dv">But did you know that the forex market also witnesses trading similar to stocks?</p>
+                                    <p className="sml-para-dv">Banks, institutions and retail traders utilise the space to exchange different currencies and leverage the change in value to make significant profits. The fluctuating values of currencies cause constant disruption in exchange and interest rates and must be carefully monitored.</p>
+                                    <p className="sml-para-dv">The flexibility of the space also makes it inviting for an investor looking to diversify their portfolio, albeit with its own set of risks. While India has placed several restrictions on the forex trading space, it is possible to trade with specific currencies, provided you open a forex trading account with a stock broker authenticated by the Securities &amp; Exchange Board of India (SEBI).</p>
+                                    <p className="sml-para-dv">Are you curious to better understand the intricacies of a currency trading account and subsequent trading in India? Read on to understand the forex market in India.</p>
+                                    <br />
+                                    <h3>What is Forex Trading &amp; Currency Trading in India?</h3>
+                                    <p className="sml-para-dv">Unsurprisingly, you might wonder: What is forex trading, and how is currency trading in India different from other parts of the world?</p>
+                                    <p className="sml-para-dv">This answer can be better understood by understanding the general rules and regulations of forex trading.</p>
+                                    <ul className="terms-list sml-para-dv">
+                                            <li>Every currency in the world is denoted by a three-letter capitalised word, usually related to the currency's name. For instance, USD signifies US dollars while GBP is short for Great Britain Pound.</li>
+                                            <li>The listed currency exchanges follow a standard format - the base currency/quote currency. Hence a GBP/USD listing with a particular value (say 0.008) indicates that one GBP equates to 0.08 USD.</li>
+                                            <li>Demand and supply are the driving factors that control the price of exchange, whereas additional elements like the economic strength of a country and geopolitical issues are also responsible for having a significant impact on the exchange rates.</li>
+                                        
+                                    </ul>
+                                    <p className="sml-para-dv">While the standardised rules are similar for forex trading in India, there's a significant catch. Indian investors can only trade in exchange pairs that include the Indian rupee or INR, as denoted on the forex market.</p>
+                                    <p className="sml-para-dv">Effectively, that leaves a few trading options.</p>
+                                    <p className="sml-para-dv">However, existing pairings of INR include USD, GBP, Euros and the Japanese Yen, which can be exchanged against the rupee for a chance to acquire profits.</p>
+                                    
+                                    <br />
+                                    <h3>How to do Forex Trading in India?</h3>
+                                    <p className="sml-para-dv">Unlike most countries, the forex market in India is regulated by SEBI, and the trades take place on standardised exchanges, namely the National Stock Exchange (NSE), Bombay Stock Exchange (BSE), and lastly, the Metropolitan Stock Exchange (MSE)</p>
+                                    <p className="sml-para-dv">The difference in forex regulation raises a question: How to do forex trading in India?</p>
+                                    <p className="sml-para-dv">The first step is to <a href="https://choiceindia.com/open-free-demat-account">open a free Demat account</a> with an online brokerage firm and get accepted for executing trades. The registration process mandates copies of documents for address and identity proof, along with a know-your-customer (KYC) process</p>
+                                    <p className="sml-para-dv">Remember that only four pairs exist in the Indian forex market. So, if you want to exchange USD for GBP, you must execute two separate trades - INR/USD and INR/GBP.</p>
+                                    <p className="sml-para-dv">However, the limited opportunities in the market are also beneficial in the long term since they help you track currency exchange rates more quickly. Tracking impactful events in major countries like the US, the UK, and Japan is more effortless, helping you rapidly identify discrepancies in exchange rates.</p>
+
+                                    <br />
+                                    <h3>What are the Active Hours of Forex Trading in the Indian Market? </h3>
+                                    <p className="sml-para-dv">Usually, forex markets are operative twenty-four hours a day and accept exchange requests five and a half days per week. But since the forex market in India is regulated by a dedicated entity, the forex market timing is adjusted to standard market times at Indian exchanges.</p>
+                                    <p className="sml-para-dv">As such, the forex market opening time in India is 9 am, and the closing time is 5 pm. Hence, you get a dedicated eight hours of currency trading time to execute a trade. </p>
+                                    
+                                    <br />
+                                    <h3>What are the Best Forex Trading Platforms in India?</h3>
+                                    <p className="sml-para-dv">Several forex trading platforms in India ease the exchange process and help you hedge against the falling currency value. However, the best forex trading platform in India can be determined by adherence to one or more of the following.</p>
+
+                                    <ul className="terms-list sml-para-dv">
+                                        <li><strong className="bold-color">Compliance :</strong> Authorisation by SEBI is the most critical factor to look out for since regulatory compliance is established to help guard you against market volatility.</li>
+                                        <li><strong className="bold-color">Optimisation :</strong> In the world of portable technology like mobiles and tablets, choosing a platform that runs smoothly on desktops and phones is crucial. Look for a forex trading app with a friendly user interface to help you rapidly make purchases and add funds to execute trades quickly.</li>
+                                        <li><strong className="bold-color">Customer service :</strong> A responsive and dedicated customer service program is vital to help you address queries regarding the elements of the platform and solve any transaction or forex trading issues.</li>
+                                      
+                                       
+                                
+                                    </ul>
+                                        
+                                    <p className="sml-para-dv">Choice India comprises all these elements, marking its reputation as one of the best forex trading platforms in India. The welcoming disposition of the dedicated <a href="https://choiceindia.com/investment-app">investment app</a> also qualifies Choice India as one of the best forex trading app in India. It is suitable for long-term investors and comprises minimal <a href="https://choiceindia.com/brokerage-charges">brokerage charges</a>.</p>
+                
+                                    <br />
+                                    <h3>What are the Forex Trading Charges in India?</h3>
+                                    <p className="sml-para-dv">Forex trading in India is synonymous with some standard forex charges, irrespective of the platform and nature of the trade executed.</p>
+                                   
+                                    <ul className="terms-list sml-para-dv">
+                                        <li><strong className="bold-color">Forex Trading Brokerage charges :</strong> While this depends on your choice of online broker, the forex trading brokerage charges in India are usually 0.02% for futures and for options Rs. 20 per trade or lot.</li>
+                                        <li><strong className="bold-color">Goods and Services Tax (GST) :</strong> The GST levied depends on the value of the transaction undertaken. For instance, transactions lesser than 1 lakh only comprise a 1% taxable value. The tax amount attracted is 18% of this taxable value.</li>
+                                        <li><strong className="bold-color">Stamp duties :</strong>The payable stamp duty differs according to the state where you reside.</li>
+                                        <li><strong className="bold-color">Direct tax :</strong> The profits acquired from forex trading are added to your overall income, and you are taxed according to the relevant pre-defined tax slabs as per the Government of India.</li>
+                                    
+                                    </ul>
+
+                                    <br />
+                                    <h3>What documents are required to Open a Forex Currency Trading Account in India?</h3>
+                                    <p className="sml-para-dv">Opening a forex trading account is no different than opening a trading account. Hence, it requires some standard documents, as listed below.</p>
+                                    <ul className="terms-list sml-para-dv">
+                                        <li>Proof-of-identity (can include government-issued documents like passport, driver's licence aadhar, voter's ID, pan card)</li>
+                                        <li>Address proof (voter's ID, driver's licence, ration card, aadhar)</li>
+                                        <li>Income proof (bank account statements of the last six months)</li>
+                                        <li>Proof of bank account (Cancelled check from your bank)</li>
+                                    
+                                    </ul>
+                                    <p className="sml-para-dv">Once you confirm your identity and verify your credentials with these documents, you are in possession of a <a href="https://choiceindia.com/demat-account">Demat account</a>. You can use it to trade derivatives or forex and explore other financial instruments!</p>
+
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+    </section>
+
+      </div>
+  );
+}
+
+export default MoreContent;
