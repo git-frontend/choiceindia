ecb88c2e567333e1c47395ad17fc92a9f314ebc0
krushna.mohanty <krushna.mohanty@choicetechlab.com>
2022-12-22 13:50:30 +0530

    banner add

--- src/components/Goverment-advisory/Banner.jsx
+++ src/components/Goverment-advisory/Banner.jsx
@@ -2,6 +2,8 @@
 import React from "react";
 import { useState } from "react";
 // import "./GovernmentAdv.scss";
+import GovernmentAdvBan from '../../assets/images/goverment-advisory/government-advisory-banner.webp';
+import LazyLoader from "../Common-features/LazyLoader";
 
 function Banner() {
 
@@ -15,14 +17,15 @@ function Banner() {
 
       <section className="goverment-advisorysection">
         <div className="container">
-          <div className="row">
-            <div className=" left-sec">
+          <div className="row align-items-center">
+            <div className="left-sec">
               <h1 className="big-ttl title">Government Advisory</h1>
               <p className="para">We collaborate for pioneering projects to consult for the development and progress of our nation. Our experienced research team is capable to deploy research, sampling & survey operations.
                 </p>
                 <p className="para pt-4">We provide end to end bid process management sevices to governments from bid solicitation to project delivery.</p>
             </div>
-            <div className=" right-sec">
+            <div className="right-sec">
+                <LazyLoader src={GovernmentAdvBan} className={'img-fluid'} width={"502"} height={"343"} alt={"Government Advisory"} />
             </div>
           </div>
         </div>
