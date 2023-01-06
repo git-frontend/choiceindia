011aa030a10120c4169bc07ec696b54316869470
Jagruti <jagruti.palkar@choicetechlab.com>
2023-01-06 15:36:23 +0530

    changes

--- src/components/Stock-Broker-Mumbai/BestInMumbai.jsx
+++ src/components/Stock-Broker-Mumbai/BestInMumbai.jsx
@@ -21,22 +21,22 @@ function BestInMumbai() {
               
 
                 <p className="">
-                <span className="">
+                <p className="">
                 Choice India comes with a legacy of two decades serving the financial sector and has earned its respect as one of the leading stock brokers in Mumbai. It extends its services in the space; of; broking &amp; distribution, wealth planning, insurance, loans, capital advisory, management consultancy, government advisory and tax advisory. 
-                </span><br /><br />
-                <span className="">
+                </p>
+                <p className="mb-0">
                 The research advisory cell gives recommendations by experts, and Signal, the automated trading platform provides trading calls for investors without any human intervention. The Stratezy tool lets the traders create, backtest, and optimize live trades. Moreover, one can invest in handpicked baskets curated by experts.
-                </span>
+                </p>
 
-                  {!showterm ? <span onClick={()=>{setshowterm(true);console.log("hhh")}}>... <em className="btn-read">Read more</em></span>:""}
-                  {showterm ?<span>  &nbsp;
-                    <span className="">
+                  {!showterm ? <span onClick={()=>{setshowterm(true);console.log("hhh")}}> <em className="btn-read">Read more</em></span>:""}
+                  {showterm ?<span>&nbsp;
+                    <p className="">
                 Choice India, through its trading app, Choice FinX, aims to provide end-to-end financial solutions in one single app with equity, mutual funds, and insurance. It provides advanced features for an investor to make his investment journey seamless with the best trading facility and the lowest brokerage charges.
-                </span><br /><br />
-                <span className="">
+                </p>
+                <p className="mb-0">
                 The financial offerings are designed in a manner to simplify personal finances and investment stems that would help create wealth to each and every individual and experience ‘The Joy of Earning.’
 
-                </span>
+                </p>
                   <span onClick={()=>{setshowterm(false);console.log("hhh")}}>&nbsp;<em className="btn-read">Read less</em></span></span>:""}</p>
 
 
