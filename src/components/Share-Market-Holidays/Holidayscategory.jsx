fdcdc31823a41801c338c2b733e19886b56b7927
jayashri <jayashri.yadav@choicetechlab.com>
2023-02-09 19:39:42 +0530

    worked on share-market-holiday

--- src/components/Share-Market-Holidays/Holidayscategory.jsx
+++ src/components/Share-Market-Holidays/Holidayscategory.jsx
@@ -80,8 +80,8 @@ function Holidayscategory() {
                     {
                       toggleState == 0 ?
                         <div>
-                          <h2 className="title-secnd1">Share Market Holidays 2023</h2>
-                          <p className="title_para res_para ">Here is the complete list of share markets  for the year 2023.</p>
+                          <h2 className="title-secnd1">Stock Market Holidays 2023</h2>
+                          <p className="title_para res_para ">Here is the complete list of Indian stock market holidays for the year 2023.</p>
                         </div> :
                         toggleState == 1 ?
                           <div>
@@ -104,12 +104,12 @@ function Holidayscategory() {
 
 
                   </div>
-                  <div className="col-xl-8 col-md-12" id="best-stock">
+                  <div className="col-xl-9 col-md-12" id="best-stock">
                     <ul className="list-group list_group1">
-                      <li className={toggleState === 0 ? "list-group-item list listsec " : "list-group-item list"} > <Link className="urllinks1" to="/stock-market-holidays"  > All</Link></li>
-                      <li className={toggleState === 1 ? "list-group-item list listsec " : "list-group-item list"} ><Link className="urllinks1" to="/nse-holidays" >NSE Holidays</Link></li>
-                      <li className={toggleState === 2 ? "list-group-item list listsec " : "list-group-item list"}><Link className="urllinks1" to="/bse-holidays" >BSE Holidays</Link></li>
-                      <li className={toggleState === 3 ? "list-group-item list listsec " : "list-group-item list"}><Link className="urllinks1" to="/mcx-ncdex-holidays" >MCX Holidays</Link></li>
+                      <li className={toggleState === 0 ? "list-group-item list listsec " : "list-group-item list"} > <Link className="urllinks1" to="/stock-market-holidays" onClick={() => setToggleState(0) } > All</Link></li>
+                      <li className={toggleState === 1 ? "list-group-item list listsec " : "list-group-item list"} ><Link className="urllinks1" to="/nse-holidays" onClick={() => setToggleState(1) } >NSE Holidays</Link></li>
+                      <li className={toggleState === 2 ? "list-group-item list listsec " : "list-group-item list"}><Link className="urllinks1" to="/bse-holidays" onClick={() => setToggleState(2) } >BSE Holidays</Link></li>
+                      <li className={toggleState === 3 ? "list-group-item list listsec " : "list-group-item list"}><Link className="urllinks1" to="/mcx-ncdex-holidays" onClick={() => setToggleState(3) } >MCX/NCDEX Holidays</Link></li>
                     </ul>
 
                   </div>
@@ -151,32 +151,27 @@ function Holidayscategory() {
                                                     <th width="20%">S.No </th>
                                                     <th>Holidays</th>
                                                     <th className="holidaydropdown">
-                                                      {/* <span className="downkey">All</span> */}
-                                                      {/* <div className="month-list">
-                                                        <form>
-                                                          <div class="form-group">
-                                                            <input type="checkbox" id="html"/>
-                                                              <label for="html">HTML</label>
-                                                          </div>
-                                                          <div class="form-group">
-                                                            <input type="checkbox" id="css"/>
-                                                              <label for="css">CSS</label>
-                                                          </div>
-                                                          <div class="form-group">
-                                                            <input type="checkbox" id="javascript"/>
-                                                              <label for="javascript">Javascript</label>
-                                                          </div>
-                                                        </form>
-                                                      </div> */}
-                                                   <Dropdown>
-                                                      <Dropdown.Toggle variant="success" id="dropdown-basic">
-                                                        Dropdown Button
+
+                                                   <Dropdown className="drop_list">
+                                                      <Dropdown.Toggle variant="success" id="dropdown-basic" className="drop-btn">
+                                                        All
                                                       </Dropdown.Toggle>
 
                                                       <Dropdown.Menu>
-                                                        <Form.Check type="checkbox" label="ALL" />
-                                                        <Form.Check type="checkbox" label="jan" />
-                                                        <Form.Check type="checkbox" label="feb" /> 
+                                                        <Form.Check type="checkbox" label="All" />
+                                                        <Form.Check type="checkbox" label="January" />
+                                                        <Form.Check type="checkbox" label="February" />
+                                                        <Form.Check type="checkbox" label="March" /> 
+                                                        <Form.Check type="checkbox" label="April" /> 
+                                                        <Form.Check type="checkbox" label="May" /> 
+                                                        <Form.Check type="checkbox" label="June" /> 
+                                                        <Form.Check type="checkbox" label="July" /> 
+                                                        <Form.Check type="checkbox" label="August" /> 
+                                                        <Form.Check type="checkbox" label="September" />
+                                                        <Form.Check type="checkbox" label="October" />
+                                                        <Form.Check type="checkbox" label="November" />
+                                                        <Form.Check type="checkbox" label="December" /> 
+
                                                       </Dropdown.Menu>
                                                     </Dropdown>
     
@@ -287,9 +282,9 @@ function Holidayscategory() {
                                               </table>
                                             </div>
                                           </div>
-                                          <div className="holiday-list d-flex justify-content-between pb-3 pt-3">
+                                          <div className="holiday-list d-flex justify-content-between ">
                                             <div className="holiday-text">Stock Market Holidays 2023 List PDF</div>
-                                            <div className="holiday-text"><strong className="mr4">Download PDF</strong><FaRegFilePdf onClick={() => { window.open(stockmarketholidays) }} className="cursor-pointer" /></div>
+                                            <div className="holiday-text download_text">Download PDF<FaRegFilePdf onClick={() => { window.open(stockmarketholidays) }} className="cursor-pointer mr4" /></div>
                                           </div>
                                         </div>
                                       </div> :
@@ -346,13 +341,15 @@ function Holidayscategory() {
                             <br />
                             Muhurat Trading shall be held on Thursday, November 04, 2023 (Diwali – Laxmi Pujan). Timings of Muhurat Trading shall be notified subsequently.
                             Please Note: The Exchange may alter/change any of the above Share Market Holidays for 2023, for which a separate circular shall be issued in advance. The Exchange may, however, close the market on days other than the above schedule holidays or may open the market on days originally declared as holidays. The Exchange may also extend, advance or reduce trading hours when it deems fit and necessary.
-                            <span className="read-more-target restext">
+                            {/* <span className="read-more-target restext">
                               Signal, an Automated Research Engine that provides research advisory with customised strategies for all types of Traders
                               has been back tested by our in-house advisory with a proven track record that has been in existence for almost a decade
                               now to provide the best intraday stocks to buy and sell.
                               <br />
                               For all stock recommendations for intra day trades, short-term trades and long-term trades , you can refer to the segment providing you with the latest research calls from the experts. <Link to="/open-free-demat-account"> Open a free demat account  </Link> with us to get the best intraday stocks to buy today in India.
-                            </span></span> <label htmlFor="post-1" className="read-more-trigger moreless-button"></label>
+                            </span> */}
+                            </span>
+                             {/* <label htmlFor="post-1" className="read-more-trigger moreless-button"></label> */}
                         </div>
                       </div>
                     </div> :
@@ -395,7 +392,8 @@ function Holidayscategory() {
                                 Open your <Link to='/open-free-demat-account'>free demat account</Link> with Choice and get the best stocks to buy today in India for a short term time horizon.
 
 
-                              </span></span> <label htmlFor="post-1" className="read-more-trigger moreless-button"></label>
+                              </span></span>
+                               {/* <label htmlFor="post-1" className="read-more-trigger moreless-button"></label> */}
                           </div>
                         </div>
                       </div> :
