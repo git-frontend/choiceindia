1d914e9e493325c74e5912e2a57d96200ad11057
krushna.mohanty <krushna.mohanty@choicetechlab.com>
2023-02-21 12:17:49 +0530

    sitemap implementation

--- src/sitemap-generator.js
+++ src/sitemap-generator.js
@@ -1,49 +1,45 @@
+// import fableServices from './Services/fableServices';
+
 require("babel-register")({
-    presets: ["es2015", "react"]
-  });
-   
-  const router = require("./sitemap-routes").default;
-  const Sitemap = require("react-router-sitemap").default;
-  
-  const AWSAmplify = require("aws-amplify");
-  const Amplify = AWSAmplify.default;
-  const API = AWSAmplify.API;
-  const config = require("../config").default;
-  
-  Amplify.configure({
-    API: {
-      endpoints: [
-        {
-          name: "posts",
-          endpoint: config.apiGateway.URL,
-          region: config.apiGateway.REGION
-        },
-      ]
-    }
-  });
-  
-  async function generateSitemap() {
-    try {
-      const posts = await API.get("posts", "/posts");
-      let idMap = [];
-  
-      for(var i = 0; i < posts.length; i++) {
-        idMap.push({ id: posts[i].postId });
-      }
-  
-      const paramsConfig = {
-        "/blog/:id": idMap
-      };
-  
-      return (
-        new Sitemap(router)
+  presets: ["es2015", "react"]
+});
+
+const router = require("./sitemap-routes").default;
+const Sitemap = require("react-router-sitemap").default;
+
+const AWSAmplify = require("aws-amplify");
+const Amplify = AWSAmplify.default;
+const API = AWSAmplify.API;
+
+
+async function generateSitemap() {
+  let idMap = [{}];
+
+  try {
+    fetch('https://choiceindia.com/fables/ghost/api/v3/content/posts/?key=280c55197998a010569e5d612a&limit=1000').then(res => {
+      return res.json();
+    })
+      .then(data => {
+        for (var i = 0; i < data.posts.length; i++) {
+          idMap.push({ id: data.posts[i].slug });
+        }
+
+        const paramsConfig = {
+          "/blog/:id": idMap
+
+        };
+
+        console.log(idMap, "data2");
+        return (
+          new Sitemap(router)
             .applyParams(paramsConfig)
-            .build("https://choiceindia.com")
+            .build("https://dev.choiceindia.com/")
             .save("./public/sitemap.xml")
-      );
-    } catch(e) {
-      console.log(e);
-    } 
+        );
+      })
+  } catch (e) {
+    console.log(e);
   }
-  
-  generateSitemap();
+}
+
+generateSitemap();
