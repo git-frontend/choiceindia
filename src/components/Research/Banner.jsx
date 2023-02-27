2b57f1d8813399c6054911a98b7d9203c7e1fb6d
SWAPNIL NEVAREKAR <swapnil.nevarekar@choicetechlab.com>
2023-02-27 18:51:32 +0530

    changes done research-banner

--- src/components/Research/Banner.jsx
+++ src/components/Research/Banner.jsx
@@ -1,12 +1,29 @@
 
 import React from "react";
-import ImageBanner from '../../assets/images/research/bannerResearch.gif';
+import ImageBanner1 from '../../assets/images/research/slider-banner1.webp';
+import ImageBanner2 from '../../assets/images/research/slider-banner2.webp';
 import SingleTemplate from "../Common-features/SingleTemplate";
 import Researchbanner from '../../assets/images/research/researchBanner.mp4'
 import { useState } from 'react';
+import Slider from "react-slick";
+import "../../../node_modules/slick-carousel/slick/slick.css";
+import "../../../node_modules/slick-carousel/slick/slick-theme.css";
 import LazyLoader from "../Common-features/LazyLoader";
 
 function Banner() {
+
+  const settings = {
+    infinite: true,
+    speed: 1500,
+    arrows: true,
+    slidesToShow: 1,
+    autoplay: false,
+    margin: 15,
+    dots: true,
+    autoplaySpeed: 1000,
+    slidesToScroll: 1,
+  };
+
 
   const [skeleton, setSkeleton] = useState(() => true);
   setTimeout(() => {
@@ -17,39 +34,61 @@ function Banner() {
     <div>
 
 
-      <section className="bannersection research-banner">
-        <div className="container">
-          <div className="row gx-5 align-items-center">
-            <div className="col-md-6">
-              <div className="bnr-left">
-                <h1 className="title">Research &amp; Insights</h1>
-                <p className="para">Your wealth passes by so many turbulences right from Income, to Spending, to Savings, to Debt. Managing Money requires Research.</p>
-                <p className="para pt-4">So let’s begin a journey of ReSearching your Wealth.</p>
-              </div>
-            </div>
-
-            {
-              skeleton ?
-                <SingleTemplate /> :
-                <div className="col-md-6 d-none d-sm-block">
-                  <div className="rightsec">
-                    {/* <video className='img-fluid' autoPlay loop muted width={"700"} height={"650"}>
-                      <source src={Researchbanner}  type='video/mp4' />
-                    </video> */}
-                    {/* <video src={Researchbanner} width="600" height="300" controls="controls" autoplay="true" /> */}
-                    {/* <video src={Researchbanner} alt={"Banner Images"} className={"img-fluid"} width={"957"} height={"623"} type={"video/mp4"}  autoPlay></video> */}
-                    {/* <img src={ImageBanner} alt="Banner Images" className="img-fluid " width={"957"} height={"623"}></img> */}
-                    <LazyLoader src={ImageBanner} alt={"Banner Images"} className={"img-fluid"} width={"957"} height={"623"}/>
-                  </div>
-                </div>
-            }
-            {/* <div className="col-md-6">
-                    <div className="rightsec">
-                      <img src={ImageBanner} alt="Banner Images" className="img-fluid thumb-img" width={""} height={""}></img>
+      <section className="research-banner-slider">
+        <Slider {...settings} className="team-list-slider">
+          <div className="slider-sub">
+                {/* <LazyLoader src={ImageBanner2} className={"img-fluid"} width={"1300"} height={"500"} alt={""} /> */}
+                <img src={ImageBanner2} className="banner-img"/>
+                <div className="slider-caption">
+                    <div className="container">
+                        <div className="slider-caption-sub">
+                          <h2 className="big-ttl"><span>Monthly Picks</span> Feb’23</h2>
+                          <p>The report highlights investment opportunities  identified by our research team during the month.</p>
+                          <span className="btn-bg btn-bg-dark"><a href="">Explore All Recommendations </a></span>
+                        </div>
                     </div>
-                </div> */}
-          </div>
-        </div>
+                </div>   
+            </div>
+            <div className="slider-sub">
+                {/* <LazyLoader src={ImageBanner2} className={"img-fluid"} width={"1300"} height={"500"} alt={""} /> */}
+                <img src={ImageBanner2} className="banner-img"/>
+                <div className="slider-caption">
+                    <div className="container">
+                        <div className="slider-caption-sub">
+                          <h2 className="big-ttl"><span>Monthly Picks</span> Feb’23</h2>
+                          <p>The report highlights investment opportunities  identified by our research team during the month.</p>
+                          <span className="btn-bg btn-bg-dark"><a href="">Explore All Recommendations </a></span>
+                        </div>
+                    </div>
+                </div>   
+            </div>
+            <div className="slider-sub">
+                {/* <LazyLoader src={ImageBanner2} className={"img-fluid"} width={"1300"} height={"500"} alt={""} /> */}
+                <img src={ImageBanner2} className="banner-img"/>
+                <div className="slider-caption">
+                    <div className="container">
+                        <div className="slider-caption-sub">
+                          <h2 className="big-ttl"><span>Monthly Picks</span> Feb’23</h2>
+                          <p>The report highlights investment opportunities  identified by our research team during the month.</p>
+                          <span className="btn-bg btn-bg-dark"><a href="">Explore All Recommendations </a></span>
+                        </div>
+                    </div>
+                </div>   
+            </div>
+            <div className="slider-sub">
+                {/* <LazyLoader src={ImageBanner2} className={"img-fluid"} width={"1300"} height={"500"} alt={""} /> */}
+                <img src={ImageBanner2} className="banner-img"/>
+                <div className="slider-caption">
+                    <div className="container">
+                        <div className="slider-caption-sub">
+                          <h2 className="big-ttl"><span>Monthly Picks</span> Feb’23</h2>
+                          <p>The report highlights investment opportunities  identified by our research team during the month.</p>
+                          <span className="btn-bg btn-bg-dark"><a href="">Explore All Recommendations </a></span>
+                        </div>
+                    </div>
+                </div>   
+            </div>
+        </Slider>
       </section>
 
 
