e256fa6321a257ada96ebeb5c58ea0899be4826e
krushna.mohanty <krushna.mohanty@choicetechlab.com>
2023-03-08 14:41:18 +0530

    source param changes

--- src/components/Common-features/DematAccountForm.jsx
+++ src/components/Common-features/DematAccountForm.jsx
@@ -315,7 +315,7 @@ function DematAccountForm(props) {
             // 'sidebar_seo_leads'
             "utm_medium":isBlog =="yes" ? UTMMedium.current || 'choice_blog' : UTMMedium.current || null,
             // 'blog_leads'
-            "utm_source": isBlog =="yes" ?UTMSource.current || 'demat_lead_generation' : UTMSource.current || null,
+            "utm_source": isBlog =="yes" ?UTMSource.current || 'seo_demat_lead_generation' : UTMSource.current || null,
             "utm_term": UTMTerm.current || null,
             // "captcha":"f9A0RMq3vF7fPYkEiqZToKUKdneNzA2YWfMeKSHhkm",
             "captchaResp": captchaToken,
