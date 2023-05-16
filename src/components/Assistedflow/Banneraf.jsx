6f8622826eb25968c29be361cf5eb0dc3da9cf22
Jagruti <jagruti.palkar@choicetechlab.com>
2023-05-16 13:26:26 +0530

    changes

--- src/components/Assistedflow/Banneraf.jsx
+++ src/components/Assistedflow/Banneraf.jsx
@@ -153,65 +153,70 @@ function Banneraf() {
                                 {showSecondDiv && (
                                     <div className="otpsec">
                                         <div className="resdiv">
-                                        <div className="otpmodal">
-                                            <p className="otptext">Enter One Time Authentication Code</p>
-                                            <p className="subtext">Code sent to your registered mobile number +91 87******68</p>
-                                        </div>
-                                        <div class="otp-wrapper" >
+                                            <div className="otpmodal">
+                                                <p className="otptext">Enter One Time Authentication Code</p>
+                                                <p className="subtext">Code sent to your registered mobile number +91 87******68</p>
+                                            </div>
+                                            {/* <div class="otp-wrapper" >
                                             <input type="text" class="otp-input" maxlength="1" / >
                                             <input type="text" class="otp-input"  maxlength="1"/ >
                                             <input type="text" class="otp-input" maxlength="1"/ >
                                             <input type="text" class="otp-input" maxlength="1"/ >
                                             <input type="text" class="otp-input" maxlength="1"/ >
                                             <input type="text"  class="otp-input" maxlength="1"/ >
+                                        </div> */}
+
+                                            <div id="divOuter">
+                                                <div id="divInner">
+                                                    <input formcontrolname="otpValue" id="partitioned" maxlength="6" onkeypress="if(this.value.length==6) return false;" type="text" class="" />
+                                                    <i class="bar"></i>
+                                                </div>
+                                            </div>
+                                            </div>
+                                            <Button className="btn-bg btn-bg-dark mt-2 submitbtn disablebtn" onClick={handleSecondButtonClick}><span>Submit</span></Button>
+                                      
                                         </div>
-                                        <p className="warning">Resend OTP in 30 sec</p>
-                                        
-                                        </div>
-                                       
-                                        <Button className="btn-bg btn-bg-dark mt-2 submitbtn disablebtn" onClick={handleSecondButtonClick}><span>Submit</span></Button>
-                                    </div>
                                 )}
-                                {showThirdDiv && (
-                                    <div className="redirectwrap">
-                                      <LazyLoader src={Redirect} alt={""} className={"img-fluid redirectimg"} width={"74"} height={"74"} />
-                                       <p className="redirecttext">Redirecting to Payment Page</p>
+                                        {showThirdDiv && (
+                                            <div className="redirectwrap">
+                                                <LazyLoader src={Redirect} alt={""} className={"img-fluid redirectimg"} width={"74"} height={"74"} />
+                                                <p className="redirecttext">Redirecting to Payment Page</p>
+                                            </div>
+                                        )}
                                     </div>
-                                )}
-                            </div>
 
                             
 
 
                         </div>
-                        <div className="sticky-investnow">
-                            <div className="stickywrap">
-                                <div className="left">
-                                    <p className="text">Total Investment Amount</p>
-                                    <p className="num">₹ 15,000</p>
+                            {/* <div className="sticky-investnow">
+                                <div className="stickywrap">
+                                    <div className="left">
+                                        <p className="text">Total Investment Amount</p>
+                                        <p className="num">₹ 15,000</p>
+                                    </div>
+                                    <div className="rightbtn">
+                                        <Link to="/" className="btn-bg btn-bg-dark investnowbtn" onClick=""><span>Invest Now</span> </Link>
+                                    </div>
                                 </div>
-                                <div className="rightbtn">
-                                    <Link to="/" className="btn-bg btn-bg-dark investnowbtn" onClick=""><span>Invest Now</span> </Link>
-                                </div>
+                            </div> */}
+                        </div>
+                        <div className="successful">
+                            <LazyLoader src={ThumbUp} alt={""} className={"img-fluid redirectimg"} width={"74"} height={"74"} />
+                            <p className="sucesstext">Your order is successful!</p>
+                            <div className="rightbtn">
+                                <Link to="/" className="btn-bg btn-bg-dark " onClick=""><span>Awesome !</span> </Link>
                             </div>
                         </div>
-                    </div>
-                                <div className="successful">
-                                    <LazyLoader src={ThumbUp} alt={""} className={"img-fluid redirectimg"} width={"74"} height={"74"} />
-                                    <p className="sucesstext">Your order is successful!</p>
-                                    <div className="rightbtn">
-                                        <Link to="/" className="btn-bg btn-bg-dark " onClick=""><span>Awesome !</span> </Link>
-                                     </div>
-                                </div>
-                 
-                             <div className="order-register">
-                                    
-                                    <p className="sucesstext">Order Registered!</p>
-                                    <p className="subtext">Copy &amp; Share link with client to complete the payment.</p>
-                                    <div className="rightbtn">
-                                        <Link to="/" className="btn-bg btn-bg-dark " onClick=""><span>Copy Link</span> </Link>
-                                    </div>
-                             </div>
+
+                        <div className="order-register">
+
+                            <p className="sucesstext">Order Registered!</p>
+                            <p className="subtext">Copy &amp; Share link with client to complete the payment.</p>
+                            <div className="rightbtn">
+                                <Link to="/" className="btn-bg btn-bg-dark " onClick=""><span>Copy Link</span> </Link>
+                            </div>
+                        </div>
                     </div>
 
             </section>
