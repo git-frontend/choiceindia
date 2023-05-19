b5ed3cfb01f059ec10ea73de89e37f5b614bdf74
jayashri <jayashri.yadav@choicetechlab.com>
2023-05-19 14:34:29 +0530

    changes done in new demat form

--- src/components/Common-features/NewDematAccountForm.jsx
+++ src/components/Common-features/NewDematAccountForm.jsx
@@ -484,23 +484,27 @@ function NewDematAccountForm(props) {
 
     useEffect(() => {
         //  console.log('Inside thanku',isShow)
-          if(showThanku.page != 'add-lead' && !showThanku.redirectionLink){
-            setTimeout(() => {
-              if (isShow.redirectionLink === null) {
-                isShow.closeMd();
-              } else {
-                isShow.closeMd(isShow.redirectionLink);
+        if(showThanku.showModal){
+            if(showThanku.page != 'add-lead' && !showThanku.redirectionLink){
+                setTimeout(() => {
+                  if (showThanku.redirectionLink === null) {
+                    showThanku.closeMd();
+                  } else {
+                    showThanku.closeMd(showThanku.redirectionLink);
+                  }
+                }, 15000)
+              } else if (showThanku.page != 'add-lead' && showThanku.redirectionLink) {
+                setTimeout(() => {
+                  if (showThanku.redirectionLink === null) {
+                    showThanku.closeMd();
+                  } else {
+                    showThanku.closeMd(showThanku.redirectionLink);
+                  }
+                }, 2000)
               }
-            }, 15000)
-          } else if (showThanku.page != 'add-lead' && showThanku.redirectionLink) {
-            setTimeout(() => {
-              if (showThanku.redirectionLink === null) {
-                showThanku.closeMd();
-              } else {
-                showThanku.closeMd(showThanku.redirectionLink);
-              }
-            }, 2000)
-          }
+
+        }
+          
         }, [showThanku.showModal])
 
     return (
@@ -519,16 +523,20 @@ function NewDematAccountForm(props) {
                         <Form>
                             <Form.Group className="mb-3 formgrp formgrp-new">
                                 <Form.Label>Mobile Number*</Form.Label>
+                               
                                 <div className="sub-formgrp sub-formgrp-new">
+                               
                                     <Form.Control isValid={!errors.invalidMobile || !errors.required} type="tel" pattern="\d*" name="mobile_no" id="mobile_no" placeholder="0000000000" className="formcontrol digit-otp " autoComplete="off" maxLength="10" isInvalid={errors.invalidMobile || errors.required} value={mobileNumber} onChange={handleMobile} />
+                                    
                                     {
                                         errors.invalidMobile ? <Form.Control.Feedback type="invalid">{OpenAccountLanguageContent.getContent(props.language ? props.language : 'en', 'invalidmob')}</Form.Control.Feedback> : ''
                                     }
                                     {
                                         errors.required ? <Form.Control.Feedback type="invalid">{OpenAccountLanguageContent.getContent(props.language ? props.language : 'en', 'reqmob')}</Form.Control.Feedback> : ''
                                     }
-                                    <span className="state-code">+91</span>
+                                    {/* <span className="state-code">+91</span> */}
                                 </div>
+                              
                                 <div key="inline-checkbox" className="sub-formgrp cust-checkbox">
                                     <Form.Check
                                         inline
@@ -566,8 +574,8 @@ function NewDematAccountForm(props) {
                     <div className="demat-account-form demat-account-form-new">
                         <div className="thank-you-msg">
                         <LazyLoader src={thumbsup} className={'img-fluid'} width={"160"} height={"160"} alt="Loading" />
-                            <h2 className="thankyou-title">Thank You For Your Registration!</h2>
-                            <p className="subheading">You are being redirected to onboarding page!</p>
+                            <h2 className="thankyou-title">Thank You!</h2>
+                            <p className="subheading">{showThanku.resText? showThanku.resText: "You are being redirected to onboarding page!"}</p>
                         </div>
                         
                     </div>
