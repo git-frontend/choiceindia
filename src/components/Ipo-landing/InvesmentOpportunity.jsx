f7901050227237446186c06b4542239520b9a177
satyavendra singh <satyavendra.singh@choicetechlab.com>
2023-05-23 17:24:11 +0530

    adding ipo landing page

--- src/components/Ipo-landing/InvesmentOpportunity.jsx
+++ src/components/Ipo-landing/InvesmentOpportunity.jsx
@@ -4,9 +4,41 @@ import ImageSub3 from '../../assets/images/ipo-landing/low-charges.svg';
 import ImageSub4 from '../../assets/images/ipo-landing/free-ipo-report.svg';
 import LazyLoader from '../Common-features/LazyLoader';
 import React from 'react';
+import Slider from 'react-slick';
 
 function InvesmentOpportunity() {
-  
+    const settings1 = {
+        infinite: true,
+        speed: 1500,
+        slidesToShow: 4,
+        arrows: false,
+        autoplay: false,
+        dots: false,
+        autoplaySpeed: 1500,
+        slidesToScroll: 1,
+        swipeToSlide: false,
+        responsive: [
+
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    dots: false,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1.5,
+                    slidesToScroll: 1,
+                    dots: false,
+                }
+            }
+
+        ]
+
+    };
 
   return (
     <>
@@ -20,26 +52,24 @@ function InvesmentOpportunity() {
                         </div>
                     </div>
                     <div className='col-md-6'>
-                        <div className='right-sec'>
-                        <div className='all-features-list'>
-                                    <div className="feature-item">
-                                        <LazyLoader src={ImageSub1} className={"img-fluid"} alt={"Stock Collection"} width={"84"} height={"76"} />
-                                        <h5 className="feature-ttl">Free Demat <span>Account Opening</span></h5>
-                                    </div>
-                                    <div className="feature-item">
-                                        <LazyLoader src={ImageSub2} className={"img-fluid"} alt={"Basket Investing "} width={"77"} height={"76"} />
-                                        <h5 className="feature-ttl">Zero AMC Charges <span>for First Year</span></h5>
-                                    </div>
-                                    <div className="feature-item">
-                                        <LazyLoader src={ImageSub3} className={"img-fluid"} alt={"Financial Planner"} width={"53"} height={"76"} />
-                                        <h5 className="feature-ttl">Low DP Charges <span>(Rs. 10 only)</span></h5>
-                                    </div>
-                                    <div className="feature-item">
-                                        <LazyLoader src={ImageSub4} className={"img-fluid"} alt={"Insurance IntelliMart"} width={"76"} height={"76"} />
-                                        <h5 className="feature-ttl">Free IPO Report <span>& Advisory</span></h5>
-                                    </div>
-                                </div>
-                        </div>
+                        <Slider {...settings1} className='all-features-list'>
+                            <div className="feature-item">
+                                <LazyLoader src={ImageSub1} className={"img-fluid"} alt={"Stock Collection"} width={"84"} height={"76"} />
+                                <h5 className="feature-ttl">Free Demat <span>Account Opening</span></h5>
+                            </div>
+                            <div className="feature-item">
+                                <LazyLoader src={ImageSub2} className={"img-fluid"} alt={"Basket Investing "} width={"77"} height={"76"} />
+                                <h5 className="feature-ttl">Zero AMC Charges <span>for First Year</span></h5>
+                            </div>
+                            <div className="feature-item">
+                                <LazyLoader src={ImageSub3} className={"img-fluid"} alt={"Financial Planner"} width={"53"} height={"76"} />
+                                <h5 className="feature-ttl">Low DP Charges <span>(Rs. 10 only)</span></h5>
+                            </div>
+                            <div className="feature-item">
+                                <LazyLoader src={ImageSub4} className={"img-fluid"} alt={"Insurance IntelliMart"} width={"76"} height={"76"} />
+                                <h5 className="feature-ttl">Free IPO Report <span>& Advisory</span></h5>
+                            </div>
+                        </Slider>
                     </div>
                 </div>
             </div>
