479f5ef70097a3cc1ddc6a4b247ecf59261fcea3
jayashri <jayashri.yadav@choicetechlab.com>
2023-05-23 15:40:41 +0530

    changes done

--- src/components/Futures-Options-Trading/futuresOptionsTradingBanner.jsx
+++ src/components/Futures-Options-Trading/futuresOptionsTradingBanner.jsx
@@ -17,6 +17,40 @@ import FandOopendematAc from './FandOopendematAc';
 import WhyChoice from './WhyChoice';
 import './futuresoptionstrading.scss';
 function futuresOptionsTradingBanner() {
+
+
+    const [name, setName ] = useState('hideform');
+    const getPosition = () => {
+      const element = document.getElementById("showForm");
+      if(element){
+          const rect = element.getBoundingClientRect();
+          
+          if(rect.top.toFixed() < 259){
+              setName('visibleform');
+          }else{
+              setName('hideform');
+          }   
+      }
+  };
+
+
+  useEffect(() => {
+    window.addEventListener('scroll', getPosition);
+}, []);
+
+
+function chapterScroll2(id) {
+  var element = document.getElementById(id);
+  var headerOffset = 140;
+  var elementPosition = element.getBoundingClientRect().top;
+  var offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+  window.scrollTo({
+    top: offsetPosition,
+    behavior: "smooth"
+  });
+}
+
+
     const [rendercount, setRenderCount] = useState(() => false);
     const settings1 = {
         infinite: true,
@@ -107,6 +141,11 @@ function futuresOptionsTradingBanner() {
                                     <DematAccountForm />
                                 </GoogleReCaptchaProvider>
                             </div>
+                        </div>
+                    </div>
+                    <div className={name}>
+                        <div className="d-flex justify-content-center btn-view-more-sticky  mt-5 btn-fixed">
+                            <button className=" primary-orange-btn scroll-top-account openbtn"  onClick={()=>{chapterScroll2('dematform')}}>Open Free Account</button>
                         </div>
                     </div>
 
