1ad77a3a8f89465c214d6b0ba2fa7c427377aa0e
Jagruti <jagruti.palkar@choicetechlab.com>
2023-05-24 17:49:09 +0530

    changes

--- src/components/Free-Demat-Campaign/Banner.jsx
+++ src/components/Free-Demat-Campaign/Banner.jsx
@@ -48,32 +48,32 @@ function Banner() {
                         <span className="all-account">All in One Account</span>
                     </div>
                 </div>
-                <div className="col-lg-6 col-md-7">
+                <div className="col-lg-7 col-md-7">
                     <div className="free-banner-caption">
                         <h1 className="big-ttl">Free Demat Account<br /> Opening in <span className="clock">5</span> Minutes </h1>
                         <div className="campaign-banner-features">
                             <div className="campaign-features-list">
                                 <span className="campaign-img">
-                                    <LazyLoader src={NoAmc} className={'img-fluid'} width={"93"} height={"82"} alt={"Free Demat Account with No Annual Charges"}/>
+                                    <LazyLoader src={NoAmc} className={'img-fluid'} height={"93"} width={"106"} alt={"Free Demat Account with No Annual Charges"}/>
                                 </span>
                                 <h3>No AMC for <br/>First Year</h3>
                             </div>
                             <div className="campaign-features-list">
                                 <span className="campaign-img">
-                                    <LazyLoader src={ResearchCalls} className={'img-fluid'} width={"90"} height={"85"} alt={"Free Demat Account with Expert Research Calls"}/>
+                                    <LazyLoader src={ResearchCalls} className={'img-fluid'} height={"90"} width={"102"} alt={"Free Demat Account with Expert Research Calls"}/>
                                 </span>
                                 <h3>Expert <br/>Research Calls</h3>
                             </div>
                             <div className="campaign-features-list">
                                 <span className="campaign-img">
-                                    <LazyLoader src={ResearchTools} className={'img-fluid'} width={"93"} height={"82"} alt={"Open Demat Account with Free Research Tools"}/>
+                                    <LazyLoader src={ResearchTools} className={'img-fluid'} height={"93"} width={"106"} alt={"Open Demat Account with Free Research Tools"}/>
                                 </span>
                                 <h3>Free <br/>Research Tools</h3>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="col-lg-6 col-md-5">
+                <div className="col-lg-5 col-md-5">
                     <div className="d-flex justify-content-end" id="campaignForm">
                         <GoogleReCaptchaProvider reCaptchaKey="6Lc9qf4hAAAAABMa3-oFLk9BAkvihcEhVHnnS7Uz">
                             <DematAccountForm />
