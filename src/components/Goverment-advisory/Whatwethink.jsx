e276c3a77b4ea152d4c634ca662271347fa66d92
Jagruti <jagruti.palkar@choicetechlab.com>
2023-06-17 13:31:29 +0530

    chnges

--- src/components/Goverment-advisory/Whatwethink.jsx
+++ src/components/Goverment-advisory/Whatwethink.jsx
@@ -18,19 +18,19 @@ function Whatwethink() {
             <div className="col-md-12 ">
               <h1 className="title-first sectitle">What we think</h1>
               <p className="mainpara" >
-                Our unique services till last mile and with value addition were always appreciated in last one decade. It was our service efforts that we grew from one state to 15 States and from 3 employee to 1000 employees in this vertical.
+              Our unique services till last mile and with value addition were always appreciated in the last one decade. It was our service efforts that we grew from one state to 15 States and from 3 employees to 1000 employees in this vertical.
                 </p>
                 <p className="mainpara" >
-                Social development sector is directly connected with under privileged, underserved citizen, here small efforts deliver a major changes.
+                Social development sector is directly connected with under privileged, underserved citizens, where small efforts deliver major changes.
                 </p>
                 <p className="mainpara" >
-                Although lots of NGOs and institutions is working under the area but still there is need of professional agencies like us that can channelize exports, design specific solutions with intention of public services.
+                Although lots of NGOs and institutions are working in the area, there is still a need for professional agencies like us that can channelize expertise and design specific solutions with the intention of public service.
                 </p>
                 <p className="mainpara" >
-                Various collector office in tribal dominated zone specially in Chhattisgarh and Jharkhand has appreciated our idea of inclusive solutions and ready to avoid us various orders.
+                Various collector offices in tribal dominated zones specially in Chhattisgarh and Jharkhand have appreciated our idea of inclusive solutions and are ready to award us various orders.
                 </p>
                 <p className="mainpara" >
-                Yes. deployment of required resources at remote area and credential matching is challenge for us, but by partnering with noble institutions we will bridge this gap.
+                Yes, deployment of required resources at remote areas and credential matching is a challenge for us, but by partnering with noble institutions we will bridge this gap.
                 </p>
             </div>
           </div>
