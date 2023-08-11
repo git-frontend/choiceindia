8bea46833106048efa7abf87e9e5294b559a9e92
satyavendra singh <satyavendra.singh@choicetechlab.com>
2023-08-11 11:48:32 +0530

    working on new changes on 10 august

--- src/components/SubBroker/BannerSBrokerCampaign.jsx
+++ src/components/SubBroker/BannerSBrokerCampaign.jsx
@@ -32,7 +32,7 @@ function BannerSBroker() {
                                 <div className="left-sec">
                                 <h1 className="tlt1"><span className="tlt2">Start Sub Broker</span>
                                     Franchise
-                                    <span className="tlt2">with Low Deposit*</span></h1>
+                                    <span className="tlt2">with Choice</span></h1>
                                 </div>
                                 <div className="right-sec">
                                     <LazyLoader src={SubBannerimage} alt={"Sub Broker Franchise Without Deposit"} className={"img-fluid"} width={"400"} height={"391"} />
@@ -45,7 +45,7 @@ function BannerSBroker() {
                                         <span>Highest</span> Revenue Sharing
                                     </li>
                                     <li>
-                                        <span>Upto ₹ 2100 Per</span> Account Incentive*
+                                        <span>Low</span> Deposit
                                     </li>
                                     <li>
                                         <span>Lifetime</span> Income Source
