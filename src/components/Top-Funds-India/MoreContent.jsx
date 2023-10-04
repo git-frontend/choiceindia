be5b3a4f9621f6893b53ef78c802199c384bf7a2
satyavendra singh <satyavendra.singh@choicetechlab.com>
2023-10-04 13:45:55 +0530

    working on elss details page

--- src/components/Top-Funds-India/MoreContent.jsx
+++ src/components/Top-Funds-India/MoreContent.jsx
@@ -5,9 +5,13 @@ function MoreContent() {
     <>
     <section className='more-content-sec'>
         <div className='container'>
+          <div className='row'>
+                <div className='col-md-12'>
+                    <h2 className='title-first'>Invest Online in the <span>Best Mutual Funds</span> in India</h2>
+                </div>
+            </div>
             <div className='row justify-content-center'>
                 <div className='col-md-11'>
-                    <h2 className='title-first'>Invest Online in the <span>Best Mutual Funds</span> in India</h2>
                     <p className='seo-para'>Following the reclassification of mutual funds, across categories of mutual funds, we bring you the Top Equity Mutual Funds and Top Debt Mutual Funds which will help you experience 'The Joy of Earning.' Each of these categories have recommendations for you to easily invest in them for achieving your goal of Mutual Fund Investments! To make your process of investing further easy, once you visit a particular category, you will be able to find 'Our Ratings' on each of the Scheme in that category along with a 'Most Invested' tag in that category! So, take no stress, begin your journey of Investing in the Top Mutual Funds in India across any of the categories.</p>
                 </div>
             </div>
