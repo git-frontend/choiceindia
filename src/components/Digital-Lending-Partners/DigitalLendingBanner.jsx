96453ece5a8c7535415bf02b739fbdc18810d431
satyavendra singh <satyavendra.singh@choicetechlab.com>
2023-10-09 11:34:43 +0530

    added new section in digital landing partner page

--- src/components/Digital-Lending-Partners/DigitalLendingBanner.jsx
+++ src/components/Digital-Lending-Partners/DigitalLendingBanner.jsx
@@ -4,6 +4,9 @@ import "./digitalpartner.scss";
 import choiceconnect from '../../assets/images/Digital-Lending-Partners/choice-connect.svg';
 import seedsfincap from '../../assets/images/Digital-Lending-Partners/seeds-fincap.svg';
 import cashinvoice from '../../assets/images/Digital-Lending-Partners/cashinvoice.svg';
+import carefi from '../../assets/images/Digital-Lending-Partners/care-fi.svg';
+import clubdiaries from '../../assets/images/Digital-Lending-Partners/clubdiaries.svg';
+import walkfin from '../../assets/images/Digital-Lending-Partners/walkfin.svg';
 import LazyLoader from '../Common-features/LazyLoader';
 
 import NBFCMenu from '../Common-features/NBFCMenu';
@@ -132,6 +135,52 @@ function DigitalLendingBanner() {
                     Cashinvoice leverages technology to transform supply chain finance. Their mission is to help corporates of all sizes unlock value locked within the supply chain to optimize their working capital more efficiently.
                   </p>
                   <a href="https://cashinvoice.in/" target="_blank"><span className="knw-btn">Know More</span></a>
+                </div>
+              </Slider>
+
+              {/* </div> */}
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              {/* <div className="carditem-list"> */}
+              <Slider {...settings} className="carditem-list">
+                <div className="digital-carditem digital-carditem2">
+                  <span className="digiimg2">
+                    <LazyLoader src={carefi} alt={"Choice Connect"} className={"img-fluid"} width={"183"} height={"59"} />
+
+                  </span>
+                  <h3 className="digi-card-title">
+                  Care.fi Technological Solutions Private Limited
+                  </h3>
+                  <p className="digi-subtext">
+                  Healthcare OS Made smart, ready for the future. Building a full stack credit-to-commerce play solving critical challenges in our entire healthcare ecosystem. They’re a passionate team sprinting to solve the most critical challenges in India’s healthcare sector, with founders armed with deep insights into the confluence of healthcare, finance and commerce.
+                  </p>
+                  <a href="https://www.carefi.in/" target="_blank"><span className="knw-btn">Know More</span></a>
+                </div>
+                <div className="digital-carditem digital-carditem2">
+                  <span className="digiimg2">
+                    <LazyLoader src={clubdiaries} alt={"Seeds Fincap"} className={"img-fluid"} width={"190"} height={"108"} />
+                  </span>
+                  <h3 className="digi-card-title">
+                  Clubdiaries Technologies Private Limited
+                  </h3>
+                  <p className="digi-subtext">
+                  They started NuShala to fill the gap in finances of private schools. Today, they are covering more than 20,000 students across schools to helps school owners run their school smoothly and drive growth through focussed execution. In the past 12 months, they have created a seamless product for schools in partnership with the leading financial institutions in India to solve the working capital and collection issues of schools for good.
+                  </p>
+                  <a href="https://www.nushala.com/" target="_blank"> <span className="knw-btn">Know More</span></a>
+                </div>
+                <div className="digital-carditem digital-carditem2">
+                  <span className="digiimg2">
+                    <LazyLoader src={walkfin} alt={"Cashinvoice"} className={"img-fluid"} width={"263"} height={"80"} />
+                  </span>
+                  <h3 className="digi-card-title">
+                  Walkfin Financing Services Private Limited
+                  </h3>
+                  <p className="digi-subtext">
+                  WALKfin Financing Services Private Limited is a new age fintech company offering quick, hassle-free and safe lending solutions to businesses in the form of Distributor Credit and Retailer Credit. Their efforts are to financing the needs of FMCG distributors and Retailers in the underserved areas of our economy. This way, they hope to address the credit issue plaguing our country and help both FMCG distributors and retailers in securing their future.
+                  </p>
+                  <a href="https://walkfin.com/" target="_blank"><span className="knw-btn">Know More</span></a>
                 </div>
               </Slider>
 
