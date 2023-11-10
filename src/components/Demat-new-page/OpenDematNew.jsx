83adc0bafdc01a12d4fea3f76e8522b0df1a31fd
Swapnil <swapnil.nevarekar@choicetechlab.com>
2023-11-10 18:19:01 +0530

    create new page for demat

--- src/components/Demat-new-page/OpenDematNew.jsx
+++ src/components/Demat-new-page/OpenDematNew.jsx
@@ -27,6 +27,7 @@ import pledgeshares from '../../assets/images/demat-images/new-demat/how-to-pled
 import addnominee from '../../assets/images/demat-images/new-demat/how-to-add-nominee-to-demat-account.webp';
 import closedemat from '../../assets/images/demat-images/new-demat/how-to-close-demat-account.webp';
 import closureDemat from '../../assets/images/demat-images/new-demat/demat-account-closing-process.webp';
+import meta_tags from "../../Data/MetaTags";
 
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -42,6 +43,7 @@ function OpenDematNew() {
 
     const [isActive, setIsActive] = useState();
     const [isActive4, setIsActive4] = useState('id');
+    const [rendercount, setRenderCount] = useState(() => false);
 
     const [isActive2, setIsActive2] = useState(false);
     
@@ -93,6 +95,32 @@ function OpenDematNew() {
             behavior: "smooth"
         });
     }
+
+    useEffect(() => {
+        setRenderCount(true)
+        if (rendercount === true) {
+          // let parser = new DOMParser();
+          // let doc = parser.parseFromString(meta_tags['sub-broker'].faqscript, 'text/html');
+          // document.body.appendChild(doc.getElementsByTagName('script')[0]? doc.getElementsByTagName('script')[0]: '' );
+          document.title = meta_tags[location.pathname.replace('/', "")] ? meta_tags[location.pathname.replace('/', "")].title : '';
+          // document.getElementById('meta-tags').name= meta_tags[location.pathname.replace('/',"")]? meta_tags[location.pathname.replace('/',"")].title : ''  ;
+          document.getElementById('meta-tags').content = meta_tags[location.pathname.replace('/', "")] ? meta_tags[location.pathname.replace('/', "")].content : '';
+          document.getElementById('canonical-link').href = meta_tags[location.pathname.replace('/', "")] ? meta_tags[location.pathname.replace('/', "")].link : '';
+          document.getElementById('language').lang = meta_tags[location.pathname.replace('/', "")] ? meta_tags[location.pathname.replace('/', "")].lang : '';
+          if(!(document.getElementById('link1')==null)){
+          
+            document.getElementById('link1').remove();
+          document.getElementById('link2').remove();
+          document.getElementById('link3').remove();
+          document.getElementById('link4').remove();
+          document.getElementById('link5').remove();
+          document.getElementById('link6').remove();
+        
+          
+        }
+          
+        }
+      }, [rendercount])
 
     
     return (
