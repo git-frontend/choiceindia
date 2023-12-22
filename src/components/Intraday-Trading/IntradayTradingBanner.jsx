6373162ef30ffaa60a35e155505bdb7691419339
satyavendra singh <satyavendra.singh@choicetechlab.com>
2023-12-22 18:42:05 +0530

    changes in intradaty trading

--- src/components/Intraday-Trading/IntradayTradingBanner.jsx
+++ src/components/Intraday-Trading/IntradayTradingBanner.jsx
@@ -20,12 +20,12 @@ function IntradayTradingBanner() {
             <section className="fno-banner intraday-banner">
                 <div className="container">
                     <div className="row gx-5 align-items-start">
-                        <div className="col-xl-6 col-md-7">
+                        <div className="col-xl-6 col-md-9">
                             <div className="ban-caption">
                                 <h5>Choice: Preferred by Intraday Traders</h5>
                                 <h1 className="title">Supercharge Your <br/> <span>Intraday</span> Trading</h1>
                                 <p className="para">Maximize your intraday profits with advanced tools and personalized services.</p>
-                                <a href="javascript:void(0)" className="btn-bg btn-bg-dark" onClick={() => scrollToId('campaignForm')}>Start Today</a>
+                                <a href="" className="btn-bg btn-bg-dark" onClick={() => scrollToId('campaignForm')}>Start Today</a>
                             </div>
                         </div>
                     </div>
@@ -33,29 +33,20 @@ function IntradayTradingBanner() {
                         <div className="col-xl-8 col-md-12">
                             <div className="inner-count">
                                 <div className="count-itm">
-                                    <h3><span class="hvr-txt"><span>25</span></span><small>+</small></h3>
+                                    <h3><span className="hvr-txt"><span>25</span></span><small>+</small></h3>
                                     <h5>Year of Experience</h5>
                                 </div>
                                 <div className="count-itm">
-                                    <h3><span class="hvr-txt"><span>104</span></span><small>+</small></h3>
+                                    <h3><span className="hvr-txt"><span>104</span></span><small>+</small></h3>
                                     <h5>Local Branches</h5>
                                 </div>
                                 <div className="count-itm">
-                                    <h3><span class="hvr-txt"><span>850K</span></span><small>+</small></h3>
+                                    <h3><span className="hvr-txt"><span>850K</span></span><small>+</small></h3>
                                     <h5>Happy Clients</h5>
                                 </div>
                             </div>
                         </div>
                    </div>
-                </div>
-            </section>
-            <section className="count-fno-members">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-12">
-                           
-                        </div>
-                    </div>
                 </div>
             </section>
 
