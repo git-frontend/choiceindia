7b6f9a93043d764f97d58c6677d7a778f4a08990
jayashri <jayashri.yadav@choicetechlab.com>
2024-01-12 18:33:45 +0530

    changes done on corporate demat account

--- src/components/Corporate-Demat-Account/TypesofCorporate.jsx
+++ src/components/Corporate-Demat-Account/TypesofCorporate.jsx
@@ -10,7 +10,7 @@ function TypesofCorporate() {
             <section className="types-of-corporate why-corporate-account">
                 <div className="container">
                     <div className="row">
-                        <div className="col-md-11">
+                        <div className="col-md-11 why-corporate-top">
                             <div className="why-corporate-heading text-center">
                                 <h2 className="title-secnd">Types of Corporate Demat Accounts</h2>
                                 <p className="why-corporate-para1">Choice offers different Corporate Demat account services for your business's varied needs. Our comprehensive services for corporates include research calls, investment services, wealth management, and portfolio management to help you build an optimal portfolio.</p>
