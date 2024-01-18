3bc2a3d3910c74a390bb8963d53220c32b4bc465
jayashri <jayashri.yadav@choicetechlab.com>
2024-01-18 16:33:38 +0530

    api key changed for startegy and faq

--- src/Services/faqService.js
+++ src/Services/faqService.js
@@ -4,7 +4,7 @@ import { API_URLS } from "./API-URLS";
 const faqService = {
 
     headerConfig: {
-        headers: { "authorization": "Basic Z0h2bnRuWjhCajhZeXQ3TjFYVA==" }
+        headers: { "authorization": "Basic WXp4cUZXZGJVdzlYOGVhMVBHejpY" }
     },
 
     FaqCategory: function () {
