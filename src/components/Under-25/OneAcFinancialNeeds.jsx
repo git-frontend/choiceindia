7736255c8fd6c84a961ef4509d9011933d306039
satyavendra singh <satyavendra.singh@choicetechlab.com>
2024-03-21 18:56:03 +0530

    pay only order section done

--- src/components/Under-25/OneAcFinancialNeeds.jsx
+++ src/components/Under-25/OneAcFinancialNeeds.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import  Avtar  from '../../assets/images/under-25/avtar.png';
 import  stocks  from '../../assets/images/under-25/stocks.png';
+import  DLF  from '../../assets/images/under-25/dlf.png';
+import  Reliance  from '../../assets/images/under-25/reliance.png';
+import  AdaniPower  from '../../assets/images/under-25/adani-power.png';
+import  Voltas  from '../../assets/images/under-25/voltas.png';
+import  Sonata  from '../../assets/images/under-25/sonata.png';
+import  eicher  from '../../assets/images/under-25/eicher-moters.png';
+import  mf  from '../../assets/images/under-25/mutual-fund.png';
 function OneAcFinancialNeeds() {
   return (
     <>
@@ -19,6 +26,38 @@ function OneAcFinancialNeeds() {
                         <div className='banner-caption'>
                             <h3>You name the stocks</h3>
                             <p>With over 5000+ stocks & 100+ ETFs, from established names like Reliance and Tata to the next big thing, own shares of some of the most popular companies.</p>
+                        </div>
+                        <div className='icons icon-1'>
+                            <img src={eicher} alt="Eicher Moter" height={121} width={122} className="img-fluid"/>
+                        </div>
+                        <div className='icons icon-2'>
+                            <img src={DLF} alt="DLF" height={121} width={122} className="img-fluid"/>
+                        </div>
+                        <div className='icons icon-3'>
+                            <img src={Reliance} alt="Reliance" height={121} width={122} className="img-fluid"/>
+                        </div>
+                        <div className='icons icon-4'>
+                            <img src={AdaniPower} alt="Adani Power" height={121} width={122} className="img-fluid"/>
+                        </div>
+                        <div className='icons icon-5'>
+                            <img src={Voltas} alt="Voltas" height={121} width={122} className="img-fluid"/>
+                        </div>
+                        <div className='icons icon-6'>
+                            <img src={Sonata} alt="Sonata" height={121} width={122} className="img-fluid"/>
+                        </div>
+                    </div>
+                    <div className='mutual-funds-order'>
+                        <div className='left-sec'>
+                            <div className='mf-funds'>
+                                <img src={mf} width={587} height={503} className="img-fluid ban-mf"/>
+                                <div className='mf-caption'>
+                                    <h3>Mutual funds ain't your grandpa's thing anymore.</h3>
+                                    <p>Build your future with compounding power, right from your phone.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='right-sec'>
+                            <div className='orders'></div>
                         </div>
                     </div>
                 </div>
