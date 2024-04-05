e7f1eb806d100126fa27506663b1b7eba10bc853
satyavendra singh <satyavendra.singh@choicetechlab.com>
2024-04-05 14:47:09 +0530

    lead form page review changes  html done

--- src/components/Lead-Form/LeadForm.jsx
+++ src/components/Lead-Form/LeadForm.jsx
@@ -25,39 +25,92 @@ function LeadForm() {
                <div className='form-card'>
                   <p>You are :</p>
                   <div className='radio-btn-sec'>
-                      <div class="rdio"> 
-                        <input name="radio" value="1" id="radio1" type="radio" checked/>
-                        <label for="radio1">Customer</label>
+                      <div className="rdio"> 
+                        <input name="radio" value="1" id="radio1" type="radio" defaultChecked/>
+                        <label htmlFor="radio1">Customer</label>
                       </div>
-                      <div class="rdio"> 
-                        <input name="radio" value="2" id="radio2" type="radio" checked/>
-                        <label for="radio2">EPC</label>
+                      <div className="rdio"> 
+                        <input name="radio" value="2" id="radio2" type="radio" defaultChecked/>
+                        <label htmlFor="radio2">EPC</label>
                       </div>
-                      <div class="rdio">
+                      <div className="rdio">
                         <input name="radio" value="3" id="radio3" type="radio"/>
-                        <label for="radio3">Manufacturer/OEM</label>
+                        <label htmlFor="radio3">Manufacturer/OEM</label>
                       </div>
                     </div>
                </div>
             </div>
             <div className='form-section'>
-                <div className='left-form-sec'> 
-                    <FloatingLabel controlId="floatingNameofEntity" label="Name of Entity" className='input-label'>
+            <div className='dis-flex'>
+                <div className='flex-items'>
+                   <FloatingLabel controlId="floatingNameofEntity" label="Name of Entity" className='input-label'>
                       <Form.Control type="text" placeholder="Name of Entity" className='input-field'/>
                     </FloatingLabel>
-                    <FloatingLabel controlId="floatingNameofEntity" label="Entity Type" className='input-label'>
+                </div>
+                <div className='flex-items'>
+                <FloatingLabel controlId="floatingNameofEntity" label="Business Address" className='input-label'>
+                      <Form.Control type="text" placeholder="Business Address" className='input-field'/>
+                    </FloatingLabel>
+                </div>
+                <div className='flex-items'>
+                <FloatingLabel controlId="floatingNameofEntity" label="Entity Type" className='input-label'>
                       <Select placeholder={false} className="input-field formselect" searchable={false} options={options} value="Proprietorship" style={{ 'fontSize': 'large' }} />
                       
                     </FloatingLabel>
-                    <FloatingLabel controlId="floatingNameofEntity" label="Contact Person" className='input-label'>
+                </div>
+                <div className='flex-items'>
+                <FloatingLabel controlId="floatingNameofEntity" label="Residence Address" className='input-label mandate-none'>
+                      <Form.Control type="text" placeholder="Residence Address" className='input-field'/>
+                    </FloatingLabel>
+                </div>
+                <div className='flex-items'>
+                <FloatingLabel controlId="floatingNameofEntity" label="Contact Person" className='input-label'>
                       <Form.Control type="text" placeholder="Contact Person" className='input-field'/>
                     </FloatingLabel>
-                    <FloatingLabel controlId="floatingNameofEntity" label="Mobile No/WhatsApp No" className='input-label'>
+                </div>
+                <div className='flex-items'>
+
+                <FloatingLabel controlId="floatingNameofEntity" label="Sales Turnover of latest FY (Rs In Lakh)" className='input-label mandate-none'>
+                      <Form.Control type="text" placeholder="Sales Turnover of latest FY (Rs In Lakh)" className='input-field'/>
+                    </FloatingLabel>
+                </div>
+                <div className='flex-items'>
+                <FloatingLabel controlId="floatingNameofEntity" label="Mobile No/WhatsApp No" className='input-label'>
                       <Form.Control type="text" placeholder="Mobile No/WhatsApp No" className='input-field'/>
                     </FloatingLabel>
-                    <FloatingLabel controlId="floatingNameofEntity" label="Mail ID" className='input-label'>
+                </div>
+                <div className='flex-items'>
+                <FloatingLabel controlId="floatingNameofEntity" label="Installed capacity of Latest FY (KW/MW)" className='input-label mandate-none'>
+                      <Form.Control type="text" placeholder="Installed capacity of Latest FY (KW/MW)" className='input-field'/>
+                    </FloatingLabel>
+                </div>
+                <div className='flex-items'>
+                <FloatingLabel controlId="floatingNameofEntity" label="Mail ID" className='input-label'>
                       <Form.Control type="text" placeholder="Mail ID" className='input-field'/>
                     </FloatingLabel>
+                </div>
+                <div className='flex-items'>
+                <FloatingLabel controlId="floatingTextarea2" label="Dealing with Manufacturers/Brands" className='textarea-label'>
+                      <Form.Control
+                      className='input-field textarea'
+                        as="textarea"
+                        placeholder="Dealing with Manufacturers/Brands"
+                        // style={{ height: '150px' }}
+                      />
+                    </FloatingLabel>
+                </div>
+                <div className='flex-items'>
+                <FloatingLabel controlId="floatingTextarea2" label="Write your message" className='textarea-label'>
+                      <Form.Control
+                      className='input-field'
+                        as="textarea"
+                        placeholder="Write your message"
+                        style={{ height: '150px' }}
+                      />
+                    </FloatingLabel>
+                </div>
+            </div>
+                <div className='col-md-12'> 
                       <div key="inline-checkbox" className="cust-checkbox">
                           <Form.Check inline name="terms_and_conditions" type="checkbox">
                               <Form.Check.Input type="checkbox" />
@@ -67,38 +120,8 @@ function LeadForm() {
                           </Form.Check>
                       </div>
                       <div className='btn-submit-sec'>
-                          <button type="submit" class="btn-bg btn-bg-dark btnsubmit">Submit</button>
+                          <button type="submit" className="btn-bg btn-bg-dark btnsubmit">Submit</button>
                       </div>
-                </div>
-                <div className='right-form-sec'> 
-                    <FloatingLabel controlId="floatingNameofEntity" label="Business Address" className='input-label'>
-                      <Form.Control type="text" placeholder="Business Address" className='input-field'/>
-                    </FloatingLabel>
-                    <FloatingLabel controlId="floatingNameofEntity" label="Residence Address" className='input-label mandate-none'>
-                      <Form.Control type="text" placeholder="Residence Address" className='input-field'/>
-                    </FloatingLabel>
-                    <FloatingLabel controlId="floatingNameofEntity" label="Sales Turnover of latest FY (Rs In Lakh)" className='input-label mandate-none'>
-                      <Form.Control type="text" placeholder="Sales Turnover of latest FY (Rs In Lakh)" className='input-field'/>
-                    </FloatingLabel>
-                    <FloatingLabel controlId="floatingNameofEntity" label="Installed capacity of Latest FY (KW/MW)" className='input-label mandate-none'>
-                      <Form.Control type="text" placeholder="Installed capacity of Latest FY (KW/MW)" className='input-field'/>
-                    </FloatingLabel>
-                    <FloatingLabel controlId="floatingTextarea2" label="Dealing with Manufacturers/Brands" className='textarea-label'>
-                      <Form.Control
-                      className='input-field textarea'
-                        as="textarea"
-                        placeholder="Dealing with Manufacturers/Brands"
-                        style={{ height: '150px' }}
-                      />
-                    </FloatingLabel>
-                    <FloatingLabel controlId="floatingTextarea2" label="Write your message" className='textarea-label'>
-                      <Form.Control
-                      className='input-field'
-                        as="textarea"
-                        placeholder="Write your message"
-                        style={{ height: '200px' }}
-                      />
-                    </FloatingLabel>
                 </div>
             </div>
           </div>
