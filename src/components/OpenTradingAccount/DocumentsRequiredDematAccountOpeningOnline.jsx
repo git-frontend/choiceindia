a323a3bb60b7a974ce12c32a3c7830f380356284
Shubham Palav <palavshubham09@gmail.com>
2024-04-30 18:39:01 +0530

    Changed the structure of trading-account page

---
+++ src/components/OpenTradingAccount/DocumentsRequiredDematAccountOpeningOnline.jsx
@@ -0,0 +1,148 @@
+
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import LazyLoader from '../Common-features/LazyLoader';
+import Image1 from '../../assets/images/free-demat-compaign/identity-proof-to-open-free-demat-account.svg';
+import Image2 from '../../assets/images/free-demat-compaign/address-proof-to-open-demat-account.svg';
+import Image3 from '../../assets/images/free-demat-compaign/income-proof-to-open-a-demat-account.svg';
+import Image4 from '../../assets/images/free-demat-compaign/bank-proof-to-open-a-free-demat-account.svg';
+import Image5 from '../../assets/images/free-demat-compaign/upload-signature-to-complete-demat-account-opening-process.svg';
+
+function DocumentsRequiredDematAccountOpeningOnline() {
+  const [toggleState, setToggleState] = useState(1);
+  const [data, setData] = useState(0);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+
+  return (
+    <div>
+    <section className="documents-required-sec">
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-12">
+                   <h2 className="title-first text-center"><span>Documents Required</span> for Opening Trading Account</h2>
+                </div>
+                <div className="col-xl-6 col-md-8">
+                  <div className="scroll-mobile">
+                    <ul className="list-group list_group1">
+                        <li className= {toggleState === 1 ? "list-group-item list listsec" : "list-group-item list"}
+                    onClick={() => { toggleTab(1); setData(0) }} >Non KRA Verified Users</li>
+                        <li className= {toggleState === 2 ? "list-group-item list listsec" : "list-group-item list"}
+                    onClick={() => { toggleTab(2); setData(2) }} >KRA Verified Users </li>
+                      </ul>
+                  </div>
+                </div>
+            </div>
+            <div className="content-tabs active-content">
+              <div className={toggleState === 1 ? "content  active-content" : "content"}>
+                <div className="row justify-content-between">
+                  <div className="col-md-12">
+                    <div className="doc-rec-list">
+                      <div className="comman-sec">
+                        <div className="doc-rec-details">
+                          <div className="doc-img">
+                            <LazyLoader src={Image1} className={'img-fluid'} width={"100"} height={"100"} alt={"Identity Proof to Open Free Demat Account"}/>
+                          </div>
+                        <div className="doc-caption">
+                          <h3 className="count">Identity Proof</h3>
+                          <p>PAN Card is mandatory for Demat Account Opening.</p>
+                        </div>
+                  </div>
+                  <div className="doc-rec-details">
+                      <div className="doc-img">
+                        <LazyLoader src={Image2} className={'img-fluid'} width={"100"} height={"100"} alt={"Address Proof to Open Demat Account"}/>
+                      </div>
+                      <div className="doc-caption">
+                        <h3 className="count count-2">Address Proof</h3>
+                        <p>Aadhaar Card, Passport, Voter ID, Driving Licence.</p>
+                      </div>
+                  </div>
+                  <div className="doc-rec-details">
+                      <div className="doc-img">
+                        <LazyLoader src={Image3} className={'img-fluid'} width={"100"} height={"100"} alt={"Income Proof to Open a Demat Account"}/>
+                      </div>
+                      <div className="doc-caption">
+                        <h3 className="count count-3">Income Proof</h3>
+                        <p>Bank Statement or ITR (Required for F&O and Currency Trading)</p>
+                      </div>
+                  </div>
+                </div>
+                <div className="comman-sec">
+                <div className="doc-rec-details">
+                      <div className="doc-img">
+                        <LazyLoader src={Image4} className={'img-fluid'} width={"100"} height={"100"} alt={"Bank Proof to Open a Free Demat Account"}/>
+                      </div>
+                      <div className="doc-caption">
+                        <h3 className="count count-4">Bank Proof</h3>
+                        <p>Bank Account Number, IFSC Code Or Cancelled Cheque  (that captures the MICR Code)</p>
+                      </div>
+                  </div>
+                  <div className="doc-rec-details">
+                      <div className="doc-img">
+                        <LazyLoader src={Image5} className={'img-fluid'} width={"100"} height={"100"} alt={"Upload Signature to Complete Demat Account Opening Process"}/>
+                      </div>
+                      <div className="doc-caption">
+                        <h3 className="count count-5">Signature</h3>
+                        <p>Upload the photo of your signature, while completing your demat account opening process.</p>
+                      </div>
+                  </div>
+                </div>
+            </div>
+            </div>
+            </div>
+              </div>
+              <div className={toggleState === 2 ? "content  active-content" : "content"}>
+              <div className="row justify-content-between">
+                  <div className="col-md-12">
+              <div className="doc-rec-list">
+                    <div className="comman-sec">
+                      <div className="doc-rec-details">
+                          <div className="doc-img">
+                          <LazyLoader src={Image3} className={'img-fluid'} width={"100"} height={"100"} alt={"Income Proof"}/>
+                          </div>
+                          <div className="doc-caption">
+                            <h3 className="count">Income Proof</h3>
+                            <p>Bank Statement or ITR (Required for F&O and Currency Trading)</p>
+                          </div>
+                      </div>
+                      <div className="doc-rec-details">
+                          <div className="doc-img">
+                            <LazyLoader src={Image4} className={'img-fluid'} width={"100"} height={"100"} alt={"Bank Proof"}/>
+                          </div>
+                          <div className="doc-caption">
+                            <h3 className="count count-2">Bank Proof</h3>
+                            <p> Bank Account Number, IFSC Code Or Cancelled Cheque  (that captures the MICR Code)</p>
+                          </div>
+                      </div>
+                    </div>
+                    <div className="comman-sec">
+                      <div className="doc-rec-details">
+                          <div className="doc-img">
+                            <LazyLoader src={Image5} className={'img-fluid'} width={"100"} height={"100"} alt={"Signature Proof"}/>
+                          </div>
+                          <div className="doc-caption">
+                            <h3 className="count count-3">Signature</h3>
+                            <p>Upload the photo of your signature, while completing your demat account opening process.</p>
+                          </div>
+                      </div>
+                    </div>
+                </div>
+              </div>
+          </div>
+          </div>
+          </div>
+          </div>
+    </section>
+
+    
+
+   
+    
+    </div>
+  );
+}
+
+export default DocumentsRequiredDematAccountOpeningOnline;
---
+++ src/components/OpenTradingAccount/OpenAccountEasySteps.jsx
@@ -0,0 +1,189 @@
+import React from 'react';
+import LazyLoader from '../Common-features/LazyLoader';
+import EasyStepsLine from '../../assets/images/free-demat-compaign/easy-steps-bg.webp';
+
+function OpenAccountEasySteps() {
+    function updateActiveStep(index) {
+        const elements = document.querySelectorAll(".display-flex");
+        elements.forEach((element, i) => {
+          if (i === index) {
+            element.classList.add("steps-active");
+          } else {
+            element.classList.remove("steps-active");
+          }
+        });
+      }
+      let currentIndex = 0;
+      function autoUpdateStep() {
+        updateActiveStep(currentIndex);
+        currentIndex = (currentIndex + 1) % document.querySelectorAll(".display-flex").length;
+      }
+      const intervalId = setInterval(autoUpdateStep, 5000);
+
+
+      function chapterScroll2(id) {
+        var element = document.getElementById(id);
+        var headerOffset = 140;
+        var elementPosition = element.getBoundingClientRect().top;
+        var offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+        window.scrollTo({
+          top: offsetPosition,
+          behavior: "smooth"
+        });
+      }
+
+  return (
+    <>
+        <section className='easy-steps-sec'>
+            <div className='container'>
+                 <div className='row'>
+                    <div className="col-md-12">
+                        <div className='text-center'>
+                            <h2 className="title-first">How to Open  <span>Trading Account</span> in Easy Steps</h2>
+                             <p>Know the comprehensive step-by-step account opening procedure by <a href="https://choiceindia.com/assets/downloads/Guidance-for-Online-Account-Opening.pdf" target="_blank">visiting this page.</a>
+                            </p>
+                        </div>
+                    </div>
+                    <div className='col-md-12'>
+                        <div className='main-easy-div'>
+                            <div className='easy-steps-bg'>
+                                <div className='steps-line'>
+                                    <LazyLoader src={EasyStepsLine} className="img-fluid middle-brd-img"></LazyLoader>
+                                    <ul>
+                                <li className='numone display-flex steps-active'>
+                                    <div className='easy-text mr-b'>
+                                    PAN & DOB
+                                    </div>
+                                    <div className='number'>
+                                        <span className='number-text'>01</span>
+                                        <div className='circle-big'>
+                                        <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <circle className='progress' cx="40" cy="40" r="39" stroke="#0CBCB7" stroke-width="2" stroke-miterlimit="2.56141"/>
+                                            </svg>
+
+                                        </div>
+                                    </div>
+                                   
+                                </li>
+                                <li className='numtwo display-flex'>
+                                    <div className='number mr-b'>
+                                        <span className='number-text'>02</span>
+                                        <div className='circle-big'>
+                                        <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <circle className='progress' cx="40" cy="40" r="39" stroke="#0CBCB7" stroke-width="2" stroke-miterlimit="2.56141"/>
+                                        </svg>
+
+                                    </div>
+                                    </div>
+                                    <div className='easy-text'>
+                                    Basic Details
+                                    </div>
+                                    
+                                </li>
+                                <li className='numthree display-flex'>
+                                    <div className='easy-text mr-b'>
+                                    Bank Details
+                                    </div>
+                                    <div className='number'>
+                                        <span className='number-text'>03</span>
+                                        <div className='circle-big'>
+                                    <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <circle className='progress' cx="40" cy="40" r="39" stroke="#0CBCB7" stroke-width="2" stroke-miterlimit="2.56141"/>
+                                    </svg>
+
+                                    </div>
+                                    </div>
+                                    
+                                </li>
+                                <li className='numfourth display-flex'>
+                                    <div className='number mr-b'>
+                                        <span className='number-text'>04</span>
+                                        <div className='circle-big'>
+                                            <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <circle className='progress' cx="40" cy="40" r="39" stroke="#0CBCB7" stroke-width="2" stroke-miterlimit="2.56141"/>
+                                            </svg>
+                                    </div>
+                                    </div>
+                                    <div className='easy-text'>
+                                    IPV Details
+                                    </div>
+                                    
+                                </li>
+                                <li className='numfive display-flex'>
+                                    <div className='easy-text mr-b'>
+                                    Documents Upload
+                                    </div>
+                                    <div className='number'>
+                                    <span className='number-text'>05</span>
+                                    <div className='circle-big'>
+                                        <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <circle className='progress' cx="40" cy="40" r="39" stroke="#0CBCB7" stroke-width="2" stroke-miterlimit="2.56141"/>
+                                        </svg>
+                                    </div>
+                                    </div>
+                                    
+                                </li>
+                                <li className='numsix display-flex'>
+                                    <div className='number mr-b'>
+                                        <span className='number-text'>06</span>
+                                        <div className='circle-big'>
+                                    <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <circle className='progress' cx="40" cy="40" r="39" stroke="#0CBCB7" stroke-width="2" stroke-miterlimit="2.56141"/>
+                                    </svg>
+
+                                    </div>
+                                    </div>
+                                    <div className='easy-text'>
+                                    Nominee Details
+                                    </div>
+                                   
+                                </li>
+                                <li className='numseven display-flex'>
+                                    <div className='easy-text mr-b'>
+                                    eSign
+                                    </div>
+                                    <div className='number'>
+                                     <span className='number-text'>07</span>    
+                                     <div className='circle-big'>
+                                    <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <circle className='progress' cx="40" cy="40" r="39" stroke="#0CBCB7" stroke-width="2" stroke-miterlimit="2.56141"/>
+                                    </svg>
+
+                                    </div>
+                                    </div>
+                                    
+                                </li>
+                                <li className='numeight display-flex'>
+                                    <div className='number mr-b'>
+                                        <span className='number-text'>08</span>
+                                        <div className='circle-big'>
+                                    <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <circle className='progress' cx="40" cy="40" r="39" stroke="#0CBCB7" stroke-width="2" stroke-miterlimit="2.56141"/>
+                                    </svg>
+
+                                    </div>
+                                    </div>
+                                    <div className='easy-text'>
+                                    Submit DDPI
+                                    </div>
+                                    
+                                </li>
+                            </ul>
+                                </div>
+                           
+                            </div>
+                        </div>
+                        <div className="d-flex justify-content-center">
+                            <a className='cursor-pointer' onClick={()=>{chapterScroll2('dematform')}}>
+                                <span className="btn-bg btn-bg-dark">Open Free Account</span>
+                            </a>
+                        </div>
+                        </div>
+                    </div>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default OpenAccountEasySteps;
