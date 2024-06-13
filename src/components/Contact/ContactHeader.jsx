22e284b2510a95198229f3cf907b12e58da59325
trilochan.badal <trilochan.badal@choicetechlab.com>
2024-06-13 21:15:28 +0530

    changes temp

--- src/components/Contact/ContactHeader.jsx
+++ src/components/Contact/ContactHeader.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import "../../assets/css/header.scss"
 import MenuHeader from '../Common-features/MenuHeader';
 import { useLocation } from 'react-router-dom';
