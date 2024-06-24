14ebb8625e4b598e2d9823b84a8bc46cb3c1e817
jayashri <jayashri.yadav@choicetechlab.com>
2024-06-24 10:10:15 +0530

    revamp functions for demat account

--- src/components/Common-features/OpenAccountOTPModal.jsx
+++ src/components/Common-features/OpenAccountOTPModal.jsx
@@ -4,17 +4,13 @@ import Alert from 'react-bootstrap/Alert';
 import openAccountService from '../../Services/openAccountService';
 import OTPimage from '../../assets/images/otp.svg';
 import "../Common-features/demat-form.scss";
-import { Link } from "react-router-dom";
 import OpenAccountLanguageContent from '../../Services/OpenAccountLanguageContent';
 import Spinner from 'react-bootstrap/Spinner';
-import Thankyoupopup from './Thanku-popup.jsx';
 import Modal from 'react-bootstrap/Modal'
-import utils from "../../Services/utils";
 import { useGoogleReCaptcha } from 'react-google-recaptcha-v3';
 
 function OpenAccountOTPModal({ mobileNumber, otpSessionID,otpLeadID, onClose, language, openInfoPopup, showPopup,updateType }) {
-    // console.log('PPP',onClose.handleOTPClose());
-    // props -> mobileNumber, otpSessionID
+   
     const [loaders, setLoaders] = useState({});
     const [count, setCount] = useState(0);
     const [otp, setOtp] = useState('');
@@ -26,17 +22,12 @@ function OpenAccountOTPModal({ mobileNumber, otpSessionID,otpLeadID, onClose, la
     const [show, setShow] = useState(true);
     const { executeRecaptcha } = useGoogleReCaptcha();
     const [captchaToken, setCaptchaToken] = useState('');
-    // console.log('SSS',show);
+    
     function handleClose() {
-        //  console.log('Handleclose');
         setShow(() => false);
     }
-    //  console.log('OOOO',showPopup);
-    /**props object for Thankyou popup */
+   
     const [showlead, setShowLead] = useState({ showModal: false, page: 'no-addlead' });
-    // console.log('OOOO',showlead.showModal)
-
-
     const [leadId, setLeadId] = useState();
     const [redirectURL, setRedirectURL] = useState(() => null);
 
@@ -122,47 +113,7 @@ function OpenAccountOTPModal({ mobileNumber, otpSessionID,otpLeadID, onClose, la
                 session_id: otpSessionID
             };
             openAccountService.verifyOTPService(request,captchaToken,hideLoader,onClose,openInfoPopup,setOTPErrors)
-            // openAccountService.verifyOTP(request, captchaToken).then((res) => {
-            //     hideLoader('verifyLoader');
-            //     if (res && res.data.StatusCode === 200 && res.data.Body) {
-            //         let verifyResponse = res.data.Body;
-            //         // console.log("verifyResponse", verifyResponse);
-
-            //         if (verifyResponse.is_onboard_flag === "C") {
-            //             onClose("https://finx.choiceindia.com/auth/login",verifyResponse.message);
-            //         } else if (verifyResponse.is_onboard_flag === 'N' || verifyResponse.is_onboard_flag === '' || verifyResponse.is_onboard_flag === 'NI') {
-
-            //             let authCode = verifyResponse.auth_code;
-            //             let request = {
-            //                 "grant_type": "authorization_code",
-            //                 "code": authCode
-            //             };
-            //             openAccountService.getSSOToken(request).then((res) => {
-            //                 if (res && res.data.StatusCode == 200) {
-            //                     localStorage.setItem('access_token', res.data.Body.access_token);
-            //                     // console.log("verifyResponse in sso",res)
-            //                     let url = verifyResponse.url + "&accessToken=" + localStorage.getItem('access_token');
-            //                     // console.log("new url", url);
-            //                     // openInfoPopup(res.data.Message);
-            //                     onClose(url,verifyResponse.message);
-            //                 } else {
-            //                     openInfoPopup(res.data.Message);
-            //                     onClose();
-            //                 }
-            //             })
-
-            //         }
-            //     } else {
-            //         setOTPErrors((res && res.data && res.data.Body && res.data.Body.Message) ? res.data.Body.Message : OpenAccountLanguageContent.getContent(language ? language : 'en', 'otperror'));
-            //     }
-            // }).catch((error) => {
-            //     hideLoader('verifyLoader');
-            //     if (error && error.response && error.response.data && error.response.data.Message) {
-            //         setOTPErrors(error.response.data.Message);
-            //     } else {
-            //         setOTPErrors(OpenAccountLanguageContent.getContent(language ? language : 'en', 'otperror'));
-            //     }
-            // });
+          
         }
     }
     
@@ -187,84 +138,6 @@ function OpenAccountOTPModal({ mobileNumber, otpSessionID,otpLeadID, onClose, la
         }
     }, [captchaToken]);
 
-    //resend OTP ON SMS
-    function resendOTP() {
-        // console.log("check",otpID)
-        if (!loaders.resendOTPLoader && !loaders.OTPOnCallLoader) {
-            showLoader('resendOTPLoader');
-            setOtp('');
-            setOTPErrors('');
-
-
-            let request = {
-
-                "mobile_no": mobileNumber,
-                "old_session_id": otpID.current,
-                "request_source": "CHOICEINDIA",
-                // "captcha":"f9A0RMq3vF7fPYkEiqZToKUKdneNzA2YWfMeKSHhkm"
-            }
-            let requestMF = {
-
-                "old_session_id": otpID.current,
-                // "captcha":"f9A0RMq3vF7fPYkEiqZToKUKdneNzA2YWfMeKSHhkm"
-            }
-
-            openAccountService.resendOTPAgain((type2 == 'MF') ? requestMF : request, type2).then((res) => {
-                hideLoader('resendOTPLoader');
-                setCount(30);
-                if (res && res.status === 200 && res.data && res.data.Body) {
-                    otpID.current = res.data.Body.session_id;
-                    handleOTPResendSuccessToaster('otp');
-                } else {
-                    setOTPErrors((res.data.Body.Message) ? res.data.Body.Message : 'Something went wrong, please try again later!');
-                }
-            }).catch((error) => {
-                hideLoader('resendOTPLoader');
-                setCount(30);
-                if (error && error.response && error.response.data && error.response.data.Message) {
-                    setOTPErrors(error.response.data.Message);
-                } else {
-                    setOTPErrors("Something went wrong, please try again later!");
-                }
-            });
-        }
-    }
-
-    //to get OTP ON CALL
-    function getOTPOnCall() {
-        if (!loaders.resendOTPLoader && !loaders.OTPOnCallLoader) {
-            showLoader('OTPOnCallLoader');
-            setOtp('');
-            setOTPErrors('');
-            let request = {
-                "mobile_no": mobileNumber,
-                "request_source": ((type2 == 'MF') ? "MF" : "CHOICEINDIA"),
-                "session_id": otpID.current,
-                // "captcha":"f9A0RMq3vF7fPYkEiqZToKUKdneNzA2YWfMeKSHhkm"
-            }
-            openAccountService.OTPOnCall(request, type2).then((res) => {
-                hideLoader('OTPOnCallLoader');
-                // setCount(30);
-                if (res && res.status === 200 && res.data && res.data.Body) {
-                    setCount(30);
-                    otpID.current = res.data.Body.session_id;
-                    handleOTPResendSuccessToaster('call');
-                } else {
-                    setOTPErrors((res.data.Body.Message) ? res.data.Body.Message : 'Something went wrong, please try again later!');
-                }
-            }).catch((error) => {
-                hideLoader('OTPOnCallLoader');
-                // setCount(30);
-                if (error && error.response && error.response.data && error.response.data.Message) {
-                    setOTPErrors(error.response.data.Message);
-                } else {
-                    setOTPErrors("Something went wrong, please try again later!");
-                }
-            });
-        }
-    }
-
-
     function handleOTPResendSuccessToaster(type) {
         setOTPSendSuccessToaster({ [type]: true });
         setTimeout(() => {
@@ -284,97 +157,7 @@ function OpenAccountOTPModal({ mobileNumber, otpSessionID,otpLeadID, onClose, la
     };
     return (
         <>
-            {/* <div id="opt-box-id">
-                <div className="modal-body opt-body">
-                    A OTP has been sent to {'******' + mobileNumber.slice(6, 10)}
-                    <Form.Control className="w-50 form-control form-control-lg mx-auto text-center" type="text" id="openAccountOTP" placeholder="Enter OTP" autoComplete="one-time-code" maxLength="6" isInvalid={OTPErrors} value={otp} onChange={(e) => handleOTP(e)} />
-                    {
-                        OTPErrors ? <Form.Control.Feedback type="invalid">{OTPErrors}</Form.Control.Feedback> : ''
-                    }
-                    <div className="modal-footer otp-modal-footer">
-                        <button className="btn btn-primary verify-btn" disabled={loaders.verifyLoader} onClick={verifyOTP}>{loaders.verifyLoader ? <div className="dotLoaderB"></div> : 'Verify'}</button>
-                    </div>
-                    <div className="modal-otp-links">
-                        {
-                            count ?
-                                <p className="didnt-text">Didn't receive the OTP?<small className="timer-text">Resend in {count} seconds</small></p> :
-                                <div className="btm-div">
-                                    <p className="resend-text" onClick={resendOTP}>{loaders.resendOTPLoader ? <div className="dotLoaderB colorB marginLoader"></div> : 'Get OTP SMS'}</p>
-                                    <p className="or-text">OR</p>
-                                    <p className="resend-text" onClick={getOTPOnCall}>{loaders.OTPOnCallLoader ? <div className="dotLoaderB colorB marginLoader"></div> : 'Get OTP on Call'}</p>
-                                </div>
-                        }
-                    </div>
-                    {
-                        (OTPSendSuccessToaster.otp || OTPSendSuccessToaster.call) ?
-                            <Alert key='success' variant='success' onClose={() => setOTPSendSuccessToaster({})} dismissible>
-                                {
-                                    (OTPSendSuccessToaster.call) ? 'You will soon receive an automated call on given Mobile Number' : 'OTP has been resent on given Mobile Number'
-                                }
-                            </Alert> : ''
-                    }
-                </div>
-            </div> */}
-
-
-            {/* <div className="exit-intent-sleekbox-overlay sleekbox-popup-active ">
-                <div className="exit-intent-sleekbox-popup">
-                    <div className="popup-sub-row">
-                        <div className="close">
-                            <a href="javascript:void(0)" onClick={onClose} className="closebtn" >&times;</a>
-                        </div>
-                        <div className="popup-sub-right">
-
-                            <div>
-                                <img src={OTPimage} />
-
-                                <p className="heading">{OpenAccountLanguageContent.getContent(language ? language : 'en', 'otpmodalheader')}</p>
-                                <p className="subheading">{OpenAccountLanguageContent.getContent(language ? language : 'en', 'otplbl')} {'******' + mobileNumber.slice(6, 10)}</p>
-                                {
-                                    count ?
-                                        <p className="time">{OpenAccountLanguageContent.getContent(language ? language : 'en', 'otptime')}:<span > {count} {OpenAccountLanguageContent.getContent(language ? language : 'en', 'otpsec')}</span></p> : ''
-                                }
-
-                            </div>
-                            <div>
-
-
-                                <Form.Control className="w-50 form-control form-control-lg mx-auto text-center digit-otp" type="text" id="openAccountOTP" placeholder="Enter OTP" autoComplete="one-time-code" maxLength="6" isInvalid={OTPErrors} value={otp} onChange={(e) => handleOTP(e)} />
-                                {
-                                    OTPErrors ? <Form.Control.Feedback type="invalid">{OTPErrors}</Form.Control.Feedback> : ''
-                                }
-                            </div>
-
-                            <div className="btnwrap">
-                                <button className="btn-bg" disabled={loaders.verifyLoader} onClick={verifyOTP}>{loaders.verifyLoader ? <div className="dotLoaderB"></div> : OpenAccountLanguageContent.getContent(language ? language : 'en', 'otpverifybtn')}</button>
-                            </div>
-                            <div>
-                                {
-                                    !count ?
-                                        <div>
-                                            <button className="resend" onClick={resendOTP}>{loaders.resendOTPLoader ? <Spinner className="marginLoader" animation="border" role="status"><span className="visually-hidden">Loading123...</span></Spinner>
-                                                // <div className="dotLoaderB colorB marginLoader"></div>
-                                                : OpenAccountLanguageContent.getContent(language ? language : 'en', 'otpresend')}</button>
-                                            <span className="ortext">{OpenAccountLanguageContent.getContent(language ? language : 'en', 'otportext')}</span>
-                                            <button className="resend" onClick={getOTPOnCall}>{loaders.OTPOnCallLoader ? <Spinner className="marginLoader" animation="border" role="status"><span className="visually-hidden">Loading456...</span></Spinner>
-                                                // <div className="dotLoaderB colorB marginLoader"></div>
-                                                : OpenAccountLanguageContent.getContent(language ? language : 'en', 'otponcall')}</button></div> : ''
-                                }
-                            </div>
-                            <div className="mt-2">
-                                {
-                                    (OTPSendSuccessToaster.otp || OTPSendSuccessToaster.call) ?
-                                        <Alert key='success' variant='success' onClose={() => setOTPSendSuccessToaster({})} dismissible>
-                                            {
-                                                (OTPSendSuccessToaster.call) ? OpenAccountLanguageContent.getContent(language ? language : 'en', 'otpresendsuccess1') : OpenAccountLanguageContent.getContent(language ? language : 'en', 'otpresendsuccess2')
-                                            }
-                                        </Alert> : ''
-                                }
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
+            
 
 
             <Modal show={show} className="bt-strap-mdl otp-main-modal " onHide={onClose} backdrop='static' keyboard={false}>
@@ -385,9 +168,7 @@ function OpenAccountOTPModal({ mobileNumber, otpSessionID,otpLeadID, onClose, la
                     <div className="exit-intent-sleekbox-overlay sleekbox-popup-active">
                         <div className="exit-intent-sleekbox-popup">
                             <div className="popup-sub-row">
-                                {/* <div className="close">
-                                    <a href="javascript:void(0)" onClick={onClose} className="closebtn" >&times;</a>
-                                </div> */}
+                                
                                 <div className="popup-sub-right">
 
                                     <div>
@@ -418,11 +199,9 @@ function OpenAccountOTPModal({ mobileNumber, otpSessionID,otpLeadID, onClose, la
                                             !count ?
                                                 <div className="d-flex align-items-center justify-content-center">
                                                     <button className="resend" onClick={handleUpdateType}>{loaders.resendOTPLoader ? <Spinner className="marginLoader" animation="border" role="status"><span className="visually-hidden">Loading123...</span></Spinner>
-                                                        // <div className="dotLoaderB colorB marginLoader"></div>
                                                         : OpenAccountLanguageContent.getContent(language ? language : 'en', 'otpresend')}</button>
                                                     <span className="ortext">{OpenAccountLanguageContent.getContent(language ? language : 'en', 'otportext')}</span>
                                                     <button className="resend" onClick={handleUpdateTypeCall}>{loaders.OTPOnCallLoader ? <Spinner className="marginLoader" animation="border" role="status"><span className="visually-hidden">Loading456...</span></Spinner>
-                                                        // <div className="dotLoaderB colorB marginLoader"></div>
                                                         : OpenAccountLanguageContent.getContent(language ? language : 'en', 'otponcall')}</button></div> : ''
                                         }
                                     </div>
@@ -442,15 +221,6 @@ function OpenAccountOTPModal({ mobileNumber, otpSessionID,otpLeadID, onClose, la
                     </div>
                 </Modal.Body>
             </Modal>
-
-
-
-
-
-            {/* {
-            showlead.showModal? <Thankyoupopup isShow={showlead}/>:''
-           } */}
-
 
         </>
     );
