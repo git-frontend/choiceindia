d630f1e904af8bc7c9732d44727431c51dac3290
Shubham Palav <palavshubham09@gmail.com>
2024-06-27 15:31:31 +0530

    Worked on getExpertResearch function in research-listing page

--- src/components/Research/SBDesk.jsx
+++ src/components/Research/SBDesk.jsx
@@ -158,7 +158,68 @@ function SBDesk() {
      * @param {Session Id} session 
      */
     let getExpertResearch = (session) => {
-        ResearchService.getExpertResearch(session,setShowLoader,setResearchReport,subscribeMultitouchline,onRealtimeCallback);
+        setShowLoader(true)
+
+        let payload = {
+            end_date: '',
+            is_expert: 1,
+            research_type: '',
+            limit: 4,
+            offset: 0,
+            segment: 'EQ',
+            start_date: '',
+            status: '',
+            subcategory_id: "",
+            search: '',
+            id: "",
+            user_id: ''
+        }
+        rest.expertReportData(payload).then(res => {
+            setShowLoader(false)
+            if (res.message == "Success") {
+
+                let response = []
+                response = res.response.research;
+                let tokenList = [];
+                //generating tokens
+                generateToken(response,tokenList);
+                //let custom=[{ 'SegmentId': 5, 'Token': 241529 }]
+                subscribeMultitouchline(tokenList, onRealtimeCallback, session);
+                // resData = response
+                setResearchReport(response)
+            } else {
+                setResearchReport([])
+            }
+
+
+        }, err => {
+            setShowLoader(false)
+        })
+    }
+
+    function generateToken(response,tokenList){
+        response.forEach(ele => {
+
+            ele.published_date = utils.formatDate(new Date(ele.published_date), "dd MMMM'yy hh:mm:ss TT")
+            if (ele.datapoints && ele.datapoints.length) {
+                ele.priceData = {}
+                ele.datapoints.forEach(sub => {
+                    sub.key = (sub.key == 'cmp') ? 'entry_price' : sub.key;
+
+                    ele.priceData[sub.key] = sub
+                })
+
+                if (ele.priceData['entry_price'] && ele.priceData['stop_loss'] && ele.priceData['target']) {
+                    ele.priceData['entry_price_percentage'] = ((Number(ele.priceData['entry_price'].value) - Number(ele.priceData['stop_loss'].value)) / (Number(ele.priceData['target'].value) - Number(ele.priceData['stop_loss'].value))) * 85
+                }
+
+            }
+
+            tokenList.push({ 'SegmentId': ele.segment_id, 'Token': ele.token })
+            //   if(session)
+            //   subscribeOnStream(1,8866,onRealtimeCallback,'guest',session,false,)
+        })
+
     }
 
 
