3d6ea2474625ad6f4205ac38aaf9b620792f41db
Shubham Palav <palavshubham09@gmail.com>
2024-07-02 13:40:00 +0530

    Worked on getKeyInfo function

--- src/Services/ResearchService.js
+++ src/Services/ResearchService.js
@@ -3,48 +3,56 @@ import { API_URLS } from "./API-URLS";
 import rest from "./rest";
 import utils from "./utils";
 
-const Research = { 
+const Research = {
+  headerConfig: {
+    headers: { "x-api-key": "B62664943BAA286B21C66BA9A614D" },
+  },
+  headerConfig1: {
+    headers: { Authorization: "Session 9CD8B0421F" },
+  },
 
-    headerConfig: {
-        headers: { 'x-api-key':'B62664943BAA286B21C66BA9A614D' }
-    },
-    headerConfig1: {
-        headers: { 'Authorization':'Session 9CD8B0421F' }
-    },
+  researchcategory: function (id) {
+    let api = new API_URLS();
+    let url = api.getresearchURL(id);
+    return axios.get(url, this.headerConfig).then(({ data }) => {
+      return data;
+    });
+  },
 
-    researchcategory: function (id) {
-        let api = new API_URLS()
-        let url = api.getresearchURL(id)
-        return axios.get(url,this.headerConfig).then(({ data }) => {
-            return data
-        })
-    }, 
+  researchipo: function () {
+    let api = new API_URLS();
+    let url = api.getresearchipoURL();
+    return axios.get(url, this.headerConfig).then(({ data }) => {
+      return data;
+    });
+  },
 
-    researchipo: function () {
-        let api = new API_URLS()
-        let url = api.getresearchipoURL()
-        return axios.get(url,this.headerConfig).then(({ data }) => {
-            return data
-        })
-    },
+  successratio: function (postdata) {
+    let api = new API_URLS();
+    let url = api.getsuccessURL();
+    return axios.post(url, postdata).then(({ data }) => {
+      return data;
+    });
+  },
 
-    
+  getSingleResearchDetail: function (id, report_category) {
+    let api = new API_URLS();
+    let url = api.getresearchDetailURL(id, report_category);
+    return axios.get(url, this.headerConfig).then(({ data }) => {
+      return data;
+    });
+  },
 
-    successratio: function (postdata) {
-        let api = new API_URLS()
-        let url = api.getsuccessURL()
-        return axios.post(url,postdata).then(({ data }) => {
-            return data
-        })
-        
-    },
-
-    getSingleResearchDetail: function(id,report_category){
-        let api = new API_URLS()
-        let url = api.getresearchDetailURL(id,report_category)
-        return axios.get(url,this.headerConfig).then(({ data }) => {
-            return data
-        })
-    }
-}
+  getKeyInfoDetails: function (payload) {
+    let api = new API_URLS();
+    let url = api.getKeyInfoURL();
+    return axios
+      .post(url, payload, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => res.data);
+  },
+};
 export default Research;
